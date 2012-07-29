d7ce3f65c7b91057ab7888367f39be81eb8cc490
xoomdev <xoomdevnet@gmail.com>
2012-07-29 09:20:28 -0400

    Settings: Show full proc/version information
    Squashed commits:
     - Settings: Make kernel version clickable
    
       Change-Id: Ic08ec7a1db62a521e434cac5188aad2d35a4825d
    
     - Settings: Change kernel preference behavior
    
       Before you could only click once to see the full kernel version and if
       you wanted it to go back to the short version, you had to exit the fragment.
       Now you can click between the two (short and long).
    
       Change-Id: I6f89d6b145d5cc7b9cca6febc3651904b98da80c
    
    [Jyotiraditya Panda <jyotiraditya@aospa.co>]:
      - Improve code (Simplify handlePreferenceTreeClick logic,
        make use of package-private, replaced "Unavailable" with
        a localized string resource, used try-with-resources)
    
    Change-Id: I2b211fb72c25cc119f2a464b04821fcef77a8908
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- src/com/android/settings/deviceinfo/firmwareversion/KernelVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/KernelVersionPreferenceController.java
@@ -17,11 +17,26 @@
 package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.preference.Preference;
 
 import com.android.settings.core.BasePreferenceController;
 import com.android.settingslib.DeviceInfoUtils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class KernelVersionPreferenceController extends BasePreferenceController {
+
+    private static final String TAG = "KernelVersionPreferenceController";
+
+    private static final String KEY_KERNEL_VERSION = "kernel_version";
+    private static final String FILENAME_PROC_VERSION = "/proc/version";
+
+    private boolean mIsFullKernelVersionShown = false;
 
     public KernelVersionPreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey);
@@ -35,5 +50,30 @@ public class KernelVersionPreferenceController extends BasePreferenceController 
     @Override
     public CharSequence getSummary() {
         return DeviceInfoUtils.getFormattedKernelVersion(mContext);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_KERNEL_VERSION;
+    }
+
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        if (!TextUtils.equals(preference.getKey(), KEY_KERNEL_VERSION)) {
+            return false;
+        }
+
+        mIsFullKernelVersionShown = !mIsFullKernelVersionShown;
+        preference.setSummary(mIsFullKernelVersionShown ? getFullKernelVersion() : getSummary());
+        return true;
+    }
+
+    String getFullKernelVersion() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILENAME_PROC_VERSION), 256)) {
+            return reader.readLine();
+        } catch (IOException e) {
+            Log.e(TAG, "Error reading kernel version", e);
+            return mContext.getString(android.R.string.unknownName);
+        }
     }
 }
