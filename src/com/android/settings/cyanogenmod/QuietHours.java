6c27cc2168c07852646395d7952b6a6015ef6071
DvTonder <david.vantonder@gmail.com>
2012-11-22 13:08:47 -0500

    [WIP] Quiet Hours (2/3)
    ***
    Forward port Quiet Hours settings
    
    Change-Id: I57ad089a2b2f7d6ed3df773ca69ad67f7b8fe185
    
    https://github.com/CyanogenMod/android_packages_apps_Settings/commit/5801049a7ab3e2b97f389bc2dab3081b77b1feb9
    
    ***
    Settings: Bring back the new Quiet Hours UI
    
    Change-Id: I3bb78cb9c8ccf6df295bd724e32d97646144a6b9
    
    https://github.com/CyanogenMod/android_packages_apps_Settings/commit/e4dd1b525711404dbba748a8fcc1a255806fbb54
    
    ***
    Quiet Hours: Allow disabling phone ringer (2/3)
    
    Change-Id: Ica45cded1b70af36f9e0efdc5ec6911a138e0551
    
    https://github.com/CyanogenMod/android_packages_apps_Settings/commit/3971186ba4895791168cffaa27cc0fee4156648e
    ***
    QuiteHours: Add cyanogenmod checkbox class
    
    * Need to cleanup this if possible, haven't looked yet
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    
    PS2: Cleanup checkbox implement, use standard android ones.
    PS3: Finish checkboxes reimplementation.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    Change-Id: I71c61777f4a6d1a4be789ae858d8f7ab961832ba

---
+++ src/com/android/settings/cyanogenmod/QuietHours.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanogenmod;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class QuietHours extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener  {
+
+    private static final String TAG = "QuietHours";
+    private static final String KEY_QUIET_HOURS_TIMERANGE = "quiet_hours_timerange";
+    private static final CharSequence KEY_QUIET_HOURS_ENABLED = "quiet_hours_enabled";
+    private static final CharSequence KEY_QUIET_HOURS_RINGER = "quiet_hours_ringer";
+    private static final CharSequence KEY_QUIET_HOURS_MUTE = "quiet_hours_mute";
+    private static final CharSequence KEY_QUIET_HOURS_HAPTIC = "quiet_hours_haptic";
+    private static final CharSequence KEY_QUIET_HOURS_SYSTEM = "quiet_hours_system";
+    private static final CharSequence KEY_QUIET_HOURS_STILL = "quiet_hours_still";
+    private static final CharSequence KEY_QUIET_HOURS_DIM = "quiet_hours_dim";
+
+    private TimeRangePreference mQuietHoursTimeRange;
+    private ListPreference mQuietHoursRinger;
+    private CheckBoxPreference mQuietHoursEnabled;
+    private CheckBoxPreference mQuietHoursMute;
+    private CheckBoxPreference mQuietHoursHaptic;
+    private CheckBoxPreference mQuietHoursSystem;
+    private CheckBoxPreference mQuietHoursStill;
+    private CheckBoxPreference mQuietHoursDim;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.quiet_hours_settings);
+        Resources res = getResources();
+        ContentResolver resolver = getContentResolver();
+
+        // Load checkboxes
+        mQuietHoursEnabled = (CheckBoxPreference) findPreference(KEY_QUIET_HOURS_ENABLED);
+        mQuietHoursEnabled.setChecked(Settings.System.getInt(resolver,
+                Settings.System.QUIET_HOURS_ENABLED, 0) == 1);
+        mQuietHoursMute = (CheckBoxPreference) findPreference(KEY_QUIET_HOURS_MUTE);
+        mQuietHoursMute.setChecked(Settings.System.getInt(resolver,
+                Settings.System.QUIET_HOURS_MUTE, 0) == 1);
+        mQuietHoursHaptic = (CheckBoxPreference) findPreference(KEY_QUIET_HOURS_HAPTIC);
+        mQuietHoursHaptic.setChecked(Settings.System.getInt(resolver,
+                Settings.System.QUIET_HOURS_HAPTIC, 0) == 1);
+        mQuietHoursSystem = (CheckBoxPreference) findPreference(KEY_QUIET_HOURS_SYSTEM);
+        mQuietHoursSystem.setChecked(Settings.System.getInt(resolver,
+                Settings.System.QUIET_HOURS_SYSTEM, 0) == 1);
+        mQuietHoursStill = (CheckBoxPreference) findPreference(KEY_QUIET_HOURS_STILL);
+        mQuietHoursStill.setChecked(Settings.System.getInt(resolver,
+                Settings.System.QUIET_HOURS_STILL, 0) == 1);
+        mQuietHoursDim = (CheckBoxPreference) findPreference(KEY_QUIET_HOURS_DIM);
+        mQuietHoursDim.setChecked(Settings.System.getInt(resolver,
+                Settings.System.QUIET_HOURS_DIM, 0) == 1);
+
+        // Load the preferences
+        mQuietHoursTimeRange =
+                (TimeRangePreference) findPreference(KEY_QUIET_HOURS_TIMERANGE);
+        mQuietHoursTimeRange.setTimeRange(
+                Settings.System.getInt(resolver, Settings.System.QUIET_HOURS_START, 0),
+                Settings.System.getInt(resolver, Settings.System.QUIET_HOURS_END, 0));
+        mQuietHoursTimeRange.setOnPreferenceChangeListener(this);
+
+
+        // Remove the ringer setting on non-telephony devices else enable it
+        mQuietHoursRinger = (ListPreference) findPreference(KEY_QUIET_HOURS_RINGER);
+        TelephonyManager telephonyManager =
+                (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        if (telephonyManager.getPhoneType() == TelephonyManager.PHONE_TYPE_NONE) {
+            getPreferenceScreen().removePreference(mQuietHoursRinger);
+            mQuietHoursRinger = null;
+        } else {
+            int muteType = Settings.System.getInt(resolver, Settings.System.QUIET_HOURS_RINGER, 0);
+            mQuietHoursRinger.setValue(String.valueOf(muteType));
+            mQuietHoursRinger.setSummary(mQuietHoursRinger.getEntry());
+            mQuietHoursRinger.setOnPreferenceChangeListener(this);
+        }
+
+        // Remove the notification light setting if the device does not support it
+        if (!res.getBoolean(com.android.internal.R.bool.config_intrusiveNotificationLed)) {
+            removePreference(Settings.System.QUIET_HOURS_DIM);
+        }
+
+        // Remove the vibrator dependent settings if the device does not have a vibrator
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (vibrator == null || !vibrator.hasVibrator()) {
+               removePreference(Settings.System.QUIET_HOURS_STILL);
+               removePreference(Settings.System.QUIET_HOURS_HAPTIC);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getContentResolver();
+        if (preference == mQuietHoursTimeRange) {
+            Settings.System.putInt(resolver, Settings.System.QUIET_HOURS_START,
+                    mQuietHoursTimeRange.getStartTime());
+            Settings.System.putInt(resolver, Settings.System.QUIET_HOURS_END,
+                    mQuietHoursTimeRange.getEndTime());
+            return true;
+        } else if (preference == mQuietHoursRinger) {
+            int ringerMuteType = Integer.valueOf((String) newValue);
+            int index = mQuietHoursRinger.findIndexOfValue((String) newValue);
+            Settings.System.putInt(resolver, Settings.System.QUIET_HOURS_RINGER, ringerMuteType);
+            mQuietHoursRinger.setSummary(mQuietHoursRinger.getEntries()[index]);
+            return true;
+        } else if (preference == mQuietHoursEnabled) {
+            Settings.System.putInt(getContentResolver(), Settings.System.QUIET_HOURS_ENABLED,
+                    mQuietHoursEnabled.isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mQuietHoursMute) {
+            Settings.System.putInt(getContentResolver(), Settings.System.QUIET_HOURS_MUTE,
+                    mQuietHoursMute.isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mQuietHoursHaptic) {
+            Settings.System.putInt(getContentResolver(), Settings.System.QUIET_HOURS_HAPTIC,
+                    mQuietHoursHaptic.isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mQuietHoursSystem) {
+            Settings.System.putInt(getContentResolver(), Settings.System.QUIET_HOURS_SYSTEM,
+                    mQuietHoursSystem.isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mQuietHoursStill) {
+            Settings.System.putInt(getContentResolver(), Settings.System.QUIET_HOURS_STILL,
+                    mQuietHoursStill.isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mQuietHoursDim) {
+            Settings.System.putInt(getContentResolver(), Settings.System.QUIET_HOURS_DIM,
+                    mQuietHoursDim.isChecked() ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+}
