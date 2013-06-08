c87d4f02317fd37ca62955f72ffc0463d281e5a6
Rakesh Pallerla <rakesh@codeaurora.org>
2013-06-08 16:49:47 +0530

    Multisim: Add support for MSIM
    Add Multi SIM Settings screen in UI which is used to select
    Voice, SMS, Data preferences.
    Extend APN Settings, Security Settings, Wireless Settings
    for MSIM.
    
    Subscription specific for Mobile data, roaming and usage
     - Provide per subscription based support for both Mobile Data
       and Data roaming options. UI and functionality changes in:
       a. Settings -> Data Usage application
       b. Settings -> Mobile Networks menu
     - Provide per subscription based data usage information. UI and
       functionality changes in Settings -> Data Usage application.
    
    Add prompt support for SMS
     - Add prompt option in SMS multi sim settings.
     - New iterface API to get SMS prompt is enabled or not.
     - Disply prompt while sending SMS, if user selects
       prompt mode in multi sim settings
    
    Extend CB "Operator info" feature to MSIM.
     - Add SUB information while broadcasting CB intent for MSIM.
     - Remove country specific checks for cellbroadcast Operator
       info for MSIM.
     - Move Operator info to About phone->Status->SelectSubscription
       ->SUB1/SUB2->Operator info in MSIM
    
    Change-Id: I84abe80f20de2415ff819c7250ef0a0799662b52
    (cherry picked from commit ccfd14b8a86c78c6a179a70be1357131b80d4823)
    (cherry picked from commit 794b5787304bed493bbde3e9c9010a4b82824641)
    
    Conflicts:
    
    	Android.mk
    	res/values/arrays.xml
    	res/xml/settings_headers.xml
    	src/com/android/settings/DeviceInfoSettings.java
    	src/com/android/settings/SecuritySettings.java

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -30,6 +30,7 @@ import android.os.UserHandle;
 import android.preference.Preference;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
+import android.telephony.MSimTelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -67,6 +68,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_PA_VERSION = "pa_version";
     private static final String KEY_PARANOIDOTA = "paranoidota_settings";
+    private static final String KEY_STATUS = "status_info";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -108,6 +110,11 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
             setStringSummary(KEY_SELINUX_STATUS, status);
         }
 
+        if (MSimTelephonyManager.getDefault().isMultiSimEnabled()) {
+            findPreference(KEY_STATUS).getIntent().setClassName(
+                    "com.android.settings","com.android.settings.deviceinfo.MSimStatus");
+        }
+
         // Remove selinux information if property is not present
         removePreferenceIfPropertyMissing(getPreferenceScreen(), KEY_SELINUX_STATUS,
                 PROPERTY_SELINUX_STATUS);
@@ -121,7 +128,8 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
                 PROPERTY_EQUIPMENT_ID);
 
         // Remove Baseband version if wifi-only device
-        if (Utils.isWifiOnly(getActivity())) {
+        if (Utils.isWifiOnly(getActivity())
+                || (MSimTelephonyManager.getDefault().isMultiSimEnabled())) {
             getPreferenceScreen().removePreference(findPreference(KEY_BASEBAND_VERSION));
         }
 
