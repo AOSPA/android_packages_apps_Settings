bb0fe43016bf62ca0ae6f8ed13869b47aae9e5bc
Jorge Ruesga <jorge@ruesga.com>
2013-10-06 00:45:02 +0200

    Settings: Long Term Orbits (LTO)
    Ported from http://review.cyanogenmod.org/#/c/51422/3
    
    Change-Id: Ia1fdf9e0ffa401f64ee9a5c130ed10709d930eb7
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>
    
    Conflicts:
    
    	res/values/cm_strings.xml

--- src/com/android/settings/cyanogenmod/BootReceiver.java
+++ src/com/android/settings/cyanogenmod/BootReceiver.java
@@ -29,6 +29,7 @@ import com.android.settings.Utils;
 import com.android.settings.hardware.DisplayColor;
 import com.android.settings.hardware.DisplayGamma;
 import com.android.settings.hardware.VibratorIntensity;
+import com.android.settings.location.LocationSettings;
 
 import java.util.Arrays;
 import java.util.List;
@@ -74,6 +75,7 @@ public class BootReceiver extends BroadcastReceiver {
         DisplayGamma.restore(ctx);
         VibratorIntensity.restore(ctx);
         DisplaySettings.restore(ctx);
+        LocationSettings.restore(ctx);
     }
 
     private void configureCPU(Context ctx) {
