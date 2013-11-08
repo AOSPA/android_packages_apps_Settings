028ba8d53e537a71892e50336897c0365088d347
Francisco Franco <franciscofranco.1990@gmail.com>
2013-11-08 23:31:56 +0000

    Fix typo.
    Change-Id: I9c69b1c9b3b739a1fd9e1fab28ed8fa6b2880476
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -63,7 +63,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
     private static final String KEY_UPDATE_SETTING = "additional_system_update_settings";
     private static final String KEY_EQUIPMENT_ID = "fcc_equipment_id";
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
-    private static final String KEY_PA_VERISION = "pa_version";
+    private static final String KEY_PA_VERSION = "pa_version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
