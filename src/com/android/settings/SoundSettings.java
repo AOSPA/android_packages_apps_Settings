ceaa1e82dd854cd8e91347acb10ef3811d62bbfa
Ayysir <dresadd09691@gmail.com>
2013-11-15 20:04:11 -0500

    [WIP] Start music player if headset is connected (2/2)
    Signed-off-by: Ayysir <dresadd09691@gmail.com>

--- src/com/android/settings/SoundSettings.java
+++ src/com/android/settings/SoundSettings.java
@@ -77,6 +77,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements
     private static final String KEY_DOCK_AUDIO_MEDIA_ENABLED = "dock_audio_media_enabled";
     private static final String KEY_VOLUME_WAKE_SCREEN = "volume_wake_screen";
     private static final String KEY_VOLUME_MUSIC_CONTROLS = "volume_music_controls";
+    private static final String KEY_HEADSET_CONNECT_PLAYER = "headset_connect_player";
 
     private static final String[] NEED_VOICE_CAPABILITY = {
             KEY_RINGTONE, KEY_DTMF_TONE, KEY_CATEGORY_CALLS,
@@ -96,6 +97,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements
     private Preference mNotificationPreference;
     private CheckBoxPreference mVolumeWakeScreen;
     private CheckBoxPreference mVolumeMusicCtrl;
+    private CheckBoxPreference mHeadsetConnectPlayer;
 
     private Runnable mRingtoneLookupRunnable;
 
@@ -186,6 +188,10 @@ public class SoundSettings extends SettingsPreferenceFragment implements
 
         mRingtonePreference = findPreference(KEY_RINGTONE);
         mNotificationPreference = findPreference(KEY_NOTIFICATION_SOUND);
+
+        mHeadsetConnectPlayer = (CheckBoxPreference) findPreference(KEY_HEADSET_CONNECT_PLAYER);
+        mHeadsetConnectPlayer.setChecked(Settings.System.getInt(resolver,
+                Settings.System.HEADSET_CONNECT_PLAYER, 0) != 0);
 
         Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         if (vibrator == null || !vibrator.hasVibrator()) {
@@ -322,6 +328,9 @@ public class SoundSettings extends SettingsPreferenceFragment implements
         } else if (preference == mVolumeMusicCtrl) {
             Settings.System.putInt(getContentResolver(), Settings.System.VOLUME_MUSIC_CONTROLS,
                     mVolumeMusicCtrl.isChecked() ? 1 : 0);
+        } else if (preference == mHeadsetConnectPlayer) {
+            Settings.System.putInt(getContentResolver(), Settings.System.HEADSET_CONNECT_PLAYER,
+                    mHeadsetConnectPlayer.isChecked() ? 1 : 0);
 
         } else if (preference == mMusicFx) {
             // let the framework fire off the intent
