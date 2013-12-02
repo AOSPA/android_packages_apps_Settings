38d30e86af617377dba87fefcaaacbd0b12d85e3
TheMrcool212 <munirmohamadrani@gmail.com>
2013-12-02 20:51:38 +0800

    Revert "Settings : Fix accessibility shortcut"
    This reverts commit b9ff7cda89b5f49ca453a00cea86d7a29bb6d0e6.

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -498,23 +498,10 @@ public class Settings extends PreferenceActivity
 
     @Override
     public Intent onBuildStartFragmentIntent(String fragmentName, Bundle args,
-            CharSequence titleText, CharSequence shortTitleText) {
-        Intent intent = super.onBuildStartFragmentIntent(fragmentName, args,
-                titleText, shortTitleText);
-        onBuildStartFragmentIntentHelper(fragmentName, intent);
-        return intent;
-    }
-
-    @Override
-    public Intent onBuildStartFragmentIntent(String fragmentName, Bundle args,
             int titleRes, int shortTitleRes) {
         Intent intent = super.onBuildStartFragmentIntent(fragmentName, args,
                 titleRes, shortTitleRes);
-        onBuildStartFragmentIntentHelper(fragmentName, intent);
-        return intent;
-    }
 
-    private void onBuildStartFragmentIntentHelper(String fragmentName, Intent intent) {
         // Some fragments want split ActionBar; these should stay in sync with
         // uiOptions for fragments also defined as activities in manifest.
         if (WifiSettings.class.getName().equals(fragmentName) ||
@@ -528,7 +515,9 @@ public class Settings extends PreferenceActivity
                 PrintServiceSettingsFragment.class.getName().equals(fragmentName)) {
             intent.putExtra(EXTRA_UI_OPTIONS, ActivityInfo.UIOPTION_SPLIT_ACTION_BAR_WHEN_NARROW);
         }
+
         intent.setClass(this, SubSettings.class);
+        return intent;
     }
 
     /**
