faa14d4e422eae287077ad56afe7e5d4b74c6c85
TheMrcool212 <munirmohamadrani@gmail.com>
2013-12-20 12:50:24 +0800

    [1/2] Additional settings: add option to hide power menu when screen is locked
    https://gerrit.omnirom.org/#/c/3472/
    
    Move clock widget option to lockscreen category as well

--- src/com/android/settings/AOSPAL/AdditionalSettings.java
+++ src/com/android/settings/AOSPAL/AdditionalSettings.java
@@ -26,10 +26,11 @@ public class AdditionalSettings extends SettingsPreferenceFragment implements
     private static final String KEY_LISTVIEW_ANIMATION = "listview_animation";
     private static final String KEY_LISTVIEW_INTERPOLATOR = "listview_interpolator";
     private static final String KEY_DUAL_PANEL = "force_dualpanel"; 
-
+    private static final String LOCKSCREEN_POWER_MENU = "lockscreen_power_menu";
 
     ListPreference mQuickPulldown;
     private CheckBoxPreference mHeadsetHookLaunchVoice;
+    private CheckBoxPreference mLockScreenPowerMenu;
     private CheckBoxPreference mDualPanel;
     private ListPreference mListViewAnimation;
     private ListPreference mListViewInterpolator;
@@ -81,6 +82,13 @@ public class AdditionalSettings extends SettingsPreferenceFragment implements
 
         mDualPanel = (CheckBoxPreference) findPreference(KEY_DUAL_PANEL);
         mDualPanel.setChecked(Settings.System.getBoolean(getContentResolver(), Settings.System.FORCE_DUAL_PANEL, false));
+
+        mLockScreenPowerMenu = (CheckBoxPreference) root.findPreference(LOCKSCREEN_POWER_MENU);
+        if (mLockScreenPowerMenu != null) {
+            mLockScreenPowerMenu.setChecked(Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.LOCK_SCREEN_POWER_MENU, 1) == 1);
+            mLockScreenPowerMenu.setOnPreferenceChangeListener(this);
+        }
     }
 
     @Override
@@ -121,11 +129,13 @@ public class AdditionalSettings extends SettingsPreferenceFragment implements
             boolean checked = ((CheckBoxPreference)preference).isChecked();
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.HEADSETHOOK_LAUNCH_VOICE, checked ? 1:0);
-
             return true;
         } else if (preference == mDualPanel) {
             Settings.System.putBoolean(getContentResolver(), Settings.System.FORCE_DUAL_PANEL, ((CheckBoxPreference) preference).isChecked());
             return true; 
+        } else if (preference == mLockScreenPowerMenu) {
+            Settings.Secure.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.Secure.LOCK_SCREEN_POWER_MENU, isToggled(preference) ? 1 : 0);
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
