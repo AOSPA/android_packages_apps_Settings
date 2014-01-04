6bbc30efae48f45d1ea48e3704361ed15f40f3fb
TheMrcool212 <munirmohamadrani@gmail.com>
2014-01-04 13:06:25 +0800

    Revert "Settings: UMS [2/2]"
    This reverts commit 65d8acbed83581fe6567351de428f0a693e6b2cf.

--- src/com/android/settings/deviceinfo/UsbSettings.java
+++ src/com/android/settings/deviceinfo/UsbSettings.java
@@ -23,8 +23,6 @@ import android.content.IntentFilter;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.os.UserManager;
-import android.os.storage.StorageManager;
-import android.os.storage.StorageVolume;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
@@ -43,14 +41,10 @@ public class UsbSettings extends SettingsPreferenceFragment {
 
     private static final String KEY_MTP = "usb_mtp";
     private static final String KEY_PTP = "usb_ptp";
-    private static final String KEY_MASS_STORAGE = "usb_mass_storage";
 
     private UsbManager mUsbManager;
-    private StorageManager storageManager;
-    private StorageVolume[] storageVolumes;
     private CheckBoxPreference mMtp;
     private CheckBoxPreference mPtp;
-    private CheckBoxPreference mUms;
     private boolean mUsbAccessoryMode;
 
     private final BroadcastReceiver mStateReceiver = new BroadcastReceiver() {
@@ -74,16 +68,11 @@ public class UsbSettings extends SettingsPreferenceFragment {
 
         mMtp = (CheckBoxPreference)root.findPreference(KEY_MTP);
         mPtp = (CheckBoxPreference)root.findPreference(KEY_PTP);
-        mUms = (CheckBoxPreference)root.findPreference(KEY_MASS_STORAGE);
-        if (!storageManager.isUsbMassStorageSupported()) {
-            root.removePreference(mUms);
-        }
 
         UserManager um = (UserManager) getActivity().getSystemService(Context.USER_SERVICE);
         if (um.hasUserRestriction(UserManager.DISALLOW_USB_FILE_TRANSFER)) {
             mMtp.setEnabled(false);
             mPtp.setEnabled(false);
-            mUms.setEnabled(false);
         }
 
         return root;
@@ -93,8 +82,6 @@ public class UsbSettings extends SettingsPreferenceFragment {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         mUsbManager = (UsbManager)getSystemService(Context.USB_SERVICE);
-        storageManager = (StorageManager) getSystemService(Context.STORAGE_SERVICE);
-        storageVolumes = storageManager.getVolumeList();
     }
 
     @Override
@@ -120,37 +107,27 @@ public class UsbSettings extends SettingsPreferenceFragment {
         if (UsbManager.USB_FUNCTION_MTP.equals(function)) {
             mMtp.setChecked(true);
             mPtp.setChecked(false);
-            mUms.setChecked(false);
         } else if (UsbManager.USB_FUNCTION_PTP.equals(function)) {
             mMtp.setChecked(false);
             mPtp.setChecked(true);
-            mUms.setChecked(false);
-        } else if (UsbManager.USB_FUNCTION_MASS_STORAGE.equals(function)) {
-            mMtp.setChecked(false);
-            mPtp.setChecked(false);
-            mUms.setChecked(true);
         } else  {
             mMtp.setChecked(false);
             mPtp.setChecked(false);
-            mUms.setChecked(false);
         }
         UserManager um = (UserManager) getActivity().getSystemService(Context.USER_SERVICE);
         if (um.hasUserRestriction(UserManager.DISALLOW_USB_FILE_TRANSFER)) {
             Log.e(TAG, "USB is locked down");
             mMtp.setEnabled(false);
             mPtp.setEnabled(false);
-            mUms.setEnabled(false);
         } else if (!mUsbAccessoryMode) {
-            // Enable function switch while USB is not in Accessory Mode, otherwise disable it
+            //Enable MTP and PTP switch while USB is not in Accessory Mode, otherwise disable it
             Log.e(TAG, "USB Normal Mode");
             mMtp.setEnabled(true);
             mPtp.setEnabled(true);
-            mUms.setEnabled(true);
         } else {
             Log.e(TAG, "USB Accessory Mode");
             mMtp.setEnabled(false);
             mPtp.setEnabled(false);
-            mUms.setEnabled(false);
         }
     }
 
@@ -174,8 +151,6 @@ public class UsbSettings extends SettingsPreferenceFragment {
             function = UsbManager.USB_FUNCTION_MTP;
         } else if (preference == mPtp && mPtp.isChecked()) {
             function = UsbManager.USB_FUNCTION_PTP;
-        } else if (preference == mUms && mUms.isChecked()) {
-            function = UsbManager.USB_FUNCTION_MASS_STORAGE;
         }
 
         mUsbManager.setCurrentFunction(function, true);
