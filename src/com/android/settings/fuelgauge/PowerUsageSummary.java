5a0468e9cdaeb935aa2e283bbdead32eba6e5dbd
maxwen <max.weninger@gmail.com>
2014-01-27 22:56:16 +0100

    Settings: fixed battery stats app list
    http://jira.omnirom.org/browse/OMNI-493
    
    The check of mAppListGroup getPreferenceCount is no longer working
    after we added other options on this page. Add a separate counter to get
    back the full possible list of 12 apps
    
    PS3:
    IMHO the number of apps should be 10 if we have
    MAX_ITEMS_TO_LIST = 10
    
    Change-Id: I679b01deec0f52d4fa2012a31b7b305f8fe176b5

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -255,6 +255,7 @@ public class PowerUsageSummary extends PreferenceFragment {
             return;
         }
         mStatsHelper.refreshStats(false);
+        int sipperCount = 0;
         List<BatterySipper> usageList = mStatsHelper.getUsageList();
         for (BatterySipper sipper : usageList) {
             if (sipper.getSortValue() < MIN_POWER_THRESHOLD) continue;
@@ -273,7 +274,8 @@ public class PowerUsageSummary extends PreferenceFragment {
                 pref.setKey(Integer.toString(sipper.uidObj.getUid()));
             }
             mAppListGroup.addPreference(pref);
-            if (mAppListGroup.getPreferenceCount() > (MAX_ITEMS_TO_LIST+1)) break;
+            sipperCount++;
+            if (sipperCount >= MAX_ITEMS_TO_LIST) break;
         }
     }
 
