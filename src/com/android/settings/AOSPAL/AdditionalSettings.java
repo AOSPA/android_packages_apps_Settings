2ca7372e63df8debd3d1a7b8769ba551b0f9441f
David96 <xd_e-mail@gmx.de>
2014-02-09 22:42:31 +0100

    Custom wallpaper: use PNG

--- src/com/android/settings/AOSPAL/AdditionalSettings.java
+++ src/com/android/settings/AOSPAL/AdditionalSettings.java
@@ -320,7 +320,7 @@ public class AdditionalSettings extends SettingsPreferenceFragment implements
             intent.putExtra("crop", "true");
             intent.putExtra("scale", true);
             intent.putExtra("scaleUpIfNeeded", false);
-            intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+            intent.putExtra("outputFormat", Bitmap.CompressFormat.PNG.toString());
 
             final Display display = getActivity().getWindowManager().getDefaultDisplay();
 
