32f5ab9a7ca3a3133693529e6ca905b31ce10ea1
Jesús David Gulfo Agudelo <jesdga95@gmail.com>
2014-02-18 10:46:39 -0500

    Revert "[2/3]Quiet Hours"
    This reverts commit 8a202029be0b528c4d1b6499126e7743590bd552.
    
    Change-Id: Iae74de61bdd252ed003eebb65727ff12f32de013

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -92,7 +92,6 @@ import com.android.settings.wifi.AdvancedWifiSettings;
 import com.android.settings.wifi.WifiEnabler;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
-import com.android.settings.paranoid.QuietHours;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -352,8 +351,7 @@ public class Settings extends PreferenceActivity
         PrintJobSettingsFragment.class.getName(),
         TrustedCredentialsSettings.class.getName(),
         PaymentSettings.class.getName(),
-        KeyboardLayoutPickerFragment.class.getName(),
-        QuietHours.class.getName()
+        KeyboardLayoutPickerFragment.class.getName()
     };
 
     @Override
@@ -1089,7 +1087,6 @@ public class Settings extends PreferenceActivity
     public static class LocalePickerActivity extends Settings { /* empty */ }
     public static class UserDictionarySettingsActivity extends Settings { /* empty */ }
     public static class SoundSettingsActivity extends Settings { /* empty */ }
-    public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
     public static class DisplaySettingsActivity extends Settings { /* empty */ }
     public static class DeviceInfoSettingsActivity extends Settings { /* empty */ }
     public static class ApplicationSettingsActivity extends Settings { /* empty */ }
