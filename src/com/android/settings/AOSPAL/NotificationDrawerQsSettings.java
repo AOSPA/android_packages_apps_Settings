cd61c8519a770364f46997d8cb85bf290c938b1c
TheMrcool212 <munirmohamadrani@gmail.com>
2014-02-27 18:29:30 +0800

    Revert "Settings: notification drawer background & transparency"
    This reverts commit 005eeac43a59b861ceb25ec422658c1f7ee103c5.

--- src/com/android/settings/AOSPAL/NotificationDrawerQsSettings.java
+++ src/com/android/settings/AOSPAL/NotificationDrawerQsSettings.java
@@ -2,10 +2,6 @@ package com.android.settings.AOSPAL;
 
 import android.app.Activity;
 import android.app.ActivityManager;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.DialogInterface;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.ContentResolver;
@@ -17,7 +13,6 @@ import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.media.Ringtone;
@@ -38,6 +33,7 @@ import android.preference.PreferenceScreen;
 import android.preference.PreferenceCategory;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.RingtonePreference;
+import android.preference.SeekBarPreference;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Display;
@@ -49,27 +45,15 @@ import android.widget.Toast;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.slim.SeekBarPreference;
 import com.android.internal.util.paranoid.DeviceUtils;
-
-import java.io.File;
-
-import net.margaritov.preference.colorpicker.ColorPickerView;
 
 public class NotificationDrawerQsSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
-    private static final String TAG = "NotificationDrawerQs";
     private static final String NOTIFICATION_DRAWER_QS_SETTINGS = "notification_drawer_qs_settings";
 
     private static final String PREF_NOTI_REMINDER_SOUND = "noti_reminder_sound";
     private static final String PREF_NOTI_REMINDER_ENABLED = "noti_reminder_enabled";
     private static final String PREF_NOTI_REMINDER_RINGTONE = "noti_reminder_ringtone";
-    private static final String PREF_NOTIFICATION_WALLPAPER = "notification_wallpaper";
-    private static final String PREF_NOTIFICATION_WALLPAPER_LANDSCAPE = "notification_wallpaper_landscape";
-    private static final String PREF_NOTIFICATION_WALLPAPER_ALPHA = "notification_wallpaper_alpha";
-    private static final String PREF_NOTIFICATION_ALPHA = "notification_alpha";
-
-    private static final int DLG_PICK_COLOR = 0;
 
     private static final String QS_CATEGORY = "qs_category";
     private static final String QUICK_PULLDOWN = "quick_pulldown";
@@ -79,17 +63,8 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment imp
     ListPreference mReminderMode;
     ListPreference mQuickPulldown;
     ListPreference mSmartPulldown;
-    private ListPreference mNotificationWallpaper;
-    private ListPreference mNotificationWallpaperLandscape;
-    SeekBarPreference mWallpaperAlpha;
-    SeekBarPreference mNotificationAlpha;
 
     RingtonePreference mReminderRingtone;
-
-    private File mImageTmp;
-    private static final int REQUEST_PICK_WALLPAPER = 201;
-    private static final int REQUEST_PICK_WALLPAPER_LANDSCAPE = 202;
-    private Activity mActivity;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -150,103 +125,16 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment imp
         mReminderRingtone.setOnPreferenceChangeListener(this);
         mReminderRingtone.setEnabled(mode != 0);
 
-        mImageTmp = new File(getActivity().getFilesDir() + "/notifi_bg.tmp");
-
-        mNotificationWallpaper =
-                (ListPreference) findPreference(PREF_NOTIFICATION_WALLPAPER);
-        mNotificationWallpaper.setOnPreferenceChangeListener(this);
-
-        mNotificationWallpaperLandscape =
-                (ListPreference) findPreference(PREF_NOTIFICATION_WALLPAPER_LANDSCAPE);
-        mNotificationWallpaperLandscape.setOnPreferenceChangeListener(this);
-
-        if (!DeviceUtils.isPhone(mActivity)) {
-            prefs.removePreference(mNotificationWallpaperLandscape);
-        }
-
-        float transparency;
-        try{
-             transparency = Settings.System.getFloat(getContentResolver(),
-                    Settings.System.NOTIFICATION_BACKGROUND_ALPHA);
-        } catch (Exception e) {
-            transparency = 0;
-             Settings.System.putFloat(getContentResolver(),
-                    Settings.System.NOTIFICATION_BACKGROUND_ALPHA, 0.1f);
-        }
-        mWallpaperAlpha = (SeekBarPreference) findPreference(PREF_NOTIFICATION_WALLPAPER_ALPHA);
-        mWallpaperAlpha.setInitValue((int) (transparency * 100));
-        mWallpaperAlpha.setProperty(Settings.System.NOTIFICATION_BACKGROUND_ALPHA);
-        mWallpaperAlpha.setOnPreferenceChangeListener(this);
-
-        try{
-            transparency = Settings.System.getFloat(getContentResolver(),
-                    Settings.System.NOTIFICATION_ALPHA);
-        } catch (Exception e) {
-            transparency = 0;
-            Settings.System.putFloat(getContentResolver(),
-                    Settings.System.NOTIFICATION_ALPHA, 0.0f);
-        }
-        mNotificationAlpha = (SeekBarPreference) findPreference(PREF_NOTIFICATION_ALPHA);
-        mNotificationAlpha.setInitValue((int) (transparency * 100));
-        mNotificationAlpha.setProperty(Settings.System.NOTIFICATION_ALPHA);
-        mNotificationAlpha.setOnPreferenceChangeListener(this);
-
-       updateCustomBackgroundSummary();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        updateCustomBackgroundSummary();
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mWallpaperAlpha) {
-            float valNav = Float.parseFloat((String) newValue);
-            Settings.System.putFloat(getContentResolver(),
-                    Settings.System.NOTIFICATION_BACKGROUND_ALPHA, valNav / 100);
-            return true;
-        } else if (preference == mNotificationAlpha) {
-            float valNav = Float.parseFloat((String) newValue);
-            Settings.System.putFloat(getContentResolver(),
-                    Settings.System.NOTIFICATION_ALPHA, valNav / 100);
-            return true;
-        }else if (preference == mNotificationWallpaper) {
-            int indexOf = mNotificationWallpaper.findIndexOfValue(newValue.toString());
-            switch (indexOf) {
-                //Displays color dialog when user has chosen color fill
-                case 0:
-                    showDialogInner(DLG_PICK_COLOR);
-                    break;
-                //Launches intent for user to select an image/crop it to set as background
-                case 1:
-                    startPictureCrop(REQUEST_PICK_WALLPAPER, false);
-                    break;
-                //Sets background to default
-                case 2:
-                    deleteWallpaper(false);
-                    deleteWallpaper(true);
-                    Settings.System.putString(getContentResolver(),
-                            Settings.System.NOTIFICATION_BACKGROUND, null);
-                    updateCustomBackgroundSummary();
-                    break;
-            }
-            return true;
-        }else if (preference == mNotificationWallpaperLandscape) {
-            int indexOf = mNotificationWallpaperLandscape.findIndexOfValue(newValue.toString());
-            switch (indexOf) {
-                //Launches intent for user to select an image/crop it to set as background
-                case 0:
-                    startPictureCrop(REQUEST_PICK_WALLPAPER_LANDSCAPE, true);
-                    break;
-                //Sets background to default
-                case 1:
-                    deleteWallpaper(true);
-                    updateCustomBackgroundSummary();
-                    break;
-            }
-            return true;
-        } else if (preference == mQuickPulldown) {
+
+        if (preference == mQuickPulldown) {
             int statusQuickPulldown = Integer.valueOf((String) newValue);
             Settings.System.putIntForUser(getContentResolver(), Settings.System.QS_QUICK_PULLDOWN,
                     statusQuickPulldown, UserHandle.USER_CURRENT);
@@ -327,189 +215,4 @@ public class NotificationDrawerQsSettings extends SettingsPreferenceFragment imp
         mReminderMode.setSummary(getResources().getString(resId));
     }
 
-    private void updateCustomBackgroundSummary() {
-        int resId;
-        String value = Settings.System.getString(getContentResolver(),
-                Settings.System.NOTIFICATION_BACKGROUND);
-        if (value == null) {
-            resId = R.string.notification_background_default_wallpaper;
-            mNotificationWallpaper.setValueIndex(2);
-            mNotificationWallpaperLandscape.setEnabled(false);
-        } else if (value.startsWith("color=")) {
-            resId = R.string.notification_background_color_fill;
-            mNotificationWallpaper.setValueIndex(0);
-            mNotificationWallpaperLandscape.setEnabled(false);
-        } else {
-            resId = R.string.notification_background_custom_image;
-            mNotificationWallpaper.setValueIndex(1);
-            mNotificationWallpaperLandscape.setEnabled(true);
-        }
-        mNotificationWallpaper.setSummary(getResources().getString(resId));
-
-        value = Settings.System.getString(getContentResolver(),
-                Settings.System.NOTIFICATION_BACKGROUND_LANDSCAPE);
-        if (value == null) {
-            resId = R.string.notification_background_default_wallpaper;
-            mNotificationWallpaperLandscape.setValueIndex(1);
-        } else {
-            resId = R.string.notification_background_custom_image;
-            mNotificationWallpaperLandscape.setValueIndex(0);
-        }
-        mNotificationWallpaperLandscape.setSummary(getResources().getString(resId));
-    }
-
-    public void deleteWallpaper(boolean orientation) {
-        String path = Settings.System.getString(getContentResolver(),
-                Settings.System.NOTIFICATION_BACKGROUND);
-        if (path != null && !path.startsWith("color=")) {
-            File wallpaperToDelete = new File(Uri.parse(path).getPath());
-
-            if (wallpaperToDelete != null
-                    && wallpaperToDelete.exists() && !orientation) {
-                wallpaperToDelete.delete();
-            }
-        }
-
-        path = Settings.System.getString(getContentResolver(),
-                Settings.System.NOTIFICATION_BACKGROUND_LANDSCAPE);
-        if (path != null) {
-            File wallpaperToDelete = new File(Uri.parse(path).getPath());
-
-            if (wallpaperToDelete != null
-                    && wallpaperToDelete.exists() && orientation) {
-                wallpaperToDelete.delete();
-            }
-            if (orientation) {
-                Settings.System.putString(getContentResolver(),
-                    Settings.System.NOTIFICATION_BACKGROUND_LANDSCAPE, null);
-            }
-        }
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == REQUEST_PICK_WALLPAPER
-                    || requestCode == REQUEST_PICK_WALLPAPER_LANDSCAPE) {
-
-                if (mImageTmp.length() == 0 || !mImageTmp.exists()) {
-                    Toast.makeText(mActivity,
-                            getResources().getString(R.string.shortcut_image_not_valid),
-                            Toast.LENGTH_LONG).show();
-                    return;
-                }
-                File image = new File(mActivity.getFilesDir() + File.separator
-                        + "notification_background_" + System.currentTimeMillis() + ".png");
-                String path = image.getAbsolutePath();
-                mImageTmp.renameTo(image);
-                image.setReadable(true, false);
-
-                if (requestCode == REQUEST_PICK_WALLPAPER) {
-                    Settings.System.putString(getContentResolver(),
-                        Settings.System.NOTIFICATION_BACKGROUND, path);
-                } else {
-                    Settings.System.putString(getContentResolver(),
-                        Settings.System.NOTIFICATION_BACKGROUND_LANDSCAPE, path);
-                }
-            }
-        } else {
-            if (mImageTmp.exists()) {
-                mImageTmp.delete();
-            }
-        }
-        updateCustomBackgroundSummary();
-    }
-
-    private void startPictureCrop(int request, boolean landscape) {
-        Display display = getActivity().getWindowManager().getDefaultDisplay();
-        int width = display.getWidth();
-        int height = display.getHeight();
-        Intent intent = new Intent(Intent.ACTION_PICK,
-                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        intent.setType("image/*");
-        intent.putExtra("crop", "true");
-        boolean isPortrait = getResources()
-            .getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
-        intent.putExtra("aspectX", (landscape ? !isPortrait : isPortrait)
-                ? width : height);
-        intent.putExtra("aspectY", (landscape ? !isPortrait : isPortrait)
-                ? height : width);
-        intent.putExtra("outputX", (landscape ? !isPortrait : isPortrait)
-                ? width : height);
-        intent.putExtra("outputY", (landscape ? !isPortrait : isPortrait)
-                ? height : width);
-        intent.putExtra("scale", true);
-        intent.putExtra("scaleUpIfNeeded", true);
-        intent.putExtra("outputFormat", Bitmap.CompressFormat.PNG.toString());
-        try {
-            mImageTmp.createNewFile();
-            mImageTmp.setWritable(true, false);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mImageTmp));
-            startActivityForResult(intent, request);
-        } catch (Exception e) {
-            Log.e(TAG, e.getMessage(), e);
-        }
-    }
-
-    private void showDialogInner(int id) {
-        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
-        newFragment.setTargetFragment(this, 0);
-        newFragment.show(getFragmentManager(), "dialog " + id);
-    }
-
-    public static class MyAlertDialogFragment extends DialogFragment {
-
-        public static MyAlertDialogFragment newInstance(int id) {
-            MyAlertDialogFragment frag = new MyAlertDialogFragment();
-            Bundle args = new Bundle();
-            args.putInt("id", id);
-            frag.setArguments(args);
-            return frag;
-        }
-
-        NotificationDrawerQsSettings getOwner() {
-            return (NotificationDrawerQsSettings) getTargetFragment();
-        }
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            int id = getArguments().getInt("id");
-            switch (id) {
-                case DLG_PICK_COLOR:
-                    final ColorPickerView colorView = new ColorPickerView(getOwner().mActivity);
-                    String currentColor = Settings.System.getString(
-                            getOwner().getContentResolver(),
-                            Settings.System.NOTIFICATION_BACKGROUND);
-                    if (currentColor != null && currentColor.startsWith("color=")) {
-                        int color = Color.parseColor(currentColor.substring("color=".length()));
-                        colorView.setColor(color);
-                    }
-                    colorView.setAlphaSliderVisible(false);
-
-                    return new AlertDialog.Builder(getActivity())
-                    .setTitle(R.string.notification_drawer_custom_background_dialog_title)
-                    .setView(colorView)
-                    .setNegativeButton(R.string.cancel, null)
-                    .setPositiveButton(R.string.dlg_ok,
-                        new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            getOwner().deleteWallpaper(false);
-                            getOwner().deleteWallpaper(true);
-                            Settings.System.putString(
-                                getOwner().getContentResolver(),
-                                Settings.System.NOTIFICATION_BACKGROUND,
-                                "color=" + String.format("#%06X",
-                                (0xFFFFFF & colorView.getColor())));
-                            getOwner().updateCustomBackgroundSummary();
-                        }
-                    })
-                    .create();
-            }
-            throw new IllegalArgumentException("unknown id " + id);
-        }
-
-        @Override
-        public void onCancel(DialogInterface dialog) {
-
-        }
-    }
 }
