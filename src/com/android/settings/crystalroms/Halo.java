401e4cabb3ecebd0fb209fb741b7433ac6f17ccd
zodex <mimirower45@onet.eu>
2014-03-09 10:46:25 +0100

    Some changes

--- src/com/android/settings/crystalroms/Halo.java
+++ src/com/android/settings/crystalroms/Halo.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.paranoid;
+package com.android.settings.crystalroms;
 
 import android.app.ActivityManager;
 import android.app.INotificationManager;
