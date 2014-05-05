26899c1a9ff952e875fe8fe5647fd2ff5047e140
TheMrcool212 <themrcool212.android@gmail.com>
2014-05-05 22:12:47 +0800

    Setup Remix Settings

--- Android.mk
+++ Android.mk
@@ -10,6 +10,13 @@ LOCAL_SRC_FILES := \
         $(call all-java-files-under, src) \
         src/com/android/settings/EventLogTags.logtags
 
+LOCAL_SRC_FILES += $(call all-java-files-under, ../RemixSettings/src)
+
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DIR += packages/apps/RemixSettings/res
+LOCAL_AAPT_FLAGS := --auto-add-overlay \
+	--extra-packages org.aospal.remixsettings
+
 LOCAL_PACKAGE_NAME := Settings
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1137,6 +1137,7 @@ public class Settings extends PreferenceActivity
     public static class PaymentSettingsActivity extends Settings { /* empty */ }
     public static class PrintSettingsActivity extends Settings { /* empty */ }
     public static class PrintJobSettingsActivity extends Settings { /* empty */ }
+    public static class RemixSettingsActivity extends Settings { /* empty */ }
     public static class ASSRamBarActivity extends Settings { /* empty */ }
     public static class BlacklistSettingsActivity extends Settings { /* empty */ }
 }
