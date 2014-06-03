06d649f595e5751beb4805b09aaba9fc5214acc7
TheMrcool212 <themrcool212.android@gmail.com>
2014-06-03 12:31:10 +0800

    Revert "Custom wallpaper: use PNG"
    This reverts commit 2ca7372e63df8debd3d1a7b8769ba551b0f9441f.
    
    Conflicts:
    	src/com/android/settings/AOSPAL/AdditionalSettings.java

--- src/com/android/settings/AOSPAL/LockscreenSettings.java
+++ src/com/android/settings/AOSPAL/LockscreenSettings.java
@@ -192,7 +192,7 @@ public class LockscreenSettings extends SettingsPreferenceFragment implements
             intent.putExtra("crop", "true");
             intent.putExtra("scale", true);
             intent.putExtra("scaleUpIfNeeded", false);
-            intent.putExtra("outputFormat", Bitmap.CompressFormat.PNG.toString());
+            intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
 
             final Display display = getActivity().getWindowManager().getDefaultDisplay();
 
