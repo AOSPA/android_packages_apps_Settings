2bc87b688c651a9d216668fdbbb04e725405fa6b
Andre Saddler <dresadd09691@gmail.com>
2014-07-19 23:58:23 -0400

    Revert "Add Xperia Settings to Legacy parts"
    This reverts commit a67cf790570ef058abb4ae076dc89144d8ce663f.
    
    Change-Id: Ia82f89ca8394e9d3dcf7dc80d2dfccdbe6fa9bc7

--- src/com/android/settings/PALP/GeneralLP.java
+++ src/com/android/settings/PALP/GeneralLP.java
@@ -17,28 +17,16 @@
 package com.android.settings.PALP;
 
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceManager;
-import android.preference.PreferenceScreen;
+import com.android.settings.SettingsPreferenceFragment;
 
 import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
 
 public class GeneralLP extends SettingsPreferenceFragment {
-
-
-   private static final String KEY_XPERIA_SETTINGS = "xperia_settings";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.general_lp_settings);
-
-
-        //XperiaSettings
-        Utils.updatePreferenceToSpecificActivityFromMetaDataOrRemove(getActivity(),
-                getPreferenceScreen(), KEY_XPERIA_SETTINGS);
     }
 }
