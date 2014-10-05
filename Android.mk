6f7514ea43eb01fc6ce1f1e7fc4edf0a0e080cc5
Rammar515 <rammarad515@gmail.com>
2014-10-05 21:06:41 +0200

    Update Android.mk 4

--- Android.mk
+++ Android.mk
@@ -28,8 +28,8 @@ LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res $(LOCAL_PATH)/../../../external/koush/Wi
 LOCAL_JAVA_LIBRARIES += org.cyanogenmod.hardware
 
 LOCAL_SRC_FILES += $(call all-java-files-under, ../PerformanceControl/src)
-LOCAL_RESOURCE_DIR += ../packages/apps/PerformanceControl/res
-LOCAL_ASSET_DIR += ../packages/apps/PerformanceControl/assets
+LOCAL_RESOURCE_DIR += packages/apps/PerformanceControl/res
+LOCAL_ASSET_DIR += packages/apps/PerformanceControl/assets
 LOCAL_AAPT_FLAGS += --extra-packages com.brewcrewfoo.performance
 
 include $(BUILD_PACKAGE)
