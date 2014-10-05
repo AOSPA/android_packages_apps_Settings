3137cdc9d11b0fb7e942fd5ebd31870590870599
Rammar515 <rammarad515@gmail.com>
2014-10-05 20:47:52 +0200

    Update Android.mk 2

--- Android.mk
+++ Android.mk
@@ -23,14 +23,14 @@ LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 LOCAL_AAPT_FLAGS := --extra-packages com.koushikdutta.superuser:com.koushikdutta.widgets --auto-add-overlay
 
 LOCAL_SRC_FILES += $(call all-java-files-under,../../../external/koush/Superuser/Superuser/src) $(call all-java-files-under,../../../external/koush/Widgets/Widgets/src)
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res $(LOCAL_PATH)/../../../external/koush/Widgets/Widgets/res $(LOCAL_PATH)/../../../external/koush/Superuser/Superuser/res
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res $(LOCAL_PATH)/../../../external/koush/Widgets/Widgets/res $(LOCAL_PATH)/../../../external/koush/Superuser/Superuser/res
 
 LOCAL_JAVA_LIBRARIES += org.cyanogenmod.hardware
 
 LOCAL_SRC_FILES += $(call all-java-files-under, ../PerformanceControl/src)
 LOCAL_RESOURCE_DIR += packages/apps/PerformanceControl/res
 LOCAL_ASSET_DIR += packages/apps/PerformanceControl/assets
-LOCAL_AAPT_FLAGS := --extra-packages com.brewcrewfoo.performance
+LOCAL_AAPT_FLAGS += --extra-packages com.brewcrewfoo.performance
 
 include $(BUILD_PACKAGE)
 
