ad06109478b47c5d366f74763b7a1f7188311098
Valter Strods <valterstrods@aospa.co>
2015-03-02 22:06:51 +0200

    Settings: Rewrite App Ops a bit
    This commit is a short rewrite of the App Ops codebase in the Settings
    application with the main goal of improving code readability and
    consistency and all them good things.
    
    Change-Id: Ia3b01bc4ec0283534350ea34975b072fa8b84847

--- src/com/android/settings/applications/AppOpsSummary.java
+++ src/com/android/settings/applications/AppOpsSummary.java
@@ -30,15 +30,7 @@ import android.view.ViewGroup;
 import com.android.settings.R;
 
 public class AppOpsSummary extends Fragment {
-    // layout inflater object used to inflate views
-    private LayoutInflater mInflater;
-    
-    private ViewGroup mContentContainer;
-    private View mRootView;
-    private ViewPager mViewPager;
-
-    CharSequence[] mPageNames;
-    static AppOpsState.OpsTemplate[] sPageTemplates = new AppOpsState.OpsTemplate[] {
+    private final static AppOpsState.OpsTemplate[] PAGE_TEMPLATES = new AppOpsState.OpsTemplate[] {
         AppOpsState.LOCATION_TEMPLATE,
         AppOpsState.PERSONAL_TEMPLATE,
         AppOpsState.MESSAGING_TEMPLATE,
@@ -46,71 +38,53 @@ public class AppOpsSummary extends Fragment {
         AppOpsState.DEVICE_TEMPLATE
     };
 
-    int mCurPos;
+    private CharSequence[] mPageNames = null;
 
-    class MyPagerAdapter extends FragmentPagerAdapter implements ViewPager.OnPageChangeListener {
+    private class AppOpsSummaryAdapter extends FragmentPagerAdapter {
+        private final AppOpsCategory[] mCategories = new AppOpsCategory[PAGE_TEMPLATES.length];
 
-        public MyPagerAdapter(FragmentManager fm) {
+        public AppOpsSummaryAdapter(final FragmentManager fm) {
             super(fm);
+
+            for (int i = 0; i < PAGE_TEMPLATES.length; i++) {
+                mCategories[i] = new AppOpsCategory(PAGE_TEMPLATES[i]);
+            }
         }
 
         @Override
-        public Fragment getItem(int position) {
-            return new AppOpsCategory(sPageTemplates[position]);
+        public Fragment getItem(final int position) {
+            return mCategories[position];
         }
 
         @Override
         public int getCount() {
-            return sPageTemplates.length;
+            return PAGE_TEMPLATES.length;
         }
 
         @Override
-        public CharSequence getPageTitle(int position) {
-            return mPageNames[position];
-        }
-
-        @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-        }
-
-        @Override
-        public void onPageSelected(int position) {
-            mCurPos = position;
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int state) {
-            if (state == ViewPager.SCROLL_STATE_IDLE) {
-                //updateCurrentTab(mCurPos);
-            }
+        public CharSequence getPageTitle(final int position) {
+            return mPageNames == null ? null : mPageNames[position];
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // initialize the inflater
-        mInflater = inflater;
-
-        View rootView = mInflater.inflate(R.layout.app_ops_summary,
-                container, false);
-        mContentContainer = container;
-        mRootView = rootView;
-
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
+            final Bundle savedInstanceState) {
         mPageNames = getResources().getTextArray(R.array.app_ops_categories);
 
-        mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
-        MyPagerAdapter adapter = new MyPagerAdapter(getChildFragmentManager());
-        mViewPager.setAdapter(adapter);
-        mViewPager.setOnPageChangeListener(adapter);
-        PagerTabStrip tabs = (PagerTabStrip) rootView.findViewById(R.id.tabs);
+        final View view = inflater.inflate(R.layout.app_ops_summary, container, false);
+
+        final ViewPager viewPager = (ViewPager) view.findViewById(R.id.pager);
+        viewPager.setAdapter(new AppOpsSummaryAdapter(getChildFragmentManager()));
+
+        final PagerTabStrip tabs = (PagerTabStrip) view.findViewById(R.id.tabs);
         tabs.setTabIndicatorColorResource(R.color.theme_accent);
 
-        // We have to do this now because PreferenceFrameLayout looks at it
-        // only when the view is added.
         if (container instanceof PreferenceFrameLayout) {
-            ((PreferenceFrameLayout.LayoutParams) rootView.getLayoutParams()).removeBorders = true;
+            // We force the PreferenceFrameLayout to look at us by doing this magic.
+            ((PreferenceFrameLayout.LayoutParams) view.getLayoutParams()).removeBorders = true;
         }
 
-        return rootView;
+        return view;
     }
 }
