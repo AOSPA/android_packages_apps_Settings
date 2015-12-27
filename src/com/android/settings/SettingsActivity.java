fc2c98f74a17abb8a703ac9edfd02cc6d5179c52
Carlo Savignano <carlosavignano@aospa.co>
2015-12-27 14:21:19 +0100

    settings: Add buttons customization fragment
    * Add fragment for buttons control.
    * Includes various key codes within customizable double tap and
      long press behaviors (menu, back, home, app switch, assist and
      camera), swap alert slider order, toggle navigation bar with
      hw keys and viceversa on devices with hw navigation keys and
      button backlight.
    
    Change-Id: Id966ec0b3ffea6b2409cb8a006f8efd699821507
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -262,6 +262,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             Settings.Lte4GEnableActivity.class.getName(),
             Settings.WirelessSettingsActivity.class.getName(),
             //device_section
+            Settings.ButtonsSettingsActivity.class.getName(),
             Settings.HomeSettingsActivity.class.getName(),
             Settings.SoundSettingsActivity.class.getName(),
             Settings.DisplaySettingsActivity.class.getName(),
@@ -384,7 +385,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             MasterClear.class.getName(),
             NightDisplaySettings.class.getName(),
             ManageDomainUrls.class.getName(),
-            AutomaticStorageManagerSettings.class.getName()
+            AutomaticStorageManagerSettings.class.getName(),
+            ButtonsSettings.class.getName()
     };
 
 
