e545e3af5661d3bca1e3b050aba555f380e497ff
Willi Ye <williye97@gmail.com>
2016-04-07 17:20:09 +0200

    Add gestures control [4/4]
    * Key contributors
    @grarak
    @carlosavignano
    * Place screen off gesture preferences inside existing system
      gestures activity.
    * Add support for new "S" gesture
    
    Ticket: NOUGAT-17
    
    Change-Id: I86d643e0d6f952b7d81e76090052fdb379d05c18
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: TheStrix <parthbhatia98@gmail.com>
    [TheStrix: Forward-ported to Pie]

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -146,6 +146,7 @@ import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
+import com.android.settings.GesturesSettings;
 
 public class SettingsGateway {
 
@@ -281,7 +282,8 @@ public class SettingsGateway {
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             BatterySaverScheduleSettings.class.getName(),
             MobileNetworkListFragment.class.getName(),
-            GlobalActionsPanelSettings.class.getName()
+            GlobalActionsPanelSettings.class.getName(),
+            GesturesSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
