14ddb7bdcf1b6274a417c729e26718578965d164
Willi Ye <williye97@gmail.com>
2016-04-07 17:20:09 +0200

    Add gestures control [4/4]
    * Key contributors
    @grarak
    @carlosavignano
    * Place screen off gesture preferences inside existing system
      gestures activity.
    
    Ticket: NOUGAT-17
    
    Change-Id: I86d643e0d6f952b7d81e76090052fdb379d05c18
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: TheStrix <parthbhatia98@gmail.com>
    [TheStrix: Forward-ported to Pie]

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -32,7 +32,6 @@ import com.android.settings.display.NightDisplayPreferenceController;
 import com.android.settings.display.NightModePreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
-import com.android.settings.display.TapToWakePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.TimeoutPreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
@@ -93,7 +92,6 @@ public class DisplaySettings extends DashboardFragment {
                 context,
                 new AmbientDisplayConfiguration(context),
                 KEY_AMBIENT_DISPLAY));
-        controllers.add(new TapToWakePreferenceController(context));
         controllers.add(new TimeoutPreferenceController(context, KEY_SCREEN_TIMEOUT));
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new ShowOperatorNamePreferenceController(context));
