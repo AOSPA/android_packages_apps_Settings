088440a1b7336600f367f2e9f064a98a72e50a3a
guangl <guangl@codeaurora.org>
2016-09-22 15:52:07 +0800

    Settings: Fix settings force close in monkey test.
    accessPreference.mState null pointer causes settings force close in
    monkey test.
    
    Add null pointer protect.
    
    Change-Id: Ia866ac02c8d75308d3ff99f1400f11318f898621
    CRs-Fixed: 1069229

--- src/com/android/settings/datausage/UnrestrictedDataAccess.java
+++ src/com/android/settings/datausage/UnrestrictedDataAccess.java
@@ -215,6 +215,9 @@ public class UnrestrictedDataAccess extends SettingsPreferenceFragment
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference instanceof AccessPreference) {
             AccessPreference accessPreference = (AccessPreference) preference;
+            if (accessPreference.mState == null) {
+                return false;
+            }
             boolean whitelisted = newValue == Boolean.TRUE;
             logSpecialPermissionChange(whitelisted, accessPreference.mEntry.info.packageName);
             mDataSaverBackend.setIsWhitelisted(accessPreference.mEntry.info.uid,
