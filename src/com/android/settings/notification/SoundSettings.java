dd59576edc222514048b20e8b938d6898800701c
thecrazyskull <anaskarbila@aospa.co>
2016-11-23 13:45:00 +0200

    Settings: Forward Port Volume Long Press Skip Tracks (2/2)
    Bring Forward Volume Rocker Track Change
    
    Change-Id: Ifa3d0987a0d54afd1f56e1d4a7488f428338c757

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -84,6 +84,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
     private static final String KEY_WIFI_DISPLAY = "wifi_display";
     private static final String KEY_ZEN_MODE = "zen_mode";
     private static final String KEY_CELL_BROADCAST_SETTINGS = "cell_broadcast_settings";
+    private static final String KEY_VOLBT_MUSIC_CONTROL = "volbt_music_controls";
 
     private static final String SELECTED_PREFERENCE_KEY = "selected_preference";
     private static final int REQUEST_CODE = 200;
@@ -114,6 +115,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
     private Preference mNotificationRingtonePreference;
     private Preference mAlarmRingtonePreference;
     private TwoStatePreference mVibrateWhenRinging;
+    private TwoStatePreference mVlbtnMusicControl;
     private ComponentName mSuppressor;
     private int mRingerMode = -1;
 
@@ -177,6 +179,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
             removePreference(KEY_CELL_BROADCAST_SETTINGS);
         }
         initRingtones();
+        initVolumeButtonMusicControl();
         initVibrateWhenRinging();
         updateRingerMode();
         updateEffectsSuppressor();
@@ -449,6 +452,25 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
         return summary;
     }
 
+    private void initVolumeButtonMusicControl() {
+        mVlbtnMusicControl = (TwoStatePreference) getPreferenceScreen().findPreference(
+                KEY_VOLBT_MUSIC_CONTROL);
+        if (mVlbtnMusicControl == null) {
+            Log.i(TAG, "Preference not found: " + KEY_VOLBT_MUSIC_CONTROL);
+            return;
+        }
+        mVlbtnMusicControl.setPersistent(false);
+        updateVolumeButtonMusicControl();
+        mVlbtnMusicControl.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                final boolean val = (Boolean) newValue;
+                return Settings.System.putInt(getContentResolver(),
+                        Settings.System.VOLBTN_MUSIC_CONTROLS, val ? 1 : 0);
+            }
+        });
+    }
+
     // === Vibrate when ringing ===
 
     private void initVibrateWhenRinging() {
@@ -476,6 +498,12 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
         });
     }
 
+    private void updateVolumeButtonMusicControl() {
+        if (mVlbtnMusicControl == null) return;
+        mVlbtnMusicControl.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.VOLBTN_MUSIC_CONTROLS, 1) != 0);
+    }
+
     private void updateVibrateWhenRinging() {
         if (mVibrateWhenRinging == null) return;
         mVibrateWhenRinging.setChecked(Settings.System.getInt(getContentResolver(),
@@ -487,6 +515,8 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
     private final class SettingsObserver extends ContentObserver {
         private final Uri VIBRATE_WHEN_RINGING_URI =
                 Settings.System.getUriFor(Settings.System.VIBRATE_WHEN_RINGING);
+        private final Uri VOLBTN_MUSIC_CONTROL_URI =
+                Settings.System.getUriFor(Settings.System.VOLBTN_MUSIC_CONTROLS);
 
         public SettingsObserver() {
             super(mHandler);
@@ -506,6 +536,8 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
             super.onChange(selfChange, uri);
             if (VIBRATE_WHEN_RINGING_URI.equals(uri)) {
                 updateVibrateWhenRinging();
+            } else if (VOLBTN_MUSIC_CONTROL_URI.equals(uri)) {
+                updateVolumeButtonMusicControl();
             }
         }
     }
