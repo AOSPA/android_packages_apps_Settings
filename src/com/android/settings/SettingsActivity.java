0df639490095434e9b05dad313864269d3155714
Thecrazyskull <anaskarbila@aospa.co>
2016-12-28 08:03:25 -0600

    Settings: default to qualcomm simsettings if it exists
    * Fallback onto AOSP setting if it doesn't
    
    Change-Id: I7959de49eb66fe12577c3253206a2825745f81ad

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1053,11 +1053,32 @@ public class SettingsActivity extends SettingsDrawerActivity
         transaction.commitAllowingStateLoss();
     }
 
+    private boolean packageExists(String packageName) {
+        final PackageManager pm = getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (NameNotFoundException e) {
+        }
+        return false;
+    }
+
     /**
      * Switch to a specific Fragment with taking care of validation, Title and BackStack
      */
     private Fragment switchToFragment(String fragmentName, Bundle args, boolean validate,
             boolean addToBackStack, int titleResId, CharSequence title, boolean withTransition) {
+
+        final String simSettingsPackage = "com.qualcomm.qti.simsettings";
+        if (packageExists(simSettingsPackage)
+                    && fragmentName.equals(SimSettings.class.getName())) {
+            Intent intent = new Intent("com.android.settings.sim.SIM_SUB_INFO_SETTINGS");
+            intent.setPackage(simSettingsPackage);
+            startActivity(intent);
+            finish();
+            return null;
+        }
+
         if (LTE_4G_FRAGMENT.equals(fragmentName)) {
             Intent newIntent = new Intent("android.settings.SETTINGS");
             newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
