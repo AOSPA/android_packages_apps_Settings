da29d6da406f829665d3368c97710a5ce70de528
ezio84 <brabus84@gmail.com>
2016-12-31 21:41:13 -0500

    Night Light brightness mode options [2/2]
    Squash of:
    Move Night light brightness options to Settings [2/2]
    Night tile: automatically set a lower brightness when active [2/2]
    Night mode tile brightness: allow to set a different value [2/2]
    Wider night light auto low brightness action [2/2]
    
    BeansTown106 Edits:
    *cleaned up java a bit remove unused imports and moved int defines
    *followed googles xml based coding and added a category
    *changed from a list preference to dropdown preference
    *added a wall of text preference describing the modification
    *removed references to qs from strings and slimmed down text a bit
    
    Change-Id: I22180ab47dde0c75878319d3317475cc80b809c6

--- src/com/android/settings/display/NightDisplaySettings.java
+++ src/com/android/settings/display/NightDisplaySettings.java
@@ -24,6 +24,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.provider.Settings.Secure;
 import android.support.v7.preference.DropDownPreference;
 import android.support.v7.preference.Preference;
@@ -49,6 +50,7 @@ public class NightDisplaySettings extends SettingsPreferenceFragment
     private static final String KEY_NIGHT_DISPLAY_START_TIME = "night_display_start_time";
     private static final String KEY_NIGHT_DISPLAY_END_TIME = "night_display_end_time";
     private static final String KEY_NIGHT_DISPLAY_ACTIVATED = "night_display_activated";
+    private static final String QS_NIGHT_BRIGHTNESS_VALUE = "qs_night_brightness_value";
 
     private static final String SETTING_WARNING_HIDDEN = "night_display_warning_hidden";
     private static final int WARNING_SHOW = 0;
@@ -64,6 +66,7 @@ public class NightDisplaySettings extends SettingsPreferenceFragment
     private Preference mStartTimePreference;
     private Preference mEndTimePreference;
     private TwoStatePreference mActivatedPreference;
+    private DropDownPreference mNightBrightValue;
 
     private boolean mUseHwc2;
 
@@ -79,6 +82,13 @@ public class NightDisplaySettings extends SettingsPreferenceFragment
 
         mUseHwc2 = context.getResources().getBoolean(
                 com.android.internal.R.bool.config_enableHWC2);
+
+        mNightBrightValue = (DropDownPreference) findPreference(QS_NIGHT_BRIGHTNESS_VALUE);
+        int nightBrightValue = Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.QS_NIGHT_BRIGHTNESS_VALUE, 0);
+        mNightBrightValue.setValue(Integer.toString(nightBrightValue));
+        mNightBrightValue.setSummary(mNightBrightValue.getEntry());
+        mNightBrightValue.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -216,6 +226,14 @@ public class NightDisplaySettings extends SettingsPreferenceFragment
             boolean activated = (Boolean) newValue;
             displayWarning = activated;
             result = mController.setActivated(activated);
+            return mController.setActivated((Boolean) newValue);
+        } else if (preference == mNightBrightValue) {
+            int nightBrightValue = Integer.valueOf((String) newValue);
+            int index = mNightBrightValue.findIndexOfValue((String) newValue);
+            mNightBrightValue.setSummary(mNightBrightValue.getEntries()[index]);
+            Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.QS_NIGHT_BRIGHTNESS_VALUE, nightBrightValue);
+            return true;
         }
 
         if (displayWarning && !mUseHwc2) {
