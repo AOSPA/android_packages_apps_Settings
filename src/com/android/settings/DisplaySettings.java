a78c1ab909f73575e2326dba1ab3f88729c5d8b5
Thecrazyskull <anaskarbila@aospa.co>
2017-01-07 08:41:42 -0600

    Add support sRGB toggle to display settings.
    * Adapted to Oreo by @Herna1994
    
    Change-Id: Ifdce3cedeff680beedcb7efe30fc9b573a86032a

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -29,6 +29,7 @@ import com.android.settings.display.PocketJudgePreferenceController;
 import com.android.settings.display.NightDisplayPreferenceController;
 import com.android.settings.display.NightModePreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
+import com.android.settings.display.SRGBModePreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.TimeoutPreferenceController;
@@ -92,6 +93,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
+        controllers.add(new SRGBModePreferenceController(context));
         return controllers;
     }
 
