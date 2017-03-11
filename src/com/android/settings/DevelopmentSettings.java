3e1245ac301d38ac710b5b4a8b58a33ca2dc0866
Alex Naidis <alex.naidis@linux.com>
2017-03-11 15:30:30 +0100

    DevelopmentSettings: Update animation default
    * To 0.8
    
    Change-Id: I73de500b584c2995a9237311ce9c12720af4073e
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -1763,7 +1763,7 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
 
     private void writeAnimationScaleOption(int which, ListPreference pref, Object newValue) {
         try {
-            float scale = newValue != null ? Float.parseFloat(newValue.toString()) : 1;
+            float scale = newValue != null ? Float.parseFloat(newValue.toString()) : 0.8f;
             mWindowManager.setAnimationScale(which, scale);
             updateAnimationScaleValue(which, pref);
         } catch (RemoteException e) {
