ae70dbb6864d53f96a44f660abf3e005bd07f279
Jake Weinstein <xboxlover360@gmail.com>
2017-04-25 00:01:21 -0400

    Revert "Adding QtiFeedback menu settings"
    This reverts commit bb63ed7016e97ac99c17ad95e81d2e5eb4d15167.
    
    Change-Id: I9994f8f9b390d8f07ee71137e211c6e2b5e8add8

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -247,7 +247,6 @@ public class SettingsActivity extends SettingsDrawerActivity
 
     private CharSequence mInitialTitle;
     private int mInitialTitleResId;
-    private SmqSettings mSMQ;
 
     // Show only these settings for restricted users
     private String[] SETTINGS_FOR_RESTRICTED = {
@@ -573,8 +572,6 @@ public class SettingsActivity extends SettingsDrawerActivity
         if (intent.getBooleanExtra(EXTRA_HIDE_DRAWER, false)) {
             setIsDrawerPresent(false);
         }
-
-        mSMQ = new SmqSettings(getApplicationContext());
 
         mDevelopmentPreferences = getSharedPreferences(DevelopmentSettings.PREF_FILE,
                 Context.MODE_PRIVATE);
@@ -1061,15 +1058,6 @@ public class SettingsActivity extends SettingsDrawerActivity
      */
     private Fragment switchToFragment(String fragmentName, Bundle args, boolean validate,
             boolean addToBackStack, int titleResId, CharSequence title, boolean withTransition) {
-
-        if (fragmentName.equals(getString(R.string.qtifeedback_intent_action))){
-             final Intent newIntent = new Intent(getString(R.string.qtifeedback_intent_action));
-             newIntent.addCategory("android.intent.category.DEFAULT");
-             startActivity(newIntent);
-             finish();
-             return null;
-        }
-
         if (fragmentName.equals(SimSettings.class.getName())){
             Log.i(LOG_TAG, "switchToFragment, launch simSettings  ");
             Intent provisioningIntent =
@@ -1111,6 +1099,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             SystemUpdateHandle ();
             return null;
         }
+
 
         if (validate && !isValidFragment(fragmentName)) {
             throw new IllegalArgumentException("Invalid fragment for this activity: "
@@ -1185,11 +1174,6 @@ public class SettingsActivity extends SettingsDrawerActivity
         final boolean isAdmin = um.isAdminUser();
 
         String packageName = getPackageName();
-        if(mSMQ.isShowSmqSettings()){
-            setTileEnabled(new ComponentName(packageName, Settings.SMQQtiFeedbackActivity.class.getName()),
-                mSMQ.isShowSmqSettings(), isAdmin, pm);
-        }
-
         setTileEnabled(new ComponentName(packageName, WifiSettingsActivity.class.getName()),
                 pm.hasSystemFeature(PackageManager.FEATURE_WIFI), isAdmin, pm);
 
