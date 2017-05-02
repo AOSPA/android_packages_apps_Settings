b3f69716712ae3092c62ff48e4d9032ac9732b6a
Anas Karbila <anaskarbila@gmail.com>
2017-05-03 04:37:15 +0900

    Settings: import AOSP gestures into our gestures settings
    Change-Id: Ie884db45ed56d68b72d360b6aeef3bcc12f45ffe

--- src/com/android/settings/GesturesSettings.java
+++ src/com/android/settings/GesturesSettings.java
@@ -17,14 +17,27 @@
 package com.android.settings;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Switch;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.android.internal.hardware.AmbientDisplayConfiguration;
+
+import com.android.settings.gestures.GesturePreference;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settings.widget.SwitchBar;
@@ -39,6 +52,8 @@ public class GesturesSettings extends SettingsPreferenceFragment implements
 
     private static final String TAG = "GesturesSettings";
 
+    private static final String KEY_OFFSCREEN_GESTURES = "offscreen_gestures";
+    private static final String KEY_SYSTEM_GESTURES = "system_gestures";
     private static final String KEY_DOUBLE_TAP = "double_tap";
     private static final String KEY_DRAW_V = "draw_v";
     private static final String KEY_DRAW_INVERSE_V = "draw_inverse_v";
@@ -52,6 +67,20 @@ public class GesturesSettings extends SettingsPreferenceFragment implements
     private static final String KEY_ONE_FINGER_SWIPE_DOWN = "one_finger_swipe_down";
     private static final String KEY_ONE_FINGER_SWIPE_LEFT = "one_finger_swipe_left";
     private static final String KEY_TWO_FINGER_SWIPE = "two_finger_swipe";
+
+    private static final String KEY_DOUBLE_TAP_POWER = "gesture_double_tap_power";
+    private static final String KEY_DOUBLE_TWIST = "gesture_double_twist";
+    private static final String KEY_PICK_UP = "gesture_pick_up";
+    private static final String KEY_SWIPE_DOWN_FINGERPRINT = "gesture_swipe_down_fingerprint";
+    private static final String KEY_DOUBLE_TAP_SCREEN = "gesture_double_tap_screen";
+    private static final String DEBUG_DOZE_COMPONENT = "debug.doze.component";
+
+    private List<GesturePreference> mPreferences;
+
+    private AmbientDisplayConfiguration mAmbientConfig;
+
+    private PreferenceCategory mOnScreenGestures;
+    private PreferenceCategory mSystemGestures;
 
     private static final HashMap<String, Integer> mGesturesKeyCodes = new HashMap<>();
     private static final HashMap<String, Integer> mGesturesDefaults = new HashMap();
@@ -117,13 +146,86 @@ public class GesturesSettings extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.gestures_settings);
 
+        Context context = getActivity();
+
+        mOnScreenGestures = (PreferenceCategory) findPreference(KEY_OFFSCREEN_GESTURES);
+        mSystemGestures = (PreferenceCategory) findPreference(KEY_SYSTEM_GESTURES);
+
+        mPreferences = new ArrayList();
+
+        // Double tap power for camera
+        if (isCameraDoubleTapPowerGestureAvailable(getResources())) {
+            int cameraDisabled = Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED, 0);
+            addPreference(KEY_DOUBLE_TAP_POWER, cameraDisabled == 0);
+        } else {
+            mSystemGestures.removePreference(findPreference(KEY_DOUBLE_TAP_POWER));
+        }
+
+        // Ambient Display
+        mAmbientConfig = new AmbientDisplayConfiguration(context);
+        if (mAmbientConfig.pulseOnPickupAvailable()) {
+            boolean pickup = mAmbientConfig.pulseOnPickupEnabled(UserHandle.myUserId());
+            addPreference(KEY_PICK_UP, pickup);
+        } else {
+            mSystemGestures.removePreference(findPreference(KEY_PICK_UP));
+        }
+        if (mAmbientConfig.pulseOnDoubleTapAvailable()) {
+            boolean doubleTap = mAmbientConfig.pulseOnDoubleTapEnabled(UserHandle.myUserId());
+            addPreference(KEY_DOUBLE_TAP_SCREEN, doubleTap);
+        } else {
+            mSystemGestures.removePreference(findPreference(KEY_DOUBLE_TAP_SCREEN));
+        }
+
+        // Fingerprint slide for notifications
+        if (isSystemUINavigationAvailable(context)) {
+            addPreference(KEY_SWIPE_DOWN_FINGERPRINT, isSystemUINavigationEnabled(context));
+        } else {
+            mSystemGestures.removePreference(findPreference(KEY_SWIPE_DOWN_FINGERPRINT));
+        }
+
+        // Double twist for camera mode
+        if (isDoubleTwistAvailable(context)) {
+            int doubleTwistEnabled = Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.CAMERA_DOUBLE_TWIST_TO_FLIP_ENABLED, 1);
+            addPreference(KEY_DOUBLE_TWIST, doubleTwistEnabled != 0);
+        } else {
+            mSystemGestures.removePreference(findPreference(KEY_DOUBLE_TWIST));
+        }
+
         for (String gestureKey : mGesturesKeyCodes.keySet()) {
             if (getResources().getInteger(mGesturesKeyCodes.get(gestureKey)) > 0) {
                 findPreference(gestureKey).setOnPreferenceChangeListener(this);
             } else {
-                removePreference(gestureKey);
+                mOnScreenGestures.removePreference(findPreference(gestureKey));
             }
         }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        RecyclerView listview = getListView();
+        listview.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                if (newState == RecyclerView.SCROLL_STATE_DRAGGING) {
+                    for (GesturePreference pref : mPreferences) {
+                        pref.setScrolling(true);
+                    }
+                } else if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    for (GesturePreference pref : mPreferences) {
+                        pref.setScrolling(false);
+                    }
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            }
+        });
+        return view;
     }
 
     @Override
@@ -139,8 +241,29 @@ public class GesturesSettings extends SettingsPreferenceFragment implements
     public void onStart() {
         super.onStart();
 
-        SettingsActivity activity = (SettingsActivity) getActivity();
-        mGesturesEnabler = new GesturesEnabler(activity.getSwitchBar());
+        for (GesturePreference preference : mPreferences) {
+            preference.onViewVisible();
+        }
+
+        if (mGesturesKeyCodes.keySet().stream().allMatch(keyCode -> getResources().getInteger(
+                mGesturesKeyCodes.get(keyCode)) > 0)) {
+            SettingsActivity activity = (SettingsActivity) getActivity();
+            mGesturesEnabler = new GesturesEnabler(activity.getSwitchBar());
+        } else {
+            getPreferenceScreen().removePreference(mOnScreenGestures);
+        }
+
+        if (mPreferences.isEmpty()) {
+            getPreferenceScreen().removePreference(mSystemGestures);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        for (GesturePreference preference : mPreferences) {
+            preference.onViewInvisible();
+        }
     }
 
     @Override
@@ -178,11 +301,34 @@ public class GesturesSettings extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        Settings.System.putInt(getContentResolver(),
-                mGesturesSettings.get(preference.getKey()),
-                Integer.parseInt((String) newValue));
+        String key = preference.getKey();
+        if (KEY_DOUBLE_TAP_POWER.equals(key)) {
+            boolean enabled = (boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED, enabled ? 0 : 1);
+        } else if (KEY_PICK_UP.equals(key)) {
+            boolean enabled = (boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(), Settings.Secure.DOZE_PULSE_ON_PICK_UP,
+                    enabled ? 1 : 0);
+        } else if (KEY_DOUBLE_TAP_SCREEN.equals(key)) {
+            boolean enabled = (boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(), Settings.Secure.DOZE_PULSE_ON_DOUBLE_TAP,
+                    enabled ? 1 : 0);
+        } else if (KEY_SWIPE_DOWN_FINGERPRINT.equals(key)) {
+            boolean enabled = (boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.SYSTEM_NAVIGATION_KEYS_ENABLED, enabled ? 1 : 0);
+        } else if (KEY_DOUBLE_TWIST.equals(key)) {
+            boolean enabled = (boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.CAMERA_DOUBLE_TWIST_TO_FLIP_ENABLED, enabled ? 1 : 0);
+        } else if (mGesturesSettings.containsKey(key)) {
+            Settings.System.putInt(getContentResolver(), mGesturesSettings.get(key),
+                    Integer.parseInt((String) newValue));
+        }
         return true;
     }
+
 
     public static boolean supportsGestures(Context context) {
         for (String gestureKey : mGesturesKeyCodes.keySet()) {
@@ -192,6 +338,49 @@ public class GesturesSettings extends SettingsPreferenceFragment implements
             }
         }
         return false;
+    }
+
+    private static boolean isCameraDoubleTapPowerGestureAvailable(Resources res) {
+        return res.getBoolean(
+                com.android.internal.R.bool.config_cameraDoubleTapPowerGestureEnabled);
+    }
+
+    private static boolean isSystemUINavigationAvailable(Context context) {
+        return context.getResources().getBoolean(
+                com.android.internal.R.bool.config_supportSystemNavigationKeys);
+    }
+
+    private static boolean isSystemUINavigationEnabled(Context context) {
+        return Settings.Secure.getInt(context.getContentResolver(),
+                Settings.Secure.SYSTEM_NAVIGATION_KEYS_ENABLED, 0) == 1;
+    }
+
+    private static boolean isDoubleTwistAvailable(Context context) {
+        return hasSensor(context, R.string.gesture_double_twist_sensor_name,
+                R.string.gesture_double_twist_sensor_vendor);
+    }
+
+    private static boolean hasSensor(Context context, int nameResId, int vendorResId) {
+        Resources resources = context.getResources();
+        String name = resources.getString(nameResId);
+        String vendor = resources.getString(vendorResId);
+        if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(vendor)) {
+            SensorManager sensorManager =
+                    (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+            for (Sensor s : sensorManager.getSensorList(Sensor.TYPE_ALL)) {
+                if (name.equals(s.getName()) && vendor.equals(s.getVendor())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void addPreference(String key, boolean enabled) {
+        GesturePreference preference = (GesturePreference) findPreference(key);
+        preference.setChecked(enabled);
+        preference.setOnPreferenceChangeListener(this);
+        mPreferences.add(preference);
     }
 
     private class GesturesEnabler implements SwitchBar.OnSwitchChangeListener {
