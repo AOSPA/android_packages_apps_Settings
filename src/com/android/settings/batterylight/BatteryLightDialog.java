74e27b798086754bc34b23bafb8ec97c9ae81baf
Chris Lahaye <dev@chrislahaye.com>
2017-06-10 19:34:11 +0200

    lights: Clean Battery Light and add color LED preview
    * Moves logic to a dedicated BatteryLightDialog class
    * Cleans BatteryLightPreference class code
    * Handles ARGB and RGB explicitly and consistently. For hardware
      we use RGB and for UI elements ARGB with 0xFF as alpha.
    * Handles dialog instance explicitly to ensure only one dialog
      exists at a time
    * Replaces "0xFF000000 + color" by "0xFF000000 | color" which
      allows color to wrongly contain an ARGB value
      instead of RGB, which happens for Android's default settings.
    * Adds selected color LED preview
    
    Change-Id: Ie8cb0ec70f94186ba27c017c62a64d4867e4bb8a
    Signed-off-by: Chris Lahaye <dev@chrislahaye.com>

---
+++ src/com/android/settings/batterylight/BatteryLightDialog.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (C) 2015-2017 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.batterylight;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.android.settings.R;
+import com.android.settingslib.ColorPickerDialog;
+import com.android.settingslib.ColorPickerDialogAdapter;
+
+public class BatteryLightDialog extends ColorPickerDialog
+        implements ColorPickerDialog.OnColorSelectedListener {
+
+    private ColorPickerDialogAdapter mAdapter;
+    private Context mContext;
+    private NotificationManager mNotificationManager;
+
+    public BatteryLightDialog(Context context) {
+        super(context);
+
+        final int[] colors = context.getResources().getIntArray(
+                R.array.led_color_picker_dialog_colors);
+
+        mAdapter = getAdapter();
+        mAdapter.setColors(colors);
+        mAdapter.setSelectedImageResourceId(R.drawable.ic_check_green_24dp);
+        mAdapter.setSelectedImageColorFilter(Color.WHITE);
+
+        mContext = context;
+        mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setOnColorSelectedListener(this);
+    }
+
+    public void onColorSelected(DialogInterface dialog, int color) {
+        mAdapter.setSelectedImageColorFilter(
+                mAdapter.getSelectedPosition() == 0 ? Color.DKGRAY : Color.WHITE);
+
+        onStart();
+    }
+
+    @Override
+    public void onStart() {
+        final Bundle b = new Bundle();
+        b.putBoolean(Notification.EXTRA_FORCE_SHOW_LIGHTS, true);
+
+        final Notification.Builder builder = new Notification.Builder(mContext);
+        builder.setLights(getSelectedColor(), 1, 0);
+        builder.setExtras(b);
+
+        builder.setSmallIcon(R.drawable.ic_settings_leds);
+        builder.setContentTitle(getContext().getString(R.string.led_notification_title));
+        builder.setContentText(getContext().getString(R.string.led_notification_text));
+        builder.setOngoing(true);
+
+        mNotificationManager.notify(1, builder.build());
+    }
+
+    @Override
+    public void onStop() {
+        mNotificationManager.cancel(1);
+    }
+}
