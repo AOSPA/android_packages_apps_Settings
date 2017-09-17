ef58c375ff3c4a35a2635afe40ff4d228b9fcc51
Hernán Castañón <herna@aospa.co>
2017-09-18 00:56:21 +0200

    Settings: delete duplicated cast option
    It's okey that there is sound cast devices, but this Cast option
    is fully centered on WiFi display, casting device screen.
    
    As we already have this option under Display this is too redundant.
    
    Change-Id: I87a83285012cef7beeda057548a08bbc072c153b
    Signed-off-by: Hernán Castañón <herna@aospa.co>

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -81,7 +81,6 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
     private static final String KEY_NOTIFICATION_RINGTONE = "notification_ringtone";
     private static final String KEY_ALARM_RINGTONE = "alarm_ringtone";
     private static final String KEY_VIBRATE_WHEN_RINGING = "vibrate_when_ringing";
-    private static final String KEY_WIFI_DISPLAY = "wifi_display";
     private static final String KEY_ZEN_MODE = "zen_mode";
     private static final String KEY_CELL_BROADCAST_SETTINGS = "cell_broadcast_settings";
     private static final String KEY_VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
@@ -688,7 +687,6 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
             } else {
                 rt.add(KEY_RING_VOLUME);
                 rt.add(KEY_PHONE_RINGTONE);
-                rt.add(KEY_WIFI_DISPLAY);
                 rt.add(KEY_VIBRATE_WHEN_RINGING);
             }
 
