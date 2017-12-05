839e621b3f9567345fed2615d3abfb098ccac076
Sandeep Gutta <sangutta@codeaurora.org>
2017-12-05 12:54:46 +0530

    Telephony(MSIM): Add vendor SimSettings apk condition check
    Launch vendor SimSettings activity only when the corresponding apk exists on the device
    
    Change-Id: I49d2109eea2546d14eda96c9e3f50f4c0859e319
    CRs-Fixed: 2153740

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -35,6 +35,8 @@ import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.support.annotation.VisibleForTesting;
@@ -68,6 +70,8 @@ import com.android.settingslib.drawer.SettingsDrawerActivity;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+
+import org.codeaurora.internal.IExtTelephony;
 
 public class SettingsActivity extends SettingsDrawerActivity
         implements PreferenceManager.OnPreferenceTreeClickListener,
@@ -727,15 +731,24 @@ public class SettingsActivity extends SettingsDrawerActivity
              return null;
         }
 
-        if (fragmentName.equals(SimSettings.class.getName())){
-            Log.i(LOG_TAG, "switchToFragment, launch simSettings  ");
-            Intent provisioningIntent =
-                    new Intent("com.android.settings.sim.SIM_SUB_INFO_SETTINGS");
-            if (!getPackageManager().queryIntentActivities(provisioningIntent, 0).isEmpty()) {
-                startActivity(provisioningIntent);
+        IExtTelephony extTelephony =
+                IExtTelephony.Stub.asInterface(ServiceManager.getService("extphone"));
+        try {
+            if (fragmentName.equals(SimSettings.class.getName()) && extTelephony != null &&
+                    extTelephony.isVendorApkAvailable("com.qualcomm.qti.simsettings")) {
+                Log.i(LOG_TAG, "switchToFragment, launch simSettings  ");
+                Intent provisioningIntent =
+                        new Intent("com.android.settings.sim.SIM_SUB_INFO_SETTINGS");
+                if (!getPackageManager().queryIntentActivities(provisioningIntent, 0).isEmpty()) {
+                    startActivity(provisioningIntent);
+                }
+                finish();
+                return null;
             }
-            finish();
-            return null;
+        } catch (RemoteException e) {
+            // could not connect to extphone service, launch the default activity
+            Log.i(LOG_TAG,
+                    "couldn't connect to extphone service, launch the default sim cards activity");
         }
 
         if (validate && !isValidFragment(fragmentName)) {
