d729351f69c82daf5001bd163d0050efabc4a905
Bruno Martins <bgcngm@gmail.com>
2018-01-15 08:44:46 +0000

    Settings: Add switch for linked ring and media notification volumes
    [mickaelmendes50]
     - Use AOSP Setting Pref. Controller for linker Switch
       instead of Custom Setting Switch Preference.
     - Update Ring Volume Seekbar string.
     - Update Notification Volume Seekbar icon and move it
       above Alarm Volume Seekbar
    
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Wed Oct 30 09:22:30 2019 -0500
    
        VolumePanel: fix showing notification volume slider when unlinked
    
        * volume_link_notification lives in Settings.Secure not LineageSettings.Secure
        * Set max panel slices to 6. We could have up to 5 volumes to control and
          MEDIA_OUTPUT_INDICATOR_SLICE_URI counts as one slice as well.
    
        Change-Id: I0a4c7db1c92e778fba669931259cd52164b1df65
    
    Settings: Use correct icon for ring volume
    
    Author: Han Wang <416810799@qq.com>
    Date:   Tue Aug 4 13:33:45 2020 +0200
    
        Settings: Fix icon size for ring volume
    
        Change-Id: Idc62abbaea8d4d4ccb72f4a9f395ce0f861dc705
    
        Change-Id: If145262562c1409cd8a6cb79ba46b6646c75d1a8
        Signed-off-by: PMS22 <prathams99@rediff.com>
    
    Change-Id: I900af398f566a5415c44fe883cc214985f4e9cd4
    Co-authored-by: Mesquita <mesquita@aospa.co>

--- src/com/android/settings/notification/NotificationVolumePreferenceController.java
+++ src/com/android/settings/notification/NotificationVolumePreferenceController.java
@@ -35,8 +35,7 @@ public class NotificationVolumePreferenceController extends
     @Override
     public int getAvailabilityStatus() {
         return mContext.getResources().getBoolean(R.bool.config_show_notification_volume)
-                && !Utils.isVoiceCapable(mContext) && !mHelper.isSingleVolume()
-                ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+                && !mHelper.isSingleVolume() ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 
     @Override
@@ -61,7 +60,21 @@ public class NotificationVolumePreferenceController extends
 
     @Override
     public int getMuteIcon() {
-        return R.drawable.ic_notifications_off_24dp;
+        return R.drawable.ic_audio_notifications_off_24dp;
     }
 
+    @Override
+    protected void updatePreferenceIcon() {
+        if (mPreference != null) {
+            if (mRingerMode == AudioManager.RINGER_MODE_VIBRATE) {
+                mMuteIcon = R.drawable.ic_volume_ringer_vibrate;
+                mPreference.showIcon(R.drawable.ic_volume_ringer_vibrate);
+            } else if (mRingerMode == AudioManager.RINGER_MODE_SILENT) {
+                mMuteIcon = R.drawable.ic_notifications_off_24dp;
+                mPreference.showIcon(R.drawable.ic_audio_notifications_off_24dp);
+            } else {
+                mPreference.showIcon(R.drawable.ic_audio_notifications);
+            }
+        }
+    }
 }
