b038681eafe380e758bc6fda827aba8f24740f20
maxwen <max.weninger@gmail.com>
2018-10-28 15:34:32 +0100

    Settings: Add back selinux status in about
    Change-Id: Ieaa32a8bc1a8eb12621ecb0509cd56e9e0d7d48d

--- src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
+++ src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -37,6 +37,7 @@ import com.android.settings.deviceinfo.FeedbackPreferenceController;
 import com.android.settings.deviceinfo.IpAddressPreferenceController;
 import com.android.settings.deviceinfo.ManualPreferenceController;
 import com.android.settings.deviceinfo.RegulatoryInfoPreferenceController;
+import com.android.settings.deviceinfo.SELinuxStatusPreferenceController;
 import com.android.settings.deviceinfo.SafetyInfoPreferenceController;
 import com.android.settings.deviceinfo.SoftwareVersionPreferenceController;
 import com.android.settings.deviceinfo.StorageSizePreferenceController;
@@ -119,6 +120,7 @@ public class MyDeviceInfoFragment extends DashboardFragment
         controllers.add(new UptimePreferenceController(context, lifecycle));
         controllers.add(new SoftwareVersionPreferenceController(context));
         controllers.add(new StorageSizePreferenceController(context));
+        controllers.add(new SELinuxStatusPreferenceController(context));
         return controllers;
     }
 
