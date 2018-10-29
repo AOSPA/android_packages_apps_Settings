4a18c568d2087fb7b213747aeaa2bc77a668eb9a
Chris Crump <chris@cypheros.co>
2018-10-29 18:09:29 +0100

    Settings: Long press power button to toggle flashlight [2/3]
    * Animation by Sahul Krishnan, remade to lottie by Andrew Fluck
    * Adapt to Android 11 by @mickaelmendes50
    
    Change-Id: Ibdce3644ba1e794120a268064843349b9fa432a9
    Signed-off-by: Mesquita <mickaelmendes50@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    
    Settings: QuickTorch: Use new material-you illustration preference
    
    * Switch to new Android 12 animation. Huge thanks to @aryanata for the animation
    
    Change-Id: I4eab164850fcef5b7da218e4ea0dc3745a7878e8

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -18,6 +18,7 @@ package com.android.settings.core.gateway;
 
 import co.aospa.settings.display.DcDimmingSettings;
 import co.aospa.settings.display.RefreshRateSettings;
+import co.aospa.settings.gestures.PowerButtonTorchSettings;
 
 import com.android.settings.AllInOneTetherSettings;
 import com.android.settings.DisplaySettings;
@@ -350,6 +351,7 @@ public class SettingsGateway {
             NightDisplaySettings.class.getName(),
             RefreshRateSettings.class.getName(),
             DcDimmingSettings.class.getName(),
+            PowerButtonTorchSettings.class.getName(),
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
             StorageDashboardFragment.class.getName(),
