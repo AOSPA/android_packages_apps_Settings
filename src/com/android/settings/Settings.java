5f17b4259cc83321fb11c0d98d6ec5fa7795a59a
Chris Crump <chris@cypheros.co>
2018-10-29 18:09:29 +0100

    Settings: Long press power button to toggle flashlight [2/3]
    * Animation by Sahul Krishnan, remade to lottie by Andrew Fluck
    * Adapt to Android 11 by @mickaelmendes50
    
    Change-Id: Ibdce3644ba1e794120a268064843349b9fa432a9
    Signed-off-by: Mesquita <mickaelmendes50@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    
    Settings: QuickTorch: Use new material-you illustration preference
    
    * Switch to new Android 12 animation. Huge thanks to @aryanata for the animation
    
    Change-Id: I4eab164850fcef5b7da218e4ea0dc3745a7878e8

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -156,6 +156,7 @@ public class Settings extends SettingsActivity {
     public static class RefreshRateSettingsActivity extends SettingsActivity { /* empty */ }
     public static class DcDimmingSettingsActivity extends SettingsActivity { /* empty */ }
     public static class AdaptivePlaybackSoundSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class PowerButtonTorchSettingsActivity extends SettingsActivity { /* empty */ }
     public static class MyDeviceInfoActivity extends SettingsActivity { /* empty */ }
     public static class ModuleLicensesActivity extends SettingsActivity { /* empty */ }
     public static class ApplicationSettingsActivity extends SettingsActivity { /* empty */ }
