d30b020adb166cedb0402f20965fe92751271ddc
Santiago Etchebehere <santie@google.com>
2019-03-28 13:34:43 -0700

    Remove check for custom themes
    All CLs are in to support one custom theme, so enable them
    again by default
    
    Fixes: 124796742
    Change-Id: I7beacd764726b897d96d21cbcedf8d2198a21bc1

--- src/com/android/customization/model/theme/DefaultThemeProvider.java
+++ src/com/android/customization/model/theme/DefaultThemeProvider.java
@@ -71,8 +71,6 @@ import java.util.function.Consumer;
 public class DefaultThemeProvider extends ResourcesApkProvider implements ThemeBundleProvider {
 
     private static final String TAG = "DefaultThemeProvider";
-    // TODO(b/124796742): remove once custom theme picker is ready to merge
-    private static final boolean SHOW_CUSTOM_THEME_OPTION = true;
 
     private static final String THEMES_ARRAY = "themes";
     private static final String TITLE_PREFIX = "theme_title_";
@@ -197,9 +195,7 @@ public class DefaultThemeProvider extends ResourcesApkProvider implements ThemeB
             }
         }
 
-        if (SHOW_CUSTOM_THEME_OPTION) {
-            addCustomTheme();
-        }
+        addCustomTheme();
     }
 
     private void addColorOverlay(Builder builder, String colorOverlayPackage)
