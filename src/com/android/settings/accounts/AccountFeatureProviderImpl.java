4041986db1f478615bacee0c9fa14d6d4eff6bde
Chris Crump <chris@cypheros.co>
2019-10-19 17:49:00 -0400

    accounts: Use Google as default account provider
    Change-Id: I731f598400385e39455bc4bd95fe9960da873d85

--- src/com/android/settings/accounts/AccountFeatureProviderImpl.java
+++ src/com/android/settings/accounts/AccountFeatureProviderImpl.java
@@ -1,16 +1,20 @@
 package com.android.settings.accounts;
 
 import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Context;
 
 public class AccountFeatureProviderImpl implements AccountFeatureProvider {
+
+	private static final String ACCOUNT_PROVIDER_GOOGLE = "com.google";
+	
     @Override
     public String getAccountType() {
-        return null;
+        return ACCOUNT_PROVIDER_GOOGLE;
     }
 
     @Override
     public Account[] getAccounts(Context context) {
-        return new Account[0];
+        return AccountManager.get(context).getAccountsByType(ACCOUNT_PROVIDER_GOOGLE);
     }
 }
