ecb28297825df8262e6b78bfa6abb5f4bc91d44d
shagbag913 <sh4gbag913@gmail.com>
2019-11-03 18:37:52 -0500

    ThemePicker: Handle themes with no specified wallpaper.
     * When no wallpaper is specified in a theme, the preview is blank until
       the "Use current wallpaper" check is toggled. Fix this by
       initializing the preview with the current home wallpaper if the theme
       doesn't provide a wallpaper.
    
     * Also, hide the "Use current wallpaper" box if no wallpaper is
       provided in the theme.
    
    Change-Id: Ifb08f81360dfb24758a83a063753eacbb24f45eb

--- src/com/android/customization/picker/theme/ThemeFragment.java
+++ src/com/android/customization/picker/theme/ThemeFragment.java
@@ -106,6 +106,7 @@ public class ThemeFragment extends ToolbarFragment {
     private WallpaperInfo mCurrentHomeWallpaper;
     private CurrentWallpaperInfoFactory mCurrentWallpaperFactory;
     private TimeTicker mTicker;
+    private boolean mNoCustomWallpaper;
 
     @Override
     public void onAttach(Context context) {
@@ -230,7 +231,7 @@ public class ThemeFragment extends ToolbarFragment {
                 (homeWallpaper, lockWallpaper, presentationMode) -> {
                     mCurrentHomeWallpaper = homeWallpaper;
                     if (mSelectedTheme != null) {
-                        if (mUseMyWallpaper || (mSelectedTheme instanceof CustomTheme)) {
+                        if (mUseMyWallpaper || mNoCustomWallpaper) {
                             mSelectedTheme.setOverrideThemeWallpaper(homeWallpaper);
                         } else {
                             mSelectedTheme.setOverrideThemeWallpaper(null);
@@ -256,8 +257,7 @@ public class ThemeFragment extends ToolbarFragment {
     }
 
     private void updateButtonsVisibility() {
-        mUseMyWallpaperButton.setVisibility(mSelectedTheme instanceof CustomTheme
-                ? View.INVISIBLE : View.VISIBLE);
+        mUseMyWallpaperButton.setVisibility(mNoCustomWallpaper ? View.GONE : View.VISIBLE);
     }
 
     private void hideError() {
@@ -284,7 +284,9 @@ public class ThemeFragment extends ToolbarFragment {
                         navigateToCustomTheme((CustomTheme) selected);
                     } else {
                         mSelectedTheme = (ThemeBundle) selected;
-                        if (mUseMyWallpaper || mSelectedTheme instanceof CustomTheme) {
+                        mNoCustomWallpaper = mSelectedTheme instanceof CustomTheme ||
+                                mSelectedTheme.getWallpaperInfo() == null;
+                        if (mUseMyWallpaper || mNoCustomWallpaper) {
                             mSelectedTheme.setOverrideThemeWallpaper(mCurrentHomeWallpaper);
                         } else {
                             mSelectedTheme.setOverrideThemeWallpaper(null);
