037d5b4ddc90778331e1a67fa87ba8a70b64fcda
Vinita S. Maloo <vmaloo@codeaurora.org>
2020-04-13 16:29:51 +0530

    Revert: Allow FILS akm from wifi settings application
    Change-Id: I6a00939af89d7f186a74221cbaf7ae0ec50f99ab
    CRs-Fixed: 2677031
    Signed-off-by: Vinita S. Maloo <vmaloo@codeaurora.org>

--- src/com/android/settings/wifi/WifiConfigController.java
+++ src/com/android/settings/wifi/WifiConfigController.java
@@ -699,12 +699,6 @@ public class WifiConfigController implements TextWatcher,
 
             case AccessPoint.SECURITY_EAP:
             case AccessPoint.SECURITY_EAP_SUITE_B:
-                if (mAccessPoint != null && mAccessPoint.isFils256Supported()) {
-                    config.allowedKeyManagement.set(KeyMgmt.FILS_SHA256);
-                }
-                if (mAccessPoint != null && mAccessPoint.isFils384Supported()) {
-                    config.allowedKeyManagement.set(KeyMgmt.FILS_SHA384);
-                }
                 if (mAccessPointSecurity == AccessPoint.SECURITY_EAP_SUITE_B) {
                     // allowedSuiteBCiphers will be set according to certificate type
                     config.setSecurityParams(WifiConfiguration.SECURITY_TYPE_EAP_SUITE_B);
@@ -846,10 +840,6 @@ public class WifiConfigController implements TextWatcher,
                 } else {
                     // clear password
                     config.enterpriseConfig.setPassword(mPasswordView.getText().toString());
-                }
-                if (mAccessPoint != null && (mAccessPoint.isFils256Supported()
-                            || mAccessPoint.isFils384Supported())) {
-                    config.enterpriseConfig.setEapErp("1");
                 }
                 break;
 
