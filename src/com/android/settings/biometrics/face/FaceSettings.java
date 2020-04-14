531367d313bb1f43fb07feca64ca90b74e932c07
Anushek Prasal <anushekprasal@gmail.com>
2020-04-15 03:35:23 +0530

    Settings: Swipe up on keyguard to use Face Unlock [2/2]
    Change-Id: I0004d57f6a291cc4bab3735a74928c26837aa77d
    Signed-off-by: rohan <purohit.rohan@gmail.com>
    Signed-off-by: Noonlord <mymmym22@gmail.com>

--- src/com/android/settings/biometrics/face/FaceSettings.java
+++ src/com/android/settings/biometrics/face/FaceSettings.java
@@ -71,6 +71,7 @@ public class FaceSettings extends DashboardFragment {
     private FaceSettingsRemoveButtonPreferenceController mRemoveController;
     private FaceSettingsEnrollButtonPreferenceController mEnrollController;
     private FaceSettingsLockscreenBypassPreferenceController mLockscreenController;
+    private FaceSettingsSwipePreferenceController mSwipeController;
     private List<AbstractPreferenceController> mControllers;
 
     private List<Preference> mTogglePreferences;
@@ -162,6 +163,7 @@ public class FaceSettings extends DashboardFragment {
         Preference confirmPref = findPreference(FaceSettingsConfirmPreferenceController.KEY);
         Preference bypassPref =
                 findPreference(mLockscreenController.getPreferenceKey());
+        Preference swipePref = findPreference(mSwipeController.getPreferenceKey());
         mTogglePreferences = new ArrayList<>(
                 Arrays.asList(keyguardPref, appPref, attentionPref, confirmPref, bypassPref));
 
@@ -182,6 +184,7 @@ public class FaceSettings extends DashboardFragment {
         if (mUserManager.isManagedProfile(mUserId)) {
             removePreference(FaceSettingsKeyguardPreferenceController.KEY);
             removePreference(mLockscreenController.getPreferenceKey());
+            removePreference(mSwipeController.getPreferenceKey());
         }
 
         if (savedInstanceState != null) {
@@ -195,6 +198,8 @@ public class FaceSettings extends DashboardFragment {
 
         mLockscreenController = use(FaceSettingsLockscreenBypassPreferenceController.class);
         mLockscreenController.setUserId(mUserId);
+        mSwipeController = use(FaceSettingsSwipePreferenceController.class);
+        mSwipeController.setUserId(mUserId);
     }
 
     @Override
