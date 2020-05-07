PixelExperience/packages_apps_Settings

Implement button settings
Credits to Steve Kondik and CM/LineageOS

https://github.com/LineageOS/android_packages_apps_LineageParts/commit/3281bd65933aa09c410cf1258040ed7d0c0fc1b3#diff-c8fb7086712ea95726e6465e32fceb54

Change-Id: I368d97f3b00adc5c0f559abb9f1cc5be6e532048

src/com/android/settings/custom/buttons/ButtonSettingsUtils.java
@@ -1,0 +1,25 @@
+/*
+* Copyright (C) 2018 The Pixel Experience Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.settings.custom.buttons;
+
+import android.content.Context;
+import com.android.internal.util.custom.NavbarUtils;
+
+public class ButtonSettingsUtils {
+    public static boolean isAvailable(Context context) {
+        return NavbarUtils.canDisable(context);
+    }
+}

AOSPA/android_packages_apps_Settings

src/com/android/settings/custom/buttons/ButtonSettingsUtils.java
@@ -1,0 +1,25 @@
+/*
+* Copyright (C) 2018 The Pixel Experience Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.settings.custom.buttons;
+
+import android.content.Context;
+import com.android.internal.util.aospa.NavbarUtils;
+
+public class ButtonSettingsUtils {
+    public static boolean isAvailable(Context context) {
+        return NavbarUtils.canDisable(context);
+    }
+}