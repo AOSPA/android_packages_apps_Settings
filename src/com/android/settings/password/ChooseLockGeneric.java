9fb1d54861409fec78bf53f7265873e2f8ce1d4d
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-10-24 22:52:31 +0300

    Settings: Make Quick Unlock compatible with long PIN/Password.
        * Idea from OnePlus OOS.
    
    [Pranav Vashi <neobuddy89@gmail.com>]:
        * Forward port to Android 12
    
    [Jyotiraditya Panda <jyotiraditya@aospa.co>]:
        * Code cleanup and format
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I9a39cdc8c53ee46cd0331f946bbe53aa6c5a0bac

--- src/com/android/settings/password/ChooseLockGeneric.java
+++ src/com/android/settings/password/ChooseLockGeneric.java
@@ -998,6 +998,7 @@ public class ChooseLockGeneric extends SettingsActivity {
                                 lock.defaultQuality,
                                 lock == ScreenLockType.NONE,
                                 false /* chooseLockSkipped */);
+                        mLockPatternUtils.setPinPasswordLength(-1, mUserId);
                         return true;
                     case PATTERN:
                     case PIN:
--- src/com/android/settings/password/ChooseLockPattern.java
+++ src/com/android/settings/password/ChooseLockPattern.java
@@ -663,6 +663,7 @@ public class ChooseLockPattern extends SettingsActivity {
                             + " when button is " + RightButtonMode.Confirm);
                 }
                 startSaveAndFinish();
+                mLockPatternUtils.setPinPasswordLength(-1, mUserId);
             } else if (mUiStage.rightMode == RightButtonMode.Ok) {
                 if (mUiStage != Stage.HelpScreen) {
                     throw new IllegalStateException("Help screen is only mode with ok button, "
