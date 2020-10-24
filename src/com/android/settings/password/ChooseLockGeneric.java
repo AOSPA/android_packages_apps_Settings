19d961f571de7a6a53ba0086b62fbdb3163580f3
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-10-24 22:52:31 +0300

    Settings: Make Quick Unlock compatible with long PIN/Password.
        * Idea from OnePlus OOS.
    
    [Pranav Vashi <neobuddy89@gmail.com>]:
        * Forward port to Android 12
    
    [Jyotiraditya Panda <jyotiraditya@aospa.co>]:
        * Code cleanup and format
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I68d5d474d36be6b112a5b06c13c21e801cff3bc9

--- src/com/android/settings/password/ChooseLockGeneric.java
+++ src/com/android/settings/password/ChooseLockGeneric.java
@@ -1004,6 +1004,7 @@ public class ChooseLockGeneric extends SettingsActivity {
                                 lock.defaultQuality,
                                 lock == ScreenLockType.NONE,
                                 false /* chooseLockSkipped */);
+                        mLockPatternUtils.setPinPasswordLength(-1, mUserId);
                         return true;
                     case PATTERN:
                     case PIN:
