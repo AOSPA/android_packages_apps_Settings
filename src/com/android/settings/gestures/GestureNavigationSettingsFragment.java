41cbe434b6407e7ec679547cd9b522d34b3c7a10
Kra1o5 <kra1o5x@gmail.com>
2020-11-09 20:10:32 +0100

    Settings: Introduce Immersive Navigation [2/3]
    Based in Hide gesture navbar from Syberia project
    
    commit 5e78e6e49ffd43005dfd1488d07755d7eb2e57b6
    Author: Anay Wadhera <awadhera@berkeley.edu>
    Date:   Thu Dec 10 10:44:36 2020 -0800
    
        gestures: expect 0 for immersive nav if it is undefined
    
        Change-Id: I4ea072623d9c345d7328550d7b7f1c20bb381659
    
    Change-Id: I6faaa8e544cd0c07431a3d40a3ee0d2fed28e998
    
    [ghostrider-reborn: minor tweaks]
    
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Co-authored-by: Víctor Santoja <victorsantoja@gmail.com>
    Change-Id: I40a73ecb1a5d094828665bcc6d05d7409f78751d
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- src/com/android/settings/gestures/GestureNavigationSettingsFragment.java
+++ src/com/android/settings/gestures/GestureNavigationSettingsFragment.java
@@ -18,11 +18,17 @@ package com.android.settings.gestures;
 
 import android.app.settings.SettingsEnums;
 import android.content.Context;
+import android.content.om.IOverlayManager;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.provider.Settings;
 import android.view.WindowManager;
+
+import androidx.preference.Preference;
+import androidx.preference.SwitchPreference;
 
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
@@ -30,6 +36,9 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.widget.LabeledSeekBarPreference;
 import com.android.settings.widget.SeekBarPreference;
 import com.android.settingslib.search.SearchIndexable;
+
+import static android.os.UserHandle.USER_CURRENT;
+import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_GESTURAL_OVERLAY;
 
 /**
  * A fragment to include all the settings related to Gesture Navigation mode.
@@ -41,9 +50,16 @@ public class GestureNavigationSettingsFragment extends DashboardFragment {
 
     public static final String GESTURE_NAVIGATION_SETTINGS =
             "com.android.settings.GESTURE_NAVIGATION_SETTINGS";
+    public static final String IMMERSIVE_NAVIGATION_SETTINGS =
+            "immersive_navigation";
 
     private static final String LEFT_EDGE_SEEKBAR_KEY = "gesture_left_back_sensitivity";
     private static final String RIGHT_EDGE_SEEKBAR_KEY = "gesture_right_back_sensitivity";
+    private static final String IMMERSIVE_NAV_KEY = "immersive_navigation";
+
+    private static final String NAV_MODE_IMMERSIVE_OVERLAY = "co.aospa.overlay.systemui.immnav.gestural";
+
+    private IOverlayManager mOverlayService;
 
     private WindowManager mWindowManager;
     private BackGestureIndicatorView mIndicatorView;
@@ -61,6 +77,8 @@ public class GestureNavigationSettingsFragment extends DashboardFragment {
 
         mIndicatorView = new BackGestureIndicatorView(getActivity());
         mWindowManager = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
+        mOverlayService = IOverlayManager.Stub
+                               .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
     }
 
     @Override
@@ -75,6 +93,7 @@ public class GestureNavigationSettingsFragment extends DashboardFragment {
 
         initSeekBarPreference(LEFT_EDGE_SEEKBAR_KEY);
         initSeekBarPreference(RIGHT_EDGE_SEEKBAR_KEY);
+        initImmersiveSwitchPreference();
     }
 
     @Override
@@ -148,6 +167,34 @@ public class GestureNavigationSettingsFragment extends DashboardFragment {
             Settings.Secure.putFloat(getContext().getContentResolver(), settingsKey, scale);
             return true;
         });
+    }
+
+    private void initImmersiveSwitchPreference() {
+        SwitchPreference prefImmersiveNav = getPreferenceScreen().findPreference(IMMERSIVE_NAV_KEY);
+
+        prefImmersiveNav.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object o) {
+                    final boolean isEnabled = (Boolean) o;
+                    if (isEnabled) {
+                        try {
+                            mOverlayService.setEnabledExclusiveInCategory(NAV_MODE_IMMERSIVE_OVERLAY, USER_CURRENT);
+                        } catch (RemoteException re) {
+                            throw re.rethrowFromSystemServer();
+                        }
+                    } else {
+                        try {
+                            mOverlayService.setEnabledExclusiveInCategory(NAV_BAR_MODE_GESTURAL_OVERLAY, USER_CURRENT);
+                            mOverlayService.setEnabled(NAV_MODE_IMMERSIVE_OVERLAY, false, USER_CURRENT);
+                        } catch (RemoteException re) {
+                            throw re.rethrowFromSystemServer();
+                        }
+                    }
+                    Settings.Secure.putInt(getContext().getContentResolver(), IMMERSIVE_NAVIGATION_SETTINGS, isEnabled ? 1 : 0);
+                    return true;
+            }
+        });
+        prefImmersiveNav.setChecked(Settings.Secure.getInt(getContext().getContentResolver(), IMMERSIVE_NAVIGATION_SETTINGS, 0) != 0);
     }
 
     private static float[] getFloatArray(TypedArray array) {
