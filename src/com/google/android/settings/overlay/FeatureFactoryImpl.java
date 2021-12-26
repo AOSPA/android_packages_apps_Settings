bb78dd088e21355a87e6f8c3e7aa07f14c7bfe0e
Anay Wadhera <awadhera@berkeley.edu>
2021-12-26 13:59:58 -0500

    [WIP] [statix-12] Settings: Import GoogleGameDashboard [2/3]
    Change-Id: I1ffec9253c5db0f424e813e854cba6e534d1a4ed
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- src/com/google/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/google/android/settings/overlay/FeatureFactoryImpl.java
@@ -3,11 +3,14 @@ package com.google.android.settings.overlay;
 import android.content.Context;
 
 import com.android.settings.fuelgauge.PowerUsageFeatureProvider;
+import com.android.settings.applications.GameSettingsFeatureProvider;
 import com.google.android.settings.fuelgauge.PowerUsageFeatureProviderGoogleImpl;
+import com.google.android.settings.games.GameSettingsFeatureProviderGoogleImpl;
 
 public final class FeatureFactoryImpl extends com.android.settings.overlay.FeatureFactoryImpl {
 
     private PowerUsageFeatureProvider mPowerUsageFeatureProvider;
+    private GameSettingsFeatureProvider mGameSettingsFeatureProvider;
 
     @Override
     public PowerUsageFeatureProvider getPowerUsageFeatureProvider(Context context) {
@@ -16,5 +19,13 @@ public final class FeatureFactoryImpl extends com.android.settings.overlay.Featu
                     context.getApplicationContext());
         }
         return mPowerUsageFeatureProvider;
+    }
+
+    @Override
+    public GameSettingsFeatureProvider getGameSettingsFeatureProvider() {
+        if (mGameSettingsFeatureProvider == null) {
+            mGameSettingsFeatureProvider = new GameSettingsFeatureProviderGoogleImpl();
+        }
+        return mGameSettingsFeatureProvider;
     }
 }
