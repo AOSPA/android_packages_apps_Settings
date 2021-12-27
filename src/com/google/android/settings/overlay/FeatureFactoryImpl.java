f0c9343320bae32c0d00d99c9ae27457b51c8339
Anay Wadhera <awadhera@berkeley.edu>
2021-12-27 19:20:45 +0530

    Settings: Import PowerUsageFeatureGoogle
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I706c3b1eabb4e6ed7622999094a260031d363897

--- src/com/google/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/google/android/settings/overlay/FeatureFactoryImpl.java
@@ -1,4 +1,20 @@
 package com.google.android.settings.overlay;
 
+import android.content.Context;
+
+import com.android.settings.fuelgauge.PowerUsageFeatureProvider;
+import com.google.android.settings.fuelgauge.PowerUsageFeatureProviderGoogleImpl;
+
 public final class FeatureFactoryImpl extends com.android.settings.overlay.FeatureFactoryImpl {
+
+    private PowerUsageFeatureProvider mPowerUsageFeatureProvider;
+
+    @Override
+    public PowerUsageFeatureProvider getPowerUsageFeatureProvider(Context context) {
+        if (mPowerUsageFeatureProvider == null) {
+            mPowerUsageFeatureProvider = new PowerUsageFeatureProviderGoogleImpl(
+                    context.getApplicationContext());
+        }
+        return mPowerUsageFeatureProvider;
+    }
 }
