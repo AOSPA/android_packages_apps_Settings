bdfe91a43f9957fb219e3a585b12b93e9223dbe6
Jake Weinstein <jake@aospa.co>
2022-10-09 02:56:56 +0900

    Revert "Adding QtiFeedback menu settings"
    We do not use this application and it has some
    unnecessary overhead.
    
    This reverts commit 4d7f9df991286c05a7edf246a74e3cbc91d96b0d.
    
    Change-Id: Ic1da21c104793a7651ec3296af4f38ff3671611e

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -177,7 +177,6 @@ public class SettingsActivity extends SettingsBaseActivity
 
     private CharSequence mInitialTitle;
     private int mInitialTitleResId;
-    private SmqSettings mSMQ;
 
     private BroadcastReceiver mDevelopmentSettingsListener;
 
@@ -278,8 +277,6 @@ public class SettingsActivity extends SettingsBaseActivity
         if (intent.hasExtra(EXTRA_UI_OPTIONS)) {
             getWindow().setUiOptions(intent.getIntExtra(EXTRA_UI_OPTIONS, 0));
         }
-
-        mSMQ = new SmqSettings(getApplicationContext());
 
         // Getting Intent properties can only be done after the super.onCreate(...)
         final String initialFragmentName = getInitialFragmentName(intent);
@@ -758,14 +755,6 @@ public class SettingsActivity extends SettingsBaseActivity
      */
     private void switchToFragment(String fragmentName, Bundle args, boolean validate,
             int titleResId, CharSequence title) {
-        if (fragmentName.equals(getString(R.string.qtifeedback_intent_action))){
-             final Intent newIntent = new Intent(getString(R.string.qtifeedback_intent_action));
-             newIntent.addCategory("android.intent.category.DEFAULT");
-             startActivity(newIntent);
-             finish();
-             return;
-        }
-
         Log.d(LOG_TAG, "Switching to fragment " + fragmentName);
 
         if (validate && !isValidFragment(fragmentName)) {
@@ -811,9 +800,6 @@ public class SettingsActivity extends SettingsBaseActivity
                 pm.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH), isAdmin)
                 || somethingChanged;
 
-        if(mSMQ.isShowSmqSettings()){
-            somethingChanged = setTileEnabled(changedList, new ComponentName(packageName, Settings.SMQQtiFeedbackActivity.class.getName()), mSMQ.isShowSmqSettings(), isAdmin) || somethingChanged;
-        }
 
         // Enable DataUsageSummaryActivity if the data plan feature flag is turned on otherwise
         // enable DataPlanUsageSummaryActivity.
