ff8fa20b34d6629934e0fa51cc878c9344317033
Mengjun Leng <quic_mengjunl@quicinc.com>
2023-04-13 15:26:30 +0530

    Refactor data preference
    The new centralized Telephony event dispatch mechanism is introduced in
    Telephony settings, to adjust with this new architecture, refactor the
    legacy data preference as follows:
    1, Fetch call event from mobile network repository instead.
    2, Move the major implementation of data preference to its own sub-class.
    
    Change-Id: Icec435e05853e34e4694277b65b06cff18c93e83
    CRs-Fixed: 3478092

--- src/com/android/settings/network/telephony/DataDefaultSubscriptionController.java
+++ src/com/android/settings/network/telephony/DataDefaultSubscriptionController.java
@@ -15,24 +15,30 @@
   * See the License for the specific language governing permissions and
   * limitations under the License.
   */
- package com.android.settings.network.telephony;
+package com.android.settings.network.telephony;
 
- import android.content.Context;
- import android.provider.Settings;
- import android.telephony.SubscriptionManager;
- import androidx.lifecycle.LifecycleOwner;
- import com.android.settings.R;
- import com.android.settingslib.core.lifecycle.Lifecycle;
- import com.android.settingslib.mobile.dataservice.SubscriptionInfoEntity;
+import android.content.Context;
+import android.provider.Settings;
+import android.sysprop.TelephonyProperties;
+import android.telephony.SubscriptionManager;
+import android.telephony.TelephonyManager;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.preference.Preference;
+import com.android.settings.R;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.android.settingslib.mobile.dataservice.SubscriptionInfoEntity;
 
 public class DataDefaultSubscriptionController extends DefaultSubscriptionController {
 
     private static final String SETTING_USER_PREF_DATA_SUB = "user_preferred_data_sub";
     private SubscriptionInfoEntity mSubscriptionInfoEntity;
+    private boolean mHasAnyOngoingCallOnDevice = false;
+    private TelephonyManager mTelephonyManager;
 
     public DataDefaultSubscriptionController(Context context, String preferenceKey,
               Lifecycle lifecycle, LifecycleOwner lifecycleOwner) {
-          super(context, preferenceKey, lifecycle, lifecycleOwner);
+        super(context, preferenceKey, lifecycle, lifecycleOwner);
+        mTelephonyManager = context.getSystemService(TelephonyManager.class);
     }
 
     @Override
@@ -63,8 +69,7 @@ public class DataDefaultSubscriptionController extends DefaultSubscriptionContro
 
     @Override
     public CharSequence getSummary() {
-        boolean isSmartDdsEnabled = Settings.Global.getInt(mContext.getContentResolver(),
-                    Settings.Global.SMART_DDS_SWITCH, 0) == 1;
+        boolean isSmartDdsEnabled = isSmartDdsEnabled();
         if (isSmartDdsEnabled) {
             return mContext.getString(R.string.dds_preference_smart_dds_switch_is_on);
         }
@@ -74,5 +79,56 @@ public class DataDefaultSubscriptionController extends DefaultSubscriptionContro
     private void setUserPrefDataSubIdInDb(int subId) {
         android.provider.Settings.Global.putInt(mContext.getContentResolver(),
                 SETTING_USER_PREF_DATA_SUB, subId);
+    }
+
+    @Override
+    public void onDefaultDataChanged(int defaultDataSubId) {
+        updateEntries();
+        refreshSummary(mPreference);
+    }
+
+    @Override
+    protected boolean isAskEverytimeSupported() {
+        return false;
+    }
+
+    @Override
+    public void onAnyOngoingCallOnDevice(boolean isAnyCallOngoing) {
+        mHasAnyOngoingCallOnDevice = isAnyCallOngoing;
+        updateEntries();
+        refreshSummary(mPreference);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mHasAnyOngoingCallOnDevice = mMobileNetworkRepository.isAnyOngoingCallOnDevice();
+    }
+
+    private boolean hasAnyOngoingCallOnDevice() {
+        return mHasAnyOngoingCallOnDevice;
+    }
+
+    @Override
+    protected void updatePreferenceState(Preference preference) {
+        if (preference != null) {
+            boolean isEcbmEnabled = mTelephonyManager.getEmergencyCallbackMode();
+            boolean isScbmEnabled = TelephonyProperties.in_scbm().orElse(false);
+            boolean isSmartDdsEnabled = isSmartDdsEnabled();
+
+            if (!isSmartDdsEnabled) {
+                preference.setEnabled(!hasAnyOngoingCallOnDevice() && !isEcbmEnabled
+                        && !isScbmEnabled
+                        && (!TelephonyUtils.isSubsidyFeatureEnabled(mContext)
+                        || TelephonyUtils.allowUsertoSetDDS(mContext)));
+            } else {
+                preference.setEnabled(false);
+            }
+        }
+    }
+
+    private boolean isSmartDdsEnabled() {
+        return Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.SMART_DDS_SWITCH, 0) == 1;
     }
 }
