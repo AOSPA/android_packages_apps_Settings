0890081fe7dbaf477c634a4fea1257d30bf2a5ff
Jake Weinstein <jake@aospa.co>
2023-04-17 07:18:31 +0000

    Re-Revert "Settings: Make Quick Unlock compatible with long PIN/Password."
    This reverts commit 70d36dcd0611141632f1f517f483baff6c90c415.
    
    Reason for revert: "QuickUnlock: Avoid saving pin/password length as in a sql db" removes the setPinPasswordLength function.
    
    Change-Id: If26ada125a696bb272e7ac7bf19f8c9df78e9a05

--- src/com/android/settings/password/ChooseLockGeneric.java
+++ src/com/android/settings/password/ChooseLockGeneric.java
@@ -998,7 +998,6 @@ public class ChooseLockGeneric extends SettingsActivity {
                                 lock.defaultQuality,
                                 lock == ScreenLockType.NONE,
                                 false /* chooseLockSkipped */);
-                        mLockPatternUtils.setPinPasswordLength(-1, mUserId);
                         return true;
                     case PATTERN:
                     case PIN:
