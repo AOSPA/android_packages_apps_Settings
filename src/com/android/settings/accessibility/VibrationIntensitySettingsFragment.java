4bd4a392fa3162b92e6d3ca0d83ae686f1081edc
Rituj Beniwal <ritujbeniwal@gmail.com>
2023-06-13 22:45:03 +0200

    Settings: Add custom vibration patterns [2/4] [SQUASH]
    a rewrite of: https://github.com/AOSPA/android_packages_apps_Settings/commit/c4560cafae3f97f09c372c5289869b35c90d3b71
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Co-authored-by: Vishalcj17 <vishalcj@aospa.co>
    
    Squashed with:
    
      Settings: Preview ringtone vibration pattern
    
        Change-Id: I7e5aff2f07618028122d759649c00236eefffdd8
    
      Settings: Follow new ringtone vibration patterns
    
        For preview
    
        See: https://github.com/yaap/packages_services_Telecomm/commit/7b3ab1300210adc23c1441953c80a4778ca8e312
        Change-Id: I5f00d655b4920748d41ccf71b0367eb70f5e49ba
    
      Settings: Allow customizing default notification vibration pattern [2/2]
    
        Re-use the patterns we've got for ringtone vibration as options
        In addition allow crafting a custom pattern same as we do for ringtone
    
        Re-use as much resources and controllers as possible from our ringtone vibration pattern options.
        To achieve that make the original controllers more generic and inherit in our notification ones.
    
        Change-Id: I4d0092b3e3f90a9971761d680a1d2b5e037189a1
    
      Settings: Add different custom notification vibration pattern [2/2]
    
        * the ones used by calls don't really fit here
    
        Change-Id: I37f912a21a1457169f02e589161b412933eb127e
    
      Settings: Add function to get call and notifs pattern amplitude
    
        Change-Id: I6808e4eec95b5aa258988fec18d9cccc99a0d5c3
    
      Settings: Move vibration patterns settings to vibration and haptics
    
        Change-Id: Ifa46e34a2cf22309826bc2116bbe3933d3dd7fca
    
      Settings: Add different custom notification vibration pattern [2/2] (2)
    
        Change-Id: If3b3437e6c9733b96b0d2deb71128604adbd4425
    
    Change-Id: Id6b7ae934803722c66970a6c5f2c532b143dafd6

--- src/com/android/settings/accessibility/VibrationIntensitySettingsFragment.java
+++ src/com/android/settings/accessibility/VibrationIntensitySettingsFragment.java
@@ -30,10 +30,17 @@ import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 import androidx.recyclerview.widget.RecyclerView;
 
+import co.aospa.settings.accessibility.NotificationVibrationPatternPreferenceController;
+import co.aospa.settings.accessibility.PhoneVibrationPatternPreferenceController;
+
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.search.SearchIndexable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /** Accessibility settings for the vibration. */
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
@@ -59,6 +66,11 @@ public class VibrationIntensitySettingsFragment extends DashboardFragment {
     @Override
     protected String getLogTag() {
         return TAG;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context);
     }
 
     @VisibleForTesting
@@ -91,7 +103,23 @@ public class VibrationIntensitySettingsFragment extends DashboardFragment {
                 protected boolean isPageSearchEnabled(Context context) {
                     return VibrationIntensitySettingsFragment.isPageSearchEnabled(context);
                 }
+
+                @Override
+                public List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+                    return buildPreferenceControllers(context);
+                }
             };
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(Context context) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+
+        // === Phone & notification ringtone vibration pattern===
+        controllers.add(new NotificationVibrationPatternPreferenceController(context));
+        controllers.add(new PhoneVibrationPatternPreferenceController(context));
+
+        return controllers;
+    }
+
 
     @Override
     public @Nullable String getPreferenceScreenBindingKey(@NonNull Context context) {
