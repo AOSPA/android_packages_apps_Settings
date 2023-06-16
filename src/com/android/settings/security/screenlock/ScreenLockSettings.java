2646b84883542aea93708fe1731308224c70f90e
Jake Weinstein <jake@aospa.co>
2023-06-16 20:41:29 +0900

    Revert "Settings: Allow user configurable fingerprint wake-and-unlock"
    This reverts commit e48aa327dfe919a52cdd692d057baed5986751bf.
    
    Change-Id: Id2a0d1fdab1012bd688ba6a3bc86eaccb014d5b7

--- src/com/android/settings/security/screenlock/ScreenLockSettings.java
+++ src/com/android/settings/security/screenlock/ScreenLockSettings.java
@@ -78,7 +78,6 @@ public class ScreenLockSettings extends DashboardFragment
                 context, MY_USER_ID, lockPatternUtils));
         controllers.add(new LockAfterTimeoutPreferenceController(
                 context, MY_USER_ID, lockPatternUtils));
-        controllers.add(new FingerprintUnlockPreferenceController(context));
         controllers.add(new OwnerInfoPreferenceController(context, parent));
         return controllers;
     }
