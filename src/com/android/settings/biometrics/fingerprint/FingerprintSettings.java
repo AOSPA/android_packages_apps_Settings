5243945c69c4eb21dc9f0b8bc82121e6e046653c
Terminator-J <terminator_j@hotbutteredmonkey.com>
2023-06-29 18:23:45 -0700

    Settings: Show single-touch wakeup setting on rear & front fps devices
    Not sure why Google assumed that showing this setting is only useful
    for devices with FP sensor built into a side-mounted power button.
    
    Change logic to preserve check for side mounted (really, power-button
    mounted) FP sensors, but allow any device which has a a rear or front
    mounted sensor to show the relevant setting; so users can toggle it.
    
    Bug: Enchilada (Oneplus 6, capacitive FP sensor on the back) no longer
    wakes & unlocks from screen-off with FP sensor touch after QPR3 merge.
    
    Test: Apply commit. "Touch to unlock anytime" setting is now visible;
    enabling once again allows one-touch wakeup-and-unlock-on-successful-
    FP-auth, does nothing with failed FP auth, while disabling toggle
    requires pressing power button to wake screen first before FP auth
    will unlock.
    
    Change-Id: I4d8204b5fed7d43baa93d6793e7280260ae404d6
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
+++ src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
@@ -248,7 +248,7 @@ public class FingerprintSettings extends SubSettings {
                     }
 
                     private void updateDialog() {
-                        if (isSfps() && !mRequireScreenOnToAuth) {
+                        if (!isUdfps() && !mRequireScreenOnToAuth) {
                             setRequireScreenOnToAuthVisibility();
                         }
                         RenameDialog renameDialog = (RenameDialog) getFragmentManager().
@@ -524,7 +524,7 @@ public class FingerprintSettings extends SubSettings {
                         return true;
                     });
             mFingerprintUnlockCategory.setVisible(false);
-            if (isSfps() && !mRequireScreenOnToAuth) {
+            if (!isUdfps() && !mRequireScreenOnToAuth) {
                 setRequireScreenOnToAuthVisibility();
             }
             setPreferenceScreen(root);
