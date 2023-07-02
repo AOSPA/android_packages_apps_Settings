Evolution-X-AOSP/packages_apps_Settings

Settings: Add Security Hub from Pixel devices
Obviously it needs the SecurityHubPrebuilt.apk installed
on your system for it to work correctly.

Change-Id: I3843f6134743db9802bc5c84bc4f609928549081
Signed-off-by: Jason Edson <jaysonedson@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -183,6 +183,8 @@ import com.android.settings.livedisplay.LiveDisplaySettings;
 import com.evolution.settings.EvolutionSettings;
 import com.evolution.settings.fragments.HeadsUpSettings;
 
+import com.google.android.settings.security.SecurityHubDashboard;
+
 public class SettingsGateway {
 
     /**
@@ -352,7 +354,8 @@ public class SettingsGateway {
             TurnScreenOnDetails.class.getName(),
             EvolutionSettings.class.getName(),
             HeadsUpSettings.class.getName(),
-            LiveDisplaySettings.class.getName()
+            LiveDisplaySettings.class.getName(),
+            SecurityHubDashboard.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

AOSPA/android_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -182,6 +182,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.google.android.settings.security.SecurityHubDashboard;
+
 public class SettingsGateway {
 
     /**
@@ -351,7 +353,8 @@ public class SettingsGateway {
             MobileNetworkSettings.class.getName(),
             AppLocaleDetails.class.getName(),
             TurnScreenOnSettings.class.getName(),
-            TurnScreenOnDetails.class.getName()
+            TurnScreenOnDetails.class.getName(),
+            SecurityHubDashboard.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {