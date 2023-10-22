5e8df2bebf24bd42daa8cafb9ad9dae208559176
Jake Weinstein <jake@aospa.co>
2023-10-23 04:02:45 +0900

    Settings: allow disable of screenshot shutter sound [2/2]
    Change-Id: Ie69f44a3b7db3bf3c9a5c098bca43568e1099283
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -280,6 +280,8 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                 new EmergencyTonePreferenceController(context, fragment, lifecycle);
         final VibrateIconPreferenceController vibrateIconPreferenceController =
                 new VibrateIconPreferenceController(context, fragment, lifecycle);
+        final ScreenshotSoundPreferenceController screenshotSoundPreferenceController =
+                new ScreenshotSoundPreferenceController(context, fragment, lifecycle);
 
         controllers.add(dialPadTonePreferenceController);
         controllers.add(callConnectedTonePreferenceController);
@@ -291,6 +293,7 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
         controllers.add(dockAudioMediaPreferenceController);
         controllers.add(bootSoundPreferenceController);
         controllers.add(emergencyTonePreferenceController);
+        controllers.add(screenshotSoundPreferenceController);
         controllers.add(new PreferenceCategoryController(context,
                 "other_sounds_and_vibrations_category").setChildren(
                 Arrays.asList(dialPadTonePreferenceController,
@@ -302,7 +305,8 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                         vibrateIconPreferenceController,
                         dockAudioMediaPreferenceController,
                         bootSoundPreferenceController,
-                        emergencyTonePreferenceController)));
+                        emergencyTonePreferenceController,
+                        screenshotSoundPreferenceController)));
 
         return controllers;
     }
