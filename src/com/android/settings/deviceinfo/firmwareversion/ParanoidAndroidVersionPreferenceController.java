3ae31a3ab872bd40f160a51b263367487b781ae3
Hernán Castañón Álvarez <herna@paranoidandroid.co>
2023-11-10 14:08:29 +0100

    Settings: Simplify Paranoid Android version
    Now we directly give the version from vendor, so no need for duplicated code.
    
    Change-Id: Ifd46690588d7c5299577b5904785a7ff5a263093
    Signed-off-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>

--- src/com/android/settings/deviceinfo/firmwareversion/ParanoidAndroidVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/ParanoidAndroidVersionPreferenceController.java
@@ -24,9 +24,7 @@ import com.android.settings.core.BasePreferenceController;
 
 public class ParanoidAndroidVersionPreferenceController extends BasePreferenceController {
 
-    private static final String AOSPA_BUILD_VARIANT_PROP = "ro.aospa.build.variant";
-    private static final String AOSPA_VERSION_MAJOR_PROP = "ro.aospa.version.major";
-    private static final String AOSPA_VERSION_MINOR_PROP = "ro.aospa.version.minor";
+    private static final String AOSPA_VERSION_PROP = "ro.aospa.version";
 
     private final Context mContext;
 
@@ -42,19 +40,7 @@ public class ParanoidAndroidVersionPreferenceController extends BasePreferenceCo
 
     @Override
     public CharSequence getSummary() {
-        String aospaVersionMajor = SystemProperties.get(AOSPA_VERSION_MAJOR_PROP,
+        return SystemProperties.get(AOSPA_VERSION_PROP,
                 mContext.getResources().getString(R.string.device_info_default));
-        String aospaVersionMinor = SystemProperties.get(AOSPA_VERSION_MINOR_PROP,
-                mContext.getResources().getString(R.string.device_info_default));
-        String aospaBuildVariant = SystemProperties.get(AOSPA_BUILD_VARIANT_PROP,
-                mContext.getResources().getString(R.string.device_info_default));
-
-        if (aospaBuildVariant.equals("Release")) {
-            return aospaVersionMajor + " " + aospaVersionMinor;
-        } else if (aospaBuildVariant.equals("Unofficial")) {
-           return aospaVersionMajor + " " + aospaBuildVariant;
-        } else {
-           return aospaVersionMajor + " " + aospaBuildVariant + " " + aospaVersionMinor;
-        }
     }
 }
