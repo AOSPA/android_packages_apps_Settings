6fff5fe98f2ddb5ac2d3b1518c7f65090b5480cd
Mesquita <mesquita@aospa.co>
2023-11-23 21:34:34 -0300

    Settings: Fix a null pointer exception on cutout force full screen
     * When orientation change occurs, android restarts the running Activity
       (onDestroy() is called, followed by onCreate()).
       That was causing DisplayCutoutForceFullscreen to crash due to
       appBarLayout being null after the screen rotation.
       Using lazy instead of lateinit will help to avoid this issue.
    
    Change-Id: Ib2ae298978bbfe5eb3defcabd013e86a5442961b
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- src/com/android/settings/display/DisplayCutoutForceFullscreenSettings.kt
+++ src/com/android/settings/display/DisplayCutoutForceFullscreenSettings.kt
@@ -56,7 +56,9 @@ class DisplayCutoutForceFullscreenSettings: Fragment(R.layout.cutout_force_fulls
     private lateinit var adapter: AppListAdapter
     private lateinit var packageList: List<PackageInfo>
     private lateinit var cutoutForceFullscreenSettings: CutoutFullscreenController
-    private lateinit var appBarLayout: AppBarLayout
+    private val appBarLayout: AppBarLayout by lazy{
+        requireActivity().findViewById(R.id.app_bar)
+    }
 
     private var searchText = ""
     private var category: Int = CATEGORY_USER_ONLY
@@ -68,7 +70,7 @@ class DisplayCutoutForceFullscreenSettings: Fragment(R.layout.cutout_force_fulls
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
         requireActivity().setTitle(getTitle())
-        appBarLayout = requireActivity().findViewById(R.id.app_bar)
+        //appBarLayout = requireActivity().findViewById(R.id.app_bar)
         activityManager = requireContext().getSystemService(ActivityManager::class.java)
         packageManager = requireContext().packageManager
         packageList = packageManager.getInstalledPackages(0)
