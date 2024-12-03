fcd40c3e7177aa76cda6f575d6f13b353980bc1d
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-12-03 14:00:21 +0100

    Settings: Rework DT2W settings to match recent ST2W and PU2W changes [1/2]
    Change-Id: Idbf6396aadb4df4b39df35a225b821f091a51d26

--- src/com/android/settings/gestures/DoubleTapScreenPreferenceController.java
+++ src/com/android/settings/gestures/DoubleTapScreenPreferenceController.java
@@ -19,27 +19,42 @@ package com.android.settings.gestures;
 import static android.provider.Settings.Secure.DOZE_DOUBLE_TAP_GESTURE;
 
 import android.annotation.UserIdInt;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.ContentObserver;
 import android.hardware.display.AmbientDisplayConfiguration;
+import android.net.Uri;
+import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.TextUtils;
 
 import androidx.annotation.VisibleForTesting;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
 
-public class DoubleTapScreenPreferenceController extends GesturePreferenceController {
+import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
+import com.android.settingslib.core.lifecycle.LifecycleObserver;
+import com.android.settingslib.core.lifecycle.events.OnStart;
+import com.android.settingslib.core.lifecycle.events.OnStop;
+import com.android.settingslib.PrimarySwitchPreference;
 
-    private final int ON = 1;
-    private final int OFF = 0;
+public class DoubleTapScreenPreferenceController extends TogglePreferenceController
+        implements LifecycleObserver, OnStart, OnStop {
 
-    private static final String PREF_KEY_VIDEO = "gesture_double_tap_screen_video";
+    private static final int ON = 1;
+    private static final int OFF = 0;
 
     private static final String SECURE_KEY = DOZE_DOUBLE_TAP_GESTURE;
 
     private AmbientDisplayConfiguration mAmbientConfig;
     @UserIdInt
     private final int mUserId;
+
+    private PrimarySwitchPreference mPreference;
+    private SettingObserver mSettingObserver;
 
     public DoubleTapScreenPreferenceController(Context context, String key) {
         super(context, key);
@@ -73,6 +88,29 @@ public class DoubleTapScreenPreferenceController extends GesturePreferenceContro
     }
 
     @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mPreference = screen.findPreference(getPreferenceKey());
+        mSettingObserver = new SettingObserver(mPreference);
+        updateState(mPreference);
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        super.updateState(preference);
+        final ContentResolver resolver = mContext.getContentResolver();
+        final boolean enabled =
+                Settings.Secure.getInt(resolver, SECURE_KEY, isChecked() ? ON : OFF) == ON;
+        String summary;
+        if (enabled) {
+            summary = mContext.getString(R.string.gesture_setting_on);
+        } else {
+            summary = mContext.getString(R.string.gesture_setting_off);
+        }
+        preference.setSummary(summary);
+    }
+
+    @Override
     public boolean isSliceable() {
         return TextUtils.equals(getPreferenceKey(), "gesture_double_tap_screen");
     }
@@ -89,11 +127,6 @@ public class DoubleTapScreenPreferenceController extends GesturePreferenceContro
     }
 
     @Override
-    protected String getVideoPrefKey() {
-        return PREF_KEY_VIDEO;
-    }
-
-    @Override
     public boolean isChecked() {
         return getAmbientConfig().doubleTapGestureEnabled(mUserId);
     }
@@ -103,5 +136,52 @@ public class DoubleTapScreenPreferenceController extends GesturePreferenceContro
             mAmbientConfig = new AmbientDisplayConfiguration(mContext);
         }
         return mAmbientConfig;
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return NO_RES;
+    }
+
+    @Override
+    public void onStart() {
+        if (mSettingObserver != null) {
+            mSettingObserver.register(mContext.getContentResolver());
+            mSettingObserver.onChange(false, null);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        if (mSettingObserver != null) {
+            mSettingObserver.unregister(mContext.getContentResolver());
+        }
+    }
+
+    private class SettingObserver extends ContentObserver {
+        private final Uri mUri = Settings.Secure.getUriFor(SECURE_KEY);
+
+        private final Preference mPreference;
+
+        SettingObserver(Preference preference) {
+            super(Handler.getMain());
+            mPreference = preference;
+        }
+
+        public void register(ContentResolver cr) {
+            cr.registerContentObserver(mUri, false, this);
+        }
+
+        public void unregister(ContentResolver cr) {
+            cr.unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            super.onChange(selfChange, uri);
+            if (uri == null || mUri.equals(uri)) {
+                updateState(mPreference);
+            }
+        }
     }
 }
