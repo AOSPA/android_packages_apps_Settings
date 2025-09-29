380f8219181ff3e0f43c97a8381d41bdfeff631d
Julia Reynolds <juliacr@google.com>
2025-09-29 11:27:32 -0400

    Always show summarization settings
    If the flag to allow app-provided settings is enabled
    
    Flag: android.app.nm_summarization_all
    Fixes: 448078621
    Test: tests/robotests/src/com/android/settings/notification
    Change-Id: I3e7a11305cc429a260db9b8c1e3673d4c18967d1

--- src/com/android/settings/notification/AdjustmentExcludedAppsPreferenceController.java
+++ src/com/android/settings/notification/AdjustmentExcludedAppsPreferenceController.java
@@ -102,8 +102,7 @@ public class AdjustmentExcludedAppsPreferenceController extends BasePreferenceCo
                 || Flags.notificationClassificationUi())) {
             return CONDITIONALLY_UNAVAILABLE;
         }
-        if (KEY_SUMMARIZATION.equals(mAdjustmentKey)
-                && mBackend.isNotificationSummarizationSupported()) {
+        if (KEY_SUMMARIZATION.equals(mAdjustmentKey) && mBackend.showSummarizationSettings()) {
             return AVAILABLE;
         }
         if (KEY_TYPE.equals(mAdjustmentKey) && mBackend.isNotificationBundlingSupported()) {
