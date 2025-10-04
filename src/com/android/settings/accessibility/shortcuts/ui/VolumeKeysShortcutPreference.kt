32d4f9c0fcadf2556c560be7c4db58c5f2c7f2dc
Chun-Ku Lin <chunkulin@google.com>
2025-10-04 02:48:47 +0000

    Create PreferenceMetadata for volume keys shortcut
    Bug: 440383851
    Test: atest SettingsRoboTests
    Flag: com.android.settings.accessibility.catalyst_edit_shortcuts
    Change-Id: I96d308ff92a6b34ecd51b541066fd2fc685862f5

---
+++ src/com/android/settings/accessibility/shortcuts/ui/VolumeKeysShortcutPreference.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.accessibility.shortcuts.ui
+
+import android.content.Context
+import androidx.preference.Preference
+import com.android.internal.accessibility.common.ShortcutConstants.UserShortcutType
+import com.android.settings.R
+import com.android.settings.accessibility.shortcuts.ShortcutOptionPreference as ShortcutOptionWidget
+import com.android.settingslib.metadata.PreferenceMetadata
+
+class VolumeKeysShortcutPreference(context: Context, targets: Set<String>) :
+    ShortcutOptionPreference(context, UserShortcutType.HARDWARE, targets) {
+    override val key: String
+        get() = KEY
+
+    override val title: Int
+        get() = R.string.accessibility_shortcut_edit_dialog_title_hardware
+
+    override val summary: Int
+        get() = R.string.accessibility_shortcut_edit_dialog_summary_hardware
+
+    override fun bind(preference: Preference, metadata: PreferenceMetadata) {
+        super.bind(preference, metadata)
+        if (preference is ShortcutOptionWidget) {
+            preference.setIntroImageResId(R.drawable.accessibility_shortcut_type_volume_keys)
+        }
+    }
+
+    companion object {
+        private const val KEY = "shortcut_volume_keys_pref"
+    }
+}
