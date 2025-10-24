70cf32c3e254d42689bff7f3442f2e1923278547
Nergi Rahardi <nergi@google.com>
2025-10-24 16:04:43 +0900

    Refactor resolution page with viewmodel to integrate refreshrate section
    This is aimed to have a clear separation of data handling with UI. So
    that fragment can focus on the UI flow. This provides:
    - Syncing data with DisplayListener and bundling "Top options", "More
    options" and the refresh rate that should be paired
    - Filtering allowed modes
    - Managing showing / hiding confirmation dialog (TODO)
    - Managing interactions with ConnectedDisplayInjector to apply / reset
    the selected Display.Mode (TODO)
    
    Please see ag/36542568 to see how the usage looks like
    
    Bug: 454516501
    Bug: 425495663
    Test: atest ResolutionRefreshRatePreferenceViewModelTest
    Flag: EXEMPT BUGFIX
    
    Change-Id: I7203cdfa708dc8aae775e65a00589e20d141d71e

---
+++ src/com/android/settings/connecteddevice/display/ResolutionRefreshRatePreferenceViewModel.kt
@@ -0,0 +1,281 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.connecteddevice.display
+
+import android.app.Application
+import android.graphics.Point
+import android.util.Log
+import android.view.Display.Mode
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.android.settings.R
+import com.android.settings.core.instrumentation.SettingsStatsLog
+
+class ResolutionRefreshRatePreferenceViewModel
+@JvmOverloads
+constructor(
+    application: Application,
+    private val displayId: Int,
+    val injector: ConnectedDisplayInjector =
+        ConnectedDisplayInjector(application.applicationContext),
+) : AndroidViewModel(application) {
+
+    data class UiState(
+        val currentActiveMode: Mode,
+        val pendingMode: Mode,
+        val topResolutionItems: List<ResolutionItem> = emptyList(),
+        val moreResolutionItems: List<ResolutionItem> = emptyList(),
+        val refreshRateItems: List<RefreshRateItem> = emptyList(),
+        val areMoreOptionsExpanded: Boolean = false,
+    )
+
+    data class ResolutionItem(val physicalWidth: Int, val physicalHeight: Int)
+
+    data class RefreshRateItem(val modeId: Int, val refreshRate: Float)
+
+    private var allowedModes: Map<Int, Mode> = emptyMap()
+
+    private var peakWidth: Int = 0
+    private var peakHeight: Int = 0
+    private var peakRefreshRate: Int = 0
+    private var isRefreshRateSyncEnabled: Boolean = false
+    private var allowedResolutions: Set<Point> = emptySet()
+
+    private val _uiState = MutableLiveData<UiState?>(null)
+    val uiState: LiveData<UiState?> = _uiState
+
+    private val displayListener =
+        object : ExternalDisplaySettingsConfiguration.DisplayListener() {
+            override fun update(updatedDisplayId: Int) {
+                if (displayId != updatedDisplayId) {
+                    return
+                }
+                reloadData()
+            }
+        }
+
+    init {
+        loadFilterConfiguration()
+        reloadData()
+        injector.registerDisplayListener(displayListener)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        injector.unregisterDisplayListener(displayListener)
+    }
+
+    private fun reloadData() {
+        val display = injector.getDisplay(displayId)
+        // Display is disconnected, reset state
+        if (display == null || display.mode == null) {
+            // Fragment should close itself if UiState is null
+            _uiState.value = null
+            return
+        }
+        val currentActiveMode = display.mode
+        val supportedModes = display.supportedModes
+        allowedModes =
+            supportedModes.filter { isAllowed(it, supportedModes) }.associateBy { it.modeId }
+        // Active mode might not be part of supported modes, but might have previously been set by
+        // the system and should still be displayed
+        allowedModes += (currentActiveMode.modeId to currentActiveMode)
+
+        logInfo("Currently selected display mode: $currentActiveMode")
+        val resolutionGroups =
+            allowedModes.values
+                .distinctBy { Point(it.physicalWidth, it.physicalHeight) }
+                .map { it.toResolutionItem() }
+                .sortedWith(
+                    compareByDescending<ResolutionItem> { it.physicalWidth }
+                        .thenByDescending { it.physicalHeight }
+                )
+
+        val moreItems = resolutionGroups.drop(TOP_MODE_RES_MAX_COUNT)
+        val previouslyExpanded = _uiState.value?.areMoreOptionsExpanded ?: false
+        val shouldExpand =
+            previouslyExpanded || moreItems.any { currentActiveMode.hasSameResolutionAs(it) }
+        _uiState.value =
+            UiState(
+                currentActiveMode = currentActiveMode,
+                pendingMode = currentActiveMode,
+                topResolutionItems = resolutionGroups.take(TOP_MODE_RES_MAX_COUNT),
+                moreResolutionItems = moreItems,
+                refreshRateItems = buildRefreshRateItems(currentActiveMode),
+                areMoreOptionsExpanded = shouldExpand,
+            )
+    }
+
+    private fun isAllowed(mode: Mode, displaySupportedModes: List<Mode>): Boolean {
+        val supportedModesMap = displaySupportedModes.associateBy { it.modeId }
+        if (
+            isRefreshRateSyncEnabled &&
+                (mode.refreshRate < (DEFAULT_LOW_REFRESH_RATE - 1) ||
+                    mode.refreshRate > (DEFAULT_LOW_REFRESH_RATE + 1))
+        ) {
+            logDebug("$mode refresh rate is out of synchronization range")
+            return false
+        }
+        if (peakHeight > 0 && mode.physicalHeight > peakHeight) {
+            logDebug("$mode height is above the allowed limit")
+            return false
+        }
+        if (peakWidth > 0 && mode.physicalWidth > peakWidth) {
+            logDebug("$mode width is above the allowed limit")
+            return false
+        }
+        if (peakRefreshRate > 0 && mode.refreshRate > peakRefreshRate) {
+            logDebug("$mode refresh rate is above the allowed limit")
+            return false
+        }
+
+        // If mode filtering is defined, only modes in the whitelist are allowed.
+        if (allowedResolutions.isNotEmpty()) {
+            // If this is an anisotropic mode, check if its base mode is supported.
+            val modeToCheck =
+                if ((mode.flags and Mode.FLAG_ANISOTROPY_CORRECTION) != 0) {
+                    supportedModesMap[mode.parentModeId]
+                } else {
+                    mode
+                }
+            return modeToCheck?.let {
+                allowedResolutions.contains(Point(it.physicalWidth, it.physicalHeight))
+            } ?: false
+        }
+
+        return true
+    }
+
+    private fun loadFilterConfiguration() {
+        val res = getApplication<Application>().resources
+        peakRefreshRate =
+            res.getInteger(com.android.internal.R.integer.config_externalDisplayPeakRefreshRate)
+        peakWidth = res.getInteger(com.android.internal.R.integer.config_externalDisplayPeakWidth)
+        peakHeight = res.getInteger(com.android.internal.R.integer.config_externalDisplayPeakHeight)
+        isRefreshRateSyncEnabled =
+            res.getBoolean(com.android.internal.R.bool.config_refreshRateSynchronizationEnabled)
+
+        val resolutionsArray = res.getIntArray(R.array.config_resolutionsShownOnExternalDisplay)
+        allowedResolutions =
+            resolutionsArray
+                .asSequence()
+                .chunked(2)
+                .filter { it.size == 2 }
+                .map { Point(it[0], it[1]) }
+                .toSet()
+        logDebug("peakRefreshRate=$peakRefreshRate")
+        logDebug("peakWidth=$peakWidth")
+        logDebug("peakHeight=$peakHeight")
+        logDebug("isRefreshRateSyncEnabled=$isRefreshRateSyncEnabled")
+        logDebug("allowedResolutions=$allowedResolutions")
+    }
+
+    fun onResolutionSelected(item: ResolutionItem) {
+        val currentState = _uiState.value ?: return
+        // Pre-select the highest refresh rate for the selected resolution
+        val newPendingMode =
+            allowedModes.values
+                .filter { it.hasSameResolutionAs(item) }
+                .maxByOrNull { it.refreshRate }
+        // Selected mode must be coming from allowedModes, and should exist here
+        if (newPendingMode == null) {
+            logWarn("No supported mode found for resolution item: $item")
+            return
+        }
+        // Ignore state update if modeId is the same
+        if (newPendingMode.modeId == currentState.pendingMode.modeId) return
+
+        updateState {
+            it.copy(
+                pendingMode = newPendingMode,
+                refreshRateItems = buildRefreshRateItems(newPendingMode),
+            )
+        }
+    }
+
+    fun onRefreshRateSelected(modeId: Int) {
+        val currentState = _uiState.value ?: return
+        // Selected mode must be coming from allowedModes, and should exist here
+        val newPendingMode = allowedModes[modeId]
+        if (newPendingMode == null) {
+            logWarn("Mode $modeId not found")
+            return
+        }
+        // Ignore state update if modeId is the same
+        if (newPendingMode.modeId == currentState.pendingMode.modeId) return
+
+        updateState { it.copy(pendingMode = newPendingMode) }
+    }
+
+    fun onMoreOptionsExpanded() {
+        updateState { it.copy(areMoreOptionsExpanded = true) }
+    }
+
+    private fun buildRefreshRateItems(resolutionSelectedMode: Mode) =
+        allowedModes.values
+            .filter { it.hasSameResolutionAs(resolutionSelectedMode) }
+            .sortedByDescending { it.refreshRate }
+            .map { it.toRefreshRateItem() }
+
+    private fun updateState(updateAction: (UiState) -> UiState) {
+        val currentState = _uiState.value ?: return
+        _uiState.value = updateAction(currentState)
+    }
+
+    private fun logResolutionChange(mode: Mode) {
+        val logger = ExternalDisplaySettingsLoggerStore.getLogger(displayId)
+        logger.updateResolution(mode.physicalWidth, mode.physicalHeight)
+        logger.log(SettingsStatsLog.EXTERNAL_DISPLAY_SETTINGS_CHANGED__SETTING__RESOLUTION)
+    }
+
+    private fun logDebug(message: String) {
+        Log.d(TAG, "[Display#$displayId] $message")
+    }
+
+    private fun logInfo(message: String) {
+        Log.i(TAG, "[Display#$displayId] $message")
+    }
+
+    private fun logWarn(message: String) {
+        Log.w(TAG, "[Display#$displayId] $message")
+    }
+
+    companion object {
+
+        private fun Mode.toResolutionItem(): ResolutionItem =
+            ResolutionItem(this.physicalWidth, this.physicalHeight)
+
+        private fun Mode.toRefreshRateItem(): RefreshRateItem =
+            RefreshRateItem(this.modeId, this.refreshRate)
+
+        private fun Mode.hasSameResolutionAs(other: Mode?): Boolean {
+            if (other == null) return false
+            return this.physicalWidth == other.physicalWidth &&
+                this.physicalHeight == other.physicalHeight
+        }
+
+        private fun Mode.hasSameResolutionAs(other: ResolutionItem): Boolean {
+            return this.physicalWidth == other.physicalWidth &&
+                this.physicalHeight == other.physicalHeight
+        }
+
+        private const val TAG = "ResRefreshRatePref"
+        private const val DEFAULT_LOW_REFRESH_RATE = 60
+        const val TOP_MODE_RES_MAX_COUNT = 3
+    }
+}
---
+++ tests/robotests/src/com/android/settings/connecteddevice/display/ResolutionRefreshRatePreferenceViewModelTest.kt
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.connecteddevice.display
+
+import android.app.Application
+import android.view.Display.Mode
+import androidx.lifecycle.Observer
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.settings.connecteddevice.display.ResolutionRefreshRatePreferenceViewModel.RefreshRateItem
+import com.android.settings.connecteddevice.display.ResolutionRefreshRatePreferenceViewModel.ResolutionItem
+import com.android.settings.connecteddevice.display.ResolutionRefreshRatePreferenceViewModel.UiState
+import com.android.settings.testutils.InstantTaskExecutorRule
+import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.kotlin.whenever
+
+/** Unit test for [ResolutionRefreshRatePreferenceViewModel] */
+@RunWith(AndroidJUnit4::class)
+class ResolutionRefreshRatePreferenceViewModelTest : ExternalDisplayTestBase() {
+
+    // Rule to execute LiveData operations synchronously
+    @get:Rule val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Mock private lateinit var uiStateObserver: Observer<UiState?>
+
+    private lateinit var viewModel: ResolutionRefreshRatePreferenceViewModel
+    private lateinit var application: Application
+
+    private val externalDisplay by lazy { mDisplays.find { it.id == EXTERNAL_DISPLAY_ID }!! }
+
+    @Before
+    override fun setUp() {
+        super.setUp()
+        application = ApplicationProvider.getApplicationContext()
+    }
+
+    @After
+    fun tearDown() {
+        viewModel.uiState.removeObserver(uiStateObserver)
+    }
+
+    private fun setupViewModel(displayId: Int = EXTERNAL_DISPLAY_ID) {
+        viewModel =
+            ResolutionRefreshRatePreferenceViewModel(application, displayId, mMockedInjector)
+        viewModel.uiState.observeForever(uiStateObserver)
+    }
+
+    @Test
+    fun init_loadsCorrectInitialState() {
+        setupViewModel()
+        val state = viewModel.uiState.value!!
+        val mode = externalDisplay.mode!!
+
+        assertThat(state.currentActiveMode).isEqualTo(mode)
+        assertThat(state.pendingMode).isEqualTo(mode)
+
+        assertThat(state.topResolutionItems)
+            .containsExactly(
+                ResolutionItem(physicalWidth = 2048, physicalHeight = 1024),
+                ResolutionItem(physicalWidth = 1920, physicalHeight = 1080),
+                ResolutionItem(physicalWidth = 800, physicalHeight = 600),
+            )
+            .inOrder()
+
+        assertThat(state.moreResolutionItems)
+            .containsExactly(
+                ResolutionItem(physicalWidth = 760, physicalHeight = 600),
+                ResolutionItem(physicalWidth = 720, physicalHeight = 480),
+                ResolutionItem(physicalWidth = 640, physicalHeight = 480),
+                ResolutionItem(physicalWidth = 320, physicalHeight = 240),
+            )
+            .inOrder()
+
+        assertThat(state.refreshRateItems).hasSize(1)
+        assertThat(state.refreshRateItems.first().modeId).isEqualTo(mode.modeId)
+        assertThat(state.refreshRateItems.first().refreshRate).isEqualTo(mode.refreshRate)
+    }
+
+    @Test
+    fun init_withUnsupportedActiveMode_includesMode() {
+        val displayId = 123
+        val width = 765
+        val height = 432
+        val unsupportedMode = Mode(234, width, height, 240f)
+        val updatedEnabledDisplays = mDisplays.toMutableList()
+        updatedEnabledDisplays.add(
+            DisplayDevice(
+                displayId,
+                "local:1111111111",
+                "test",
+                unsupportedMode,
+                externalDisplay.supportedModes,
+                /* isEnabled= */ DisplayIsEnabled.YES,
+                /* isConnectedDisplay= */ true,
+                /* rotation= */ 0,
+            )
+        )
+        updateDisplaysAndTopology(updatedEnabledDisplays)
+
+        setupViewModel(displayId)
+        val state = viewModel.uiState.value!!
+
+        assertThat(state.currentActiveMode).isEqualTo(unsupportedMode)
+        assertThat(state.pendingMode).isEqualTo(unsupportedMode)
+
+        val allResolutions = state.topResolutionItems + state.moreResolutionItems
+        assertThat(allResolutions).contains(ResolutionItem(width, height))
+    }
+
+    @Test
+    fun displayDisconnected_emitsEmptyState() {
+        setupViewModel()
+        whenever(mMockedInjector.getDisplay(EXTERNAL_DISPLAY_ID)).thenReturn(null)
+
+        mListener.update(EXTERNAL_DISPLAY_ID)
+        assertThat(viewModel.uiState.value).isNull()
+    }
+
+    @Test
+    fun onResolutionSelected_newResolution_selectsHighestRefreshRateAndUpdatesState() {
+        setupViewModel()
+        // This resolution has 50Hz and 60Hz refresh rate variants
+        val newResolutionItem = ResolutionItem(physicalWidth = 640, physicalHeight = 480)
+
+        viewModel.onResolutionSelected(newResolutionItem)
+        val state = viewModel.uiState.value!!
+
+        val expectedHighestRefreshRateMode = externalDisplay.supportedModes.find { it.modeId == 3 }
+        assertThat(expectedHighestRefreshRateMode).isNotNull()
+        assertThat(state.pendingMode).isEqualTo(expectedHighestRefreshRateMode)
+        assertThat(state.refreshRateItems).hasSize(2)
+        assertThat(state.refreshRateItems)
+            .containsExactly(
+                RefreshRateItem(modeId = 3, refreshRate = 60f),
+                RefreshRateItem(modeId = 4, refreshRate = 50f),
+            )
+            .inOrder()
+    }
+
+    @Test
+    fun onRefreshRateSelected_updatesPendingMode() {
+        setupViewModel()
+        val currentActiveMode = viewModel.uiState.value!!.currentActiveMode
+
+        val resolutionToSelect = ResolutionItem(physicalWidth = 640, physicalHeight = 480)
+        viewModel.onResolutionSelected(resolutionToSelect)
+
+        val targetModeAt50Hz = externalDisplay.supportedModes.find { it.modeId == 4 }
+        assertThat(targetModeAt50Hz).isNotNull()
+
+        viewModel.onRefreshRateSelected(targetModeAt50Hz!!.modeId)
+        val state = viewModel.uiState.value!!
+
+        assertThat(state.pendingMode).isEqualTo(targetModeAt50Hz)
+        assertThat(state.currentActiveMode).isEqualTo(currentActiveMode)
+    }
+}
