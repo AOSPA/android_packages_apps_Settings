472a91248121a2477d238c0c01fc33787b583fbe
Ganesh Vilas Chaudhari <ganeshvi@qti.qualcomm.com>
2025-11-28 10:32:19 +0530

    Update the copyrights marking.
    Change-Id: I93890df6d3e9815ac4534a4ce88ec180fb434d06
    CRs-Fixed: 4344069

--- src/com/android/settings/network/telephony/TelephonyUtils.java
+++ src/com/android/settings/network/telephony/TelephonyUtils.java
@@ -29,7 +29,7 @@
  */
 
 /*
- * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
  * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
