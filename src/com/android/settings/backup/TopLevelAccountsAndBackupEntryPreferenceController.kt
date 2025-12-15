740ff05b4732df297a0aec7f0209e8795869a1cf
Ruslan Tkhakokhov <rthakohov@google.com>
2025-12-15 17:29:09 +0000

    Add profile selection dialog to Accounts and backup
    Bug: 436614052
    Flag: com.android.settings.flags.enable_accounts_and_backup_screen
    Test: build and verify the dialog shows up and takes user to the correct
    page
    
    Change-Id: Ided073b64ad0440e0aadd4beab26566e30e3fdb7

--- src/com/android/settings/backup/TopLevelAccountsAndBackupEntryPreferenceController.kt
+++ src/com/android/settings/backup/TopLevelAccountsAndBackupEntryPreferenceController.kt
@@ -16,14 +16,62 @@
 
 package com.android.settings.backup
 
+import android.app.Dialog
 import android.content.Context
+import android.content.Intent
+import android.os.UserHandle
+import android.os.UserManager
+import androidx.preference.Preference
+import com.android.settings.Settings.AccountsAndBackupDashboardActivity
+import com.android.settings.SettingsLaunchpadActivity
 import com.android.settings.core.BasePreferenceController
+import com.android.settings.dashboard.profileselector.ProfileSelectDialog
+import com.android.settings.dashboard.profileselector.UserAdapter
 import com.android.settings.flags.Flags
+import java.lang.ref.WeakReference
 
-class TopLevelAccountsAndBackupEntryPreferenceController(context: Context, key: String) :
-    BasePreferenceController(context, key) {
+class TopLevelAccountsAndBackupEntryPreferenceController(
+    context: Context,
+    key: String,
+) : BasePreferenceController(context, key) {
+
+    private var profileSelectDialog: WeakReference<Dialog> = WeakReference(null)
 
     override fun getAvailabilityStatus(): Int {
         return if (Flags.enableAccountsAndBackupScreen()) AVAILABLE else CONDITIONALLY_UNAVAILABLE
+    }
+
+    override fun handlePreferenceTreeClick(preference: Preference): Boolean {
+        if (preference.key == preferenceKey) {
+            // Manually create the Intent to launch the correct Catalyst activity.
+            val intent =
+                Intent(mContext, AccountsAndBackupDashboardActivity::class.java).apply {
+                    putExtra(
+                        SettingsLaunchpadActivity.EXTRA_SCREEN_KEY,
+                        AccountsAndBackupScreen.KEY,
+                    )
+                }
+
+            val userHandles = UserManager.get(mContext).enabledProfiles
+            if (userHandles.size <= 1) {
+                mContext.startActivityAsUser(intent, userHandles.get(UserHandle.myUserId())!!)
+                return true
+            }
+
+            val clickListener =
+                UserAdapter.OnClickListener { position: Int ->
+                    val selectedUser = userHandles.get(position)
+                    mContext.startActivityAsUser(intent, selectedUser)
+                    profileSelectDialog.get()?.dismiss()
+                }
+
+            val dialog = ProfileSelectDialog.createDialog(mContext, userHandles, clickListener)
+            profileSelectDialog = WeakReference(dialog)
+            dialog.show()
+
+            return true
+        }
+
+        return super.handlePreferenceTreeClick(preference)
     }
 }
