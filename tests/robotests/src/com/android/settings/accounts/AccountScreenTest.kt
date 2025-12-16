7593b7a68b5c05badcc6cda55a6124958bf1bcda
Jason Chiu <chiujason@google.com>
2025-12-16 15:36:47 +0800

    [Catalyst] Clean up the TS flag catalyst_accounts_screen
    NO_IFTTT=Remove flag
    
    Fix: 467481397
    Test: robotest
    Flag: EXEMPT FLAG_REMOVAL
    Change-Id: I48131fc979a27f5c8eaa39104d569492f221307d

--- tests/robotests/src/com/android/settings/accounts/AccountScreenTest.kt
+++ tests/robotests/src/com/android/settings/accounts/AccountScreenTest.kt
@@ -15,16 +15,43 @@
  */
 package com.android.settings.accounts
 
-import com.android.settings.flags.Flags
-import com.android.settings.testutils2.SettingsCatalystTestCase
-import com.android.settings.testutils.shadow.ShadowAccountManager
-import com.android.settings.testutils.shadow.ShadowContentResolver
-import org.robolectric.annotation.Config
+import android.content.Context
+import android.platform.test.annotations.DisableFlags
+import android.platform.test.annotations.EnableFlags
+import android.platform.test.flag.junit.SetFlagsRule
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.settings.R
+import com.android.settingslib.widget.theme.flags.Flags.FLAG_IS_EXPRESSIVE_DESIGN_ENABLED
+import com.google.common.truth.Truth.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
-@Config(shadows = [ShadowAccountManager::class, ShadowContentResolver::class])
-class AccountScreenTest : SettingsCatalystTestCase() {
-    override val preferenceScreenCreator = AccountScreen()
+@RunWith(AndroidJUnit4::class)
+class AccountScreenTest {
 
-    override val flagName: String
-        get() = Flags.FLAG_CATALYST_ACCOUNTS_SCREEN
+    @get:Rule val setFlagsRule = SetFlagsRule()
+
+    private val preferenceScreen = AccountScreen()
+
+    private val context: Context = ApplicationProvider.getApplicationContext()
+
+    @Test
+    fun key_isEqualToStatic() {
+        assertThat(preferenceScreen.key).isEqualTo(AccountScreen.KEY)
+    }
+
+    @Test
+    @EnableFlags(FLAG_IS_EXPRESSIVE_DESIGN_ENABLED)
+    fun getIcon_isExpressiveTheme() {
+        assertThat(preferenceScreen.getIcon(context)).isEqualTo(R.drawable.ic_homepage_accounts)
+    }
+
+    @Test
+    @DisableFlags(FLAG_IS_EXPRESSIVE_DESIGN_ENABLED)
+    fun getIcon_notExpressiveTheme() {
+        assertThat(preferenceScreen.getIcon(context))
+            .isEqualTo(R.drawable.ic_settings_passwords_filled)
+    }
 }
