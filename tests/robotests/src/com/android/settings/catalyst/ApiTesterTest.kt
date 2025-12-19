61e47d1b30db0d7b0b8b79fbd11caf1f82a1c639
tchirila <tchirila@google.com>
2025-12-19 12:55:40 +0000

    [Catalyst][API First] add getLaunchIntent in ApiTester
    Bug: 467339223
    Test: atest ApiTesterTest
    Flag: EXEMPT TEST_ONLY
    
    Change-Id: Ie8a03ca291f14337ecfaefb3296e4d436a6a636c

--- tests/robotests/src/com/android/settings/catalyst/ApiTesterTest.kt
+++ tests/robotests/src/com/android/settings/catalyst/ApiTesterTest.kt
@@ -16,14 +16,13 @@
 
 package com.android.settings.catalyst
 
-import android.Manifest
+import android.Manifest.permission.INTERACT_ACROSS_PROFILES
 import android.platform.test.annotations.DisableFlags
 import android.platform.test.annotations.EnableFlags
 import android.platform.test.flag.junit.SetFlagsRule
 import androidx.fragment.app.Fragment
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.settings.R;
-import com.android.settings.SettingsApplication
 import com.android.settings.flags.Flags
 import com.android.settings.flags.Flags.FLAG_CATALYST_MIGRATION_26Q2
 import com.android.settings.testutils2.ApiTester
@@ -31,8 +30,6 @@ import com.android.settings.testutils2.CannotSetException
 import com.android.settings.testutils2.FailedPreconditionException
 import com.android.settings.testutils2.HardwareUnsupportedException
 import com.android.settings.testutils2.MissingPermissionException
-import com.android.settingslib.metadata.ProvidePreferenceScreen
-import com.android.settingslib.metadata.ProvidePreferenceScreenOptions
 import com.android.settingslib.metadata.preferencesapi.PreferencesApiScreen
 import com.android.settingslib.metadata.preferencesapi.category.Category
 import com.android.settingslib.metadata.preferencesapi.preconditions.Allowed
@@ -44,10 +41,10 @@ import kotlin.test.assertFailsWith
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
 class ApiTesterTest {
+
     class TestScreen : PreferencesApiScreen(
         key = "A",
         topLevelSettingsCategory = Category.SYSTEM,
@@ -70,11 +67,38 @@ class ApiTesterTest {
                 }
             }
             preference(
+                key = "preference_which_fails_permissions_in_get",
+                purpose = 0,
+                type = AnyString
+            ) {
+                get {
+                    permissions(listOf(INTERACT_ACROSS_PROFILES))
+                    execute {
+                        "Hello"
+                    }
+                }
+            }
+            preference(
+                key = "preference_which_fails_permissions_in_set",
+                purpose = 0,
+                type = AnyString
+            ) {
+                get {
+                    execute {
+                        "Hey"
+                    }
+                }
+                set {
+                    permissions(listOf(INTERACT_ACROSS_PROFILES))
+                    execute { }
+                }
+            }
+            preference(
                 key = "preference_which_requires_interact_across_profiles",
                 purpose = 0,
                 type = AnyString
             ) {
-                permissions(listOf(Manifest.permission.INTERACT_ACROSS_PROFILES))
+                permissions(listOf(INTERACT_ACROSS_PROFILES))
                 get {
                     execute {
                         "Hello"
@@ -156,7 +180,69 @@ class ApiTesterTest {
             }
         }
     }
+
+    class FailingPermissionScreen : PreferencesApiScreen(
+        key = "B",
+        topLevelSettingsCategory = Category.SYSTEM,
+        fragment = Fragment::class,
+        purpose = 0
+    ) {
+        init {
+            flag {
+                Flags.catalystMigration26q2()
+            }
+            permissions(listOf(INTERACT_ACROSS_PROFILES))
+            preference(
+                key = "main_pref",
+                purpose = 0,
+                type = AnyString
+            ) {
+            get {
+                execute {
+                    "Hello"
+                }
+            }
+            set {
+                execute {}
+            }
+        }
+        }
+    }
+
+    class FailingPreconditionScreen : PreferencesApiScreen(
+        key = "C",
+        topLevelSettingsCategory = Category.SYSTEM,
+        fragment = Fragment::class,
+        purpose = 0
+    ) {
+        init {
+            flag {
+                Flags.catalystMigration26q2()
+            }
+            preconditions(0){
+                HardwareUnsupported(R.string.hardware_unsupported_exception)
+            }
+            preference(
+                key = "main_pref",
+                purpose = 0,
+                type = AnyString
+            ) {
+                get {
+                    execute {
+                        "Hello"
+                    }
+                }
+                set {
+                    execute {}
+                }
+            }
+        }
+    }
+
     val tester = ApiTester(TestScreen())
+    val testerFailingPermissions = ApiTester(FailingPermissionScreen())
+    val testerFailingPreconditions = ApiTester(FailingPreconditionScreen())
+
     @get:Rule val setFlagsRule = SetFlagsRule()
 
     @Test
@@ -169,6 +255,36 @@ class ApiTesterTest {
     @DisableFlags(FLAG_CATALYST_MIGRATION_26Q2)
     fun getScreen_flagDisabled_isNull() {
         Truth.assertThat(tester.getScreen()).isNull()
+    }
+
+    @Test
+    fun get_failingScreenPermissions_throwsError() {
+        assertFailsWith<MissingPermissionException> { testerFailingPermissions.get<String>("main_pref") }
+    }
+
+    @Test
+    fun set_failingScreenPermissions_throwsError() {
+        assertFailsWith<MissingPermissionException> { testerFailingPermissions.set<String>("main_pref", "") }
+    }
+
+    @Test
+    fun get_failingScreenPreconditions_throwsError() {
+        assertFailsWith<HardwareUnsupportedException> { testerFailingPreconditions.get<String>("main_pref") }
+    }
+
+    @Test
+    fun set_failingScreenPreconditions_throwsError() {
+        assertFailsWith<HardwareUnsupportedException> { testerFailingPreconditions.set<String>("main_pref", "") }
+    }
+
+    @Test
+    fun get_failingGetPermissions_throwsError() {
+        assertFailsWith<MissingPermissionException> { tester.get<String>("preference_which_fails_permissions_in_get") }
+    }
+
+    @Test
+    fun get_failingSetPermissions_throwsError() {
+        assertFailsWith<MissingPermissionException> { tester.set<String>("preference_which_fails_permissions_in_set", "") }
     }
 
     @Test
@@ -214,5 +330,24 @@ class ApiTesterTest {
     @Test
     fun set_failsValuePreconditions_throwsException() {
         assertFailsWith<FailedPreconditionException> { tester.set("preference_with_setter_and_getter_cant_set_a", "a") }
+    }
+
+    @Test
+    fun launchIntent_onScreenPermissionFailure_fails() {
+        assertFailsWith<MissingPermissionException> {
+            testerFailingPermissions.getLaunchIntent()
+        }
+    }
+
+    @Test
+    fun launchIntent_onScreenPreconditionsFailure_fails() {
+        assertFailsWith<HardwareUnsupportedException> {
+            testerFailingPreconditions.getLaunchIntent()
+        }
+    }
+
+    @Test
+    fun launchIntent_correctScreen_hasIntent() {
+        Truth.assertThat(tester.getLaunchIntent()).isNotNull()
     }
 }
