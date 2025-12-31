7faa47df63a7b669da0dc06921a838a1ce6b75b2
Menghan Li <menghanli@google.com>
2025-12-31 02:57:17 +0000

    feat(A11ySuW): Impl new accessibility service entry with setup lib
    Bug: 467927243
    Bug: 467928474
    Test: atest AccessibilityServiceItemControllerTest
    Test: atest AccessibilityServiceSetupWizardFragmentTest
    Test: atest AccessibilitySetupWizardFragmentTest
    Flag: com.android.settings.accessibility.enable_setuplib_in_a11y_setup_page
    Change-Id: Ieb6f012cb08c3a10b96b9a25204bb021283cd2df

---
+++ src/com/android/settings/accessibility/setupwizard/AccessibilityServiceSetupWizardFragment.kt
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.accessibility.setupwizard
+
+import android.content.ComponentName
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import com.android.settings.R
+import com.android.settings.accessibility.data.AccessibilityRepositoryProvider
+import com.android.settings.accessibility.extensions.getFeatureName
+import com.google.android.setupdesign.GlifLayout
+import com.google.android.setupdesign.items.RecyclerItemAdapter
+
+/** Color inversion for Setup Wizard. */
+class AccessibilityServiceSetupWizardFragment : BaseSetupWizardFragment() {
+
+    override fun createControllers(adapter: RecyclerItemAdapter): Map<Int, BaseItemController> =
+        emptyMap()
+
+    override val fragmentLayoutResId: Int = R.layout.accessibility_service_suw_screen
+
+    override val glifLayoutId: Int = R.id.accessibility_service_suw_screen_layout
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        val glifLayout = view as? GlifLayout ?: return view
+
+        val context = requireContext()
+        val componentName = arguments?.getParcelable(ARG_COMPONENT_NAME, ComponentName::class.java)
+        componentName?.let {
+            val serviceInfo =
+                AccessibilityRepositoryProvider.get(context).getAccessibilityServiceInfo(it)
+
+            // Set Header Text
+            glifLayout.headerText = serviceInfo?.getFeatureName(context)
+
+            // Set Description Text
+            glifLayout.descriptionText = arguments?.getString(ARG_DESCRIPTION)
+        }
+
+        return view
+    }
+
+    companion object {
+        const val ARG_COMPONENT_NAME = "arg_component_name"
+        const val ARG_DESCRIPTION = "arg_description"
+
+        /**
+         * Use this factory method to create a new instance of this fragment using the provided
+         * parameters.
+         */
+        fun newInstance(
+            componentName: ComponentName,
+            description: String,
+        ): AccessibilityServiceSetupWizardFragment =
+            AccessibilityServiceSetupWizardFragment().apply {
+                arguments =
+                    Bundle().apply {
+                        putParcelable(ARG_COMPONENT_NAME, componentName)
+                        putString(ARG_DESCRIPTION, description)
+                    }
+            }
+
+        fun show(
+            fragmentManager: FragmentManager,
+            containerId: Int,
+            componentName: ComponentName,
+            description: String,
+        ) {
+            val fragment = newInstance(componentName, description)
+            fragmentManager
+                .beginTransaction()
+                .replace(containerId, fragment)
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+}
