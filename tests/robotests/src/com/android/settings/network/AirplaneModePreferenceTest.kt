a99d4164f1cc53e7c9fd1acf5d8984aba16b18c3
Ramprakash Krishnan <ramprakashk@google.com>
2026-01-06 18:10:48 -0800

    Check the System Data Sync Flag to show new APM UI
    Show the new UI only if the watch supports Airplane Mode Sync
    
    Bug: 471486951
    Flag: com.android.server.connectivity.sync_airplane_mode_with_watches
    Test: atest SettingsRoboTests
    Change-Id: If0112bdfab54f22a79299e9e8f86155181cec51f

--- tests/robotests/src/com/android/settings/network/AirplaneModePreferenceTest.kt
+++ tests/robotests/src/com/android/settings/network/AirplaneModePreferenceTest.kt
@@ -24,6 +24,7 @@ import android.companion.AssociationInfo
 import android.companion.AssociationRequest
 import android.companion.CompanionDeviceManager
 import android.companion.CompanionDeviceManager.FEATURE_CROSS_DEVICE_SYNC
+import android.companion.CompanionDeviceManager.FLAG_AIRPLANE_MODE
 import android.companion.Flags.FLAG_ENABLE_DATA_SYNC
 import android.content.Context.COMPANION_DEVICE_SERVICE
 import android.content.ContextWrapper
@@ -346,6 +347,7 @@ class AirplaneModePreferenceTest {
                         .setDeviceProfile(AssociationRequest.DEVICE_PROFILE_WATCH)
                         .setDisplayName("Smart Watch")
                         .setMetadata(metadata)
+                        .setSystemDataSyncFlags(FLAG_AIRPLANE_MODE)
                         .build()
                 )
             on { getLocalMetadata(UserHandle.USER_ALL) } doReturn metadata
