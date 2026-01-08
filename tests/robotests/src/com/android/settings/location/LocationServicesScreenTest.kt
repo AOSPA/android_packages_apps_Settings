ff03e7771d3c88dd154c27bdbbb0e8679a4abd3a
Sunny Shao <sunnyshao@google.com>
2026-01-08 07:17:27 +0000

    [Catalyst] Remove the deeplink_others_25q4 TS flag
    NO_IFTTT=Remove flag
    
    Fixes: 467480440
    Test: atest
    Flag: EXEMPT FLAG_REMOVAL
    Change-Id: Ie437a0e99df3a656e4ced9dd7659643bdc119a00

--- tests/robotests/src/com/android/settings/location/LocationServicesScreenTest.kt
+++ tests/robotests/src/com/android/settings/location/LocationServicesScreenTest.kt
@@ -16,29 +16,31 @@
 
 package com.android.settings.location
 
+import android.content.Context
 import android.content.ContextWrapper
 import android.content.res.Resources
-import com.android.settings.flags.Flags
-import com.android.settings.testutils2.SettingsCatalystTestCase
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.ArgumentMatchers.anyInt
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.stub
 
-class LocationServicesScreenTest : SettingsCatalystTestCase() {
-    override val flagName: String
-        get() = Flags.FLAG_DEEPLINK_OTHERS_25Q4
+@RunWith(AndroidJUnit4::class)
+class LocationServicesScreenTest {
 
     private val mockResources = mock<Resources>()
+    private val appContext: Context = ApplicationProvider.getApplicationContext()
 
     private val context =
         object : ContextWrapper(appContext) {
             override fun getResources(): Resources = mockResources
         }
 
-    override val preferenceScreenCreator = LocationServicesScreen()
+    private val preferenceScreenCreator = LocationServicesScreen()
 
     @Test
     fun isAvailable_configIsTrue_returnsTrue() {
