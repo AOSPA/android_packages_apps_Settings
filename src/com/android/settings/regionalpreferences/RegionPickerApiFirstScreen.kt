1adebd1cbfca40b98cb8e425a0dc3584d0ff2873
Zoey Chen <zoeychen@google.com>
2026-01-14 15:50:45 +0000

    [Api First] Radio design: GET/SET for default system language's region
    NO_IFTTT=API First only
    
    Bug: 469279332
    Test: atest RegionPickerApiFirstScreenTest
    Flag: com.android.settings.flags.catalyst_migration_26q2
    Change-Id: I19e37bd21487f62b461b18dc29c28eb22440a492

--- src/com/android/settings/regionalpreferences/RegionPickerApiFirstScreen.kt
+++ src/com/android/settings/regionalpreferences/RegionPickerApiFirstScreen.kt
@@ -16,11 +16,19 @@
 
 package com.android.settings.regionalpreferences
 
+import android.content.Context
+import android.os.LocaleList
+import com.android.internal.app.LocalePicker
+import com.android.internal.app.LocaleStore
+import com.android.internal.app.SystemLocaleCollector
 import com.android.settings.R
 import com.android.settings.flags.Flags
 import com.android.settingslib.metadata.ProvidePreferenceScreen
 import com.android.settingslib.metadata.preferencesapi.PreferencesApiScreen
 import com.android.settingslib.metadata.preferencesapi.category.Category
+import com.android.settingslib.metadata.preferencesapi.types.GeneratedType
+import com.android.settingslib.metadata.preferencesapi.types.GeneratedValue
+import java.util.Locale
 
 // LINT.IfChange
 @ProvidePreferenceScreen(RegionPickerApiFirstScreen.KEY)
@@ -34,10 +42,101 @@ class RegionPickerApiFirstScreen :
 
     init {
         flag { Flags.catalystMigration26q2() }
+
+        preference(
+            key = KEY_PREFERENCE,
+            purpose = R.string.system_locale_region_picker_preference_purpose,
+            type =
+                GeneratedType(description = R.string.default_system_language_region_description) {
+                    val defaultLocaleInfo = LocaleStore.getLocaleInfo(Locale.getDefault())
+                    val parentLocale = defaultLocaleInfo.parent
+                    val parentLocaleInfo = LocaleStore.getLocaleInfo(parentLocale)
+                    val localeInfoList = getSupportedLanguageList(context, parentLocaleInfo)
+                    // Only show the default system language's region
+                    // value: country code, for example, US, UK, IN
+                    // description: full country name, for example, United State, United Kingdom,
+                    // India
+                    localeInfoList.add(defaultLocaleInfo)
+                    localeInfoList.map {
+                        GeneratedValue(it.locale.country, it.fullCountryNameNative)
+                    }
+                },
+        ) {
+            get { execute { Locale.getDefault().country } }
+            set {
+                execute { value ->
+                    val locale =
+                        Locale.Builder()
+                            .setLocale(Locale.getDefault())
+                            .setRegion(value)
+                            .build()
+                    updateRegion(locale)
+                }
+            }
+        }
+    }
+
+    private fun getSupportedLanguageList(
+        context: Context,
+        parent: LocaleStore.LocaleInfo?,
+    ): MutableList<LocaleStore.LocaleInfo> {
+        return SystemLocaleCollector(context, null)
+            .getSupportedLocaleList(parent, false, true)
+            .toMutableList()
+    }
+
+    private fun updateRegion(selectedLocale: Locale) {
+        val newLocales = getUpdatedLocales(selectedLocale)
+        val localeList = LocaleList(*newLocales)
+        LocaleList.setDefault(localeList)
+        LocalePicker.updateLocales(localeList)
+    }
+
+    private fun getUpdatedLocales(selectedLocale: Locale): Array<Locale?> {
+        val localeList = LocaleList.getDefault()
+        val newLocales = arrayOfNulls<Locale>(localeList.size())
+        for (i in 0..<localeList.size()) {
+            val target = localeList.get(i)
+            if (sameLanguageAndScript(selectedLocale, target!!)) {
+                newLocales[i] = appendLocaleExtension(selectedLocale)
+            } else {
+                newLocales[i] = localeList.get(i)
+            }
+        }
+        return newLocales
+    }
+
+    private fun appendLocaleExtension(selectedLocale: Locale): Locale {
+        val systemLocale = Locale.getDefault()
+        val extensionKeys = systemLocale.getExtensionKeys()
+        val builder = Locale.Builder()
+        builder.setLocale(selectedLocale)
+        if (!extensionKeys.isEmpty()) {
+            for (extKey in extensionKeys) {
+                builder.setExtension(extKey, systemLocale.getExtension(extKey))
+            }
+        }
+        return builder.build()
+    }
+
+    private fun sameLanguageAndScript(source: Locale, target: Locale): Boolean {
+        val sourceLanguage = source.language
+        val targetLanguage = target.language
+        val sourceLocaleScript = source.script
+        val targetLocaleScript = target.script
+        if (sourceLanguage == targetLanguage) {
+            if (!sourceLocaleScript.isEmpty() && !targetLocaleScript.isEmpty()) {
+                return sourceLocaleScript == targetLocaleScript
+            }
+            return true
+        }
+        return false
     }
 
     companion object {
+        const val TAG = "RegionPickerApiFirstScreen"
         const val KEY = "key_system_region_picker_page"
+        const val KEY_PREFERENCE = "default_system_language_region_preference"
     }
 }
 // LINT.ThenChange(RegionPickerFragment.java)
