b417c0a2032704ef4800f496a2ea2ba365917819
Zoey Chen <zoeychen@google.com>
2026-01-27 08:05:20 +0000

    [Api First] App language picker: GETTER/SETTER
    NO_IFTTT=API First only
    
    Flag: com.android.settings.flags.catalyst_migration_26q2
    Bug: 469279332
    Test: atest AppLocalePickerApiFirstScreenTest
    Change-Id: I17c0aa5da8cff94c3858e6785d6db296cd9322f4

--- src/com/android/settings/localepicker/AppLocalePickerApiFirstScreen.kt
+++ src/com/android/settings/localepicker/AppLocalePickerApiFirstScreen.kt
@@ -16,14 +16,27 @@
 
 package com.android.settings.localepicker
 
+import android.content.Context
+import android.text.TextUtils
+import com.android.internal.app.AppLocaleCollector
+import com.android.internal.app.LocaleStore
 import com.android.settings.R
 import com.android.settings.flags.Flags
+import com.android.settings.localepicker.AppLocalePickerFragment.ARG_PACKAGE_NAME
+import com.android.settings.localepicker.LocaleUtils.canDisplayLocaleUi
+import com.android.settings.localepicker.LocaleUtils.getAppList
 import com.android.settingslib.metadata.ProvidePreferenceScreen
 import com.android.settingslib.metadata.preferencesapi.PreferencesApiScreen
 import com.android.settingslib.metadata.preferencesapi.category.Category
+import com.android.settingslib.metadata.preferencesapi.preconditions.Allowed
+import com.android.settingslib.metadata.preferencesapi.preconditions.Custom
+import com.android.settingslib.metadata.preferencesapi.types.GeneratedParameterType
+import com.android.settingslib.metadata.preferencesapi.types.GeneratedType
+import com.android.settingslib.metadata.preferencesapi.types.GeneratedValue
+import java.util.Locale
 
 // LINT.IfChange
-@ProvidePreferenceScreen(AppLocalePickerApiFirstScreen.KEY)
+@ProvidePreferenceScreen(AppLocalePickerApiFirstScreen.KEY, parameterized = true)
 class AppLocalePickerApiFirstScreen :
     PreferencesApiScreen(
         key = KEY,
@@ -34,10 +47,97 @@ class AppLocalePickerApiFirstScreen :
 
     init {
         flag { Flags.catalystMigration26q2() }
+
+        parameters {
+            parameter(
+                name = ARG_PACKAGE_NAME,
+                purpose = R.string.app_locale_picker_parameter_purpose,
+                type =
+                    GeneratedParameterType(R.string.app_locale_picker_parameter_description) {
+                        getAppList(context, context.packageManager).map { it ->
+                            val packageName = it.packageName
+                            val appLabel = it.loadLabel(context.packageManager).toString()
+                            GeneratedValue(packageName, appLabel)
+                        }
+                    },
+            )
+
+            prepareScreenExtras { parameters, extras ->
+                extras.putString(ARG_PACKAGE_NAME, parameters[ARG_PACKAGE_NAME])
+            }
+        }
+
+        preconditions(R.string.app_locale_picker_screen_preconditions) {
+            val packageName = keyParameters?.get(ARG_PACKAGE_NAME) ?: ""
+            if (!TextUtils.isEmpty(packageName) && canDisplayLocaleUi(context, packageName)) {
+                Allowed
+            } else {
+                Custom(R.string.app_locale_picker_screen_unavailable)
+            }
+        }
+
+        preference(
+            key = KEY_PREFERENCE,
+            purpose = R.string.app_locale_picker_preference_purpose,
+            type =
+                GeneratedType(description = R.string.app_locale_picker_description) {
+                    // value: language code/tag, for example, en-US, fr-CA
+                    // description: Full language name (language with region), for example,
+                    // English (United State), Français (Canada)
+                    val localeInfoList =
+                        getLocaleInfoList(context, keyParameters?.get(ARG_PACKAGE_NAME) ?: "")
+                    localeInfoList.map {
+                        GeneratedValue(it.locale.toLanguageTag(), it.fullNameNative)
+                    }
+                },
+        ) {
+            get {
+                execute {
+                    val localeInfoList =
+                        getLocaleInfoList(context, keyParameters?.get(ARG_PACKAGE_NAME) ?: "")
+                    val localeCode =
+                        localeInfoList.find { it.isAppCurrentLocale }?.locale?.toLanguageTag() ?: ""
+                    localeCode
+                }
+            }
+            set {
+                execute { value ->
+                    val localeInfo = LocaleStore.getLocaleInfo(Locale.forLanguageTag(value))
+                    LocaleUtils.onLocaleSelected(
+                        context,
+                        localeInfo,
+                        keyParameters?.get(ARG_PACKAGE_NAME) ?: ""
+                    )
+                }
+            }
+        }
+    }
+
+    private fun getSupportedLanguageList(
+        context: Context,
+        appPackageName: String,
+        parent: LocaleStore.LocaleInfo?,
+        isForCountryMode: Boolean,
+    ): Set<LocaleStore.LocaleInfo> {
+        return AppLocaleCollector(context, appPackageName)
+            .getSupportedLocaleList(parent, false, isForCountryMode)
+    }
+
+    private fun getLocaleInfoList(
+        context: Context,
+        appPackageName: String,
+    ): MutableList<LocaleStore.LocaleInfo> {
+        val languageList = getSupportedLanguageList(context, appPackageName, null, false)
+        val localeInfoList: MutableList<LocaleStore.LocaleInfo> = ArrayList()
+        for (language in languageList) {
+            localeInfoList.addAll(getSupportedLanguageList(context, appPackageName, language, true))
+        }
+        return localeInfoList
     }
 
     companion object {
         const val KEY = "key_app_language_picker_page"
+        const val KEY_PREFERENCE = "app_language_picker_preference"
     }
 }
 // LINT.ThenChange(AppLocalePickerFragment.java)
