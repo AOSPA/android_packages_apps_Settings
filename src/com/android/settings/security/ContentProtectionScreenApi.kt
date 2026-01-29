2114f1b6119b149afa18aa2b7170bdab13f9e8a3
Yuri Ufimtsev <yufimtsev@google.com>
2026-01-29 09:40:09 +0000

    Add Catalyst support for Content Protection toggles
    api-tool dump output: https://paste.googleplex.com/5726753748287488
    
    NO_IFTTT=rules are just added, and everything is in sync
    Test: atest --build -c SettingsRoboTests
    Flag: com.android.settings.flags.catalyst_migration_26q2
    Bug: 464954990
    
    Change-Id: I8b49a85162cd3e2374a2921694b0f94eb7ca6487

--- src/com/android/settings/security/ContentProtectionScreenApi.kt
+++ src/com/android/settings/security/ContentProtectionScreenApi.kt
@@ -16,13 +16,27 @@
 
 package com.android.settings.security
 
+import android.Manifest.permission.WRITE_SECURE_SETTINGS
+import android.annotation.SuppressLint
+import android.annotation.UserIdInt
+import android.app.admin.DevicePolicyManager
+import android.content.Context
+import android.os.UserHandle
+import android.os.UserManager
+import android.provider.Settings
+import androidx.core.content.getSystemService
 import com.android.settings.R
 import com.android.settings.flags.Flags
+import com.android.settings.security.ContentProtectionPreferenceUtils.getContentProtectionPolicy
+import com.android.settings.security.ContentProtectionPreferenceUtils.getManagedProfile
+import com.android.settingslib.RestrictedLockUtilsInternal
 import com.android.settingslib.metadata.ProvidePreferenceScreen
 import com.android.settingslib.metadata.preferencesapi.PreferencesApiScreen
 import com.android.settingslib.metadata.preferencesapi.category.Category
+import com.android.settingslib.metadata.preferencesapi.multiusers.PreferenceTarget
 import com.android.settingslib.metadata.preferencesapi.preconditions.Allowed
 import com.android.settingslib.metadata.preferencesapi.preconditions.Custom
+import com.android.settingslib.metadata.preferencesapi.types.AnyBoolean
 
 // LINT.IfChange
 @ProvidePreferenceScreen(ContentProtectionScreenApi.KEY)
@@ -42,11 +56,96 @@ class ContentProtectionScreenApi :
                 Custom(R.string.content_protection_unsupported)
             }
         }
+
+        // Toggle to turn Scanning for Deceptive Apps on or off.
+        preference(
+            key = "content_protection_preference_user_consent_switch",
+            purpose = R.string.content_protection_switch_purpose,
+            type = AnyBoolean,
+            appliesTo = PreferenceTarget.DEVICE,
+        ) {
+            preconditions(R.string.content_protection_switch_preconditions) {
+                if (preferenceAvailableToUser(context, userId)) {
+                    Allowed
+                } else {
+                    // For non-primary user, it is impossible to identify the Preference state,
+                    // as it may depend on the work profile policy of the primary user.
+                    Custom(R.string.content_protection_switch_unsupported)
+                }
+            }
+            get {
+                execute {
+                    if (hasAdmin(context)) {
+                        val contentProtectionPolicy =
+                            getContentProtectionPolicy(context, getManagedProfile(context))
+
+                        @SuppressLint("SwitchIntDef")
+                        when (contentProtectionPolicy) {
+                            DevicePolicyManager.CONTENT_PROTECTION_DISABLED -> return@execute false
+                            DevicePolicyManager.CONTENT_PROTECTION_ENABLED -> return@execute true
+                        }
+                    }
+                    Settings.Global.getInt(
+                        context.getContentResolver(),
+                        KEY_CONTENT_PROTECTION_PREFERENCE,
+                        0,
+                    ) >= 0
+                }
+            }
+            set {
+                permissions(WRITE_SECURE_SETTINGS)
+                preconditions(R.string.content_protection_switch_set_preconditions) {
+                    if (
+                        hasAdmin(context) &&
+                            contentProtectionControlledByPolicy(
+                                context = context,
+                                managedProfile = getManagedProfile(context),
+                            )
+                    ) {
+                        Custom(R.string.content_protection_switch_set_controlled_by_device_admin)
+                    } else {
+                        Allowed
+                    }
+                }
+                execute { isChecked ->
+                    Settings.Global.putInt(
+                        context.getContentResolver(),
+                        KEY_CONTENT_PROTECTION_PREFERENCE,
+                        if (isChecked) 1 else -1,
+                    )
+                }
+            }
+        }
     }
+
+    private fun hasAdmin(context: Context) =
+        if (android.app.admin.flags.Flags.policyTransparencyRefactorV2()) {
+            ContentProtectionPreferenceUtils.getContentProtectionEnforcingAdmin(
+                context,
+                getManagedProfile(context),
+            )
+        } else {
+            RestrictedLockUtilsInternal.getDeviceOwner(context)
+        } != null
+
+    private fun preferenceAvailableToUser(context: Context, @UserIdInt userId: Int): Boolean {
+        val userManager = context.getSystemService<UserManager>() ?: return false
+        val userInfo = userManager.getUserInfo(userId) ?: return false
+        return userInfo.isAdmin || userInfo.isManagedProfile
+    }
+
+    private fun contentProtectionControlledByPolicy(
+        context: Context,
+        managedProfile: UserHandle?,
+    ): Boolean =
+        getContentProtectionPolicy(context, managedProfile) !=
+            DevicePolicyManager.CONTENT_PROTECTION_NOT_CONTROLLED_BY_POLICY
 
     companion object {
         const val KEY = "content_protection_preference_subpage"
+        const val KEY_CONTENT_PROTECTION_PREFERENCE = "content_protection_user_consent"
     }
 }
 // LINT.ThenChange(ContentProtectionPreferenceFragment.java,
-//                 ContentProtectionPreferenceUtils.java)
+//                 ContentProtectionPreferenceUtils.java
+//                 ContentProtectionTogglePreferenceController.java)
