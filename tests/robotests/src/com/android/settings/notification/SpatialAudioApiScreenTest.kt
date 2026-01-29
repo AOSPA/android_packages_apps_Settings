d74630623f3a1ed890518008ab3da47d7d509a8a
abiii <abiii@google.com>
2026-01-29 10:43:12 +0000

    [Catalyst][API First] Migrate Sound & vibration > Spatial audio (Device/hardware dependent) > Phone speakers Sound preference
    NO_IFTTT=API First only
    
    Bug: 479735403
    Test: ApiTester test case & manual with api-tool
    Flag: com.android.settings.flags.catalyst_migration_26q2
    
    Change-Id: I7659ef559802fa168e05a1bc77e935236a6a636c

--- tests/robotests/src/com/android/settings/notification/SpatialAudioApiScreenTest.kt
+++ tests/robotests/src/com/android/settings/notification/SpatialAudioApiScreenTest.kt
@@ -16,6 +16,8 @@
 
 package com.android.settings.notification
 
+import android.media.AudioDeviceAttributes
+import android.media.AudioDeviceInfo
 import android.media.Spatializer
 import android.platform.test.annotations.DisableFlags
 import android.platform.test.annotations.EnableFlags
@@ -23,7 +25,9 @@ import android.platform.test.flag.junit.SetFlagsRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.settings.flags.Flags
 import com.android.settings.testutils2.ApiTester
+import com.android.settings.testutils2.HardwareUnsupportedException
 import com.google.common.truth.Truth.assertThat
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -36,6 +40,14 @@ class SpatialAudioApiScreenTest {
     @get:Rule val setFlagsRule = SetFlagsRule()
 
     private val tester = ApiTester(SpatialAudioApiScreen())
+    private val speaker = AudioDeviceAttributes(
+        AudioDeviceAttributes.ROLE_OUTPUT, AudioDeviceInfo.TYPE_BUILTIN_SPEAKER, ""
+    )
+
+    @Before
+    fun setUp() {
+        ShadowSpatializer.reset()
+    }
 
     @Test
     @EnableFlags(Flags.FLAG_CATALYST_MIGRATION_26Q2)
@@ -51,6 +63,58 @@ class SpatialAudioApiScreenTest {
 
     @Test
     @Config(shadows = [ShadowSpatializer::class])
+    fun get_spatialAudio_whenCompatible_returnsTrue() {
+        ShadowSpatializer.compatibleDevices.add(speaker)
+
+        assertThat(tester.get<Boolean>("spatial_audio")).isTrue()
+    }
+
+    @Test
+    @Config(shadows = [ShadowSpatializer::class])
+    fun get_spatialAudio_whenNotCompatible_returnsFalse() {
+        ShadowSpatializer.compatibleDevices.clear()
+
+        assertThat(tester.get<Boolean>("spatial_audio")).isFalse()
+    }
+
+    @Test
+    @Config(shadows = [ShadowSpatializer::class])
+    fun set_spatialAudio_toTrue_addsDevice() {
+        tester.set("spatial_audio", true)
+
+        assertThat(ShadowSpatializer.compatibleDevices).contains(speaker)
+    }
+
+    @Test
+    @Config(shadows = [ShadowSpatializer::class])
+    fun set_spatialAudio_toFalse_removesDevice() {
+        ShadowSpatializer.compatibleDevices.add(speaker)
+
+        tester.set("spatial_audio", false)
+
+        assertThat(ShadowSpatializer.compatibleDevices).doesNotContain(speaker)
+    }
+
+    @Test(expected = HardwareUnsupportedException::class)
+    @Config(shadows = [ShadowSpatializer::class])
+    fun get_screenPreconditionFails_throwsException() {
+        // Simulate hardware not supporting spatial audio at all
+        ShadowSpatializer.level = Spatializer.SPATIALIZER_IMMERSIVE_LEVEL_NONE
+
+        tester.get<Boolean>("spatial_audio")
+    }
+
+    @Test(expected = HardwareUnsupportedException::class)
+    @Config(shadows = [ShadowSpatializer::class])
+    fun get_preferencePreconditionFails_throwsException() {
+        // Simulate speaker specifically not supporting spatial audio
+        ShadowSpatializer.availableForDevice = false
+
+        tester.get<Boolean>("spatial_audio")
+    }
+
+    @Test
+    @Config(shadows = [ShadowSpatializer::class])
     fun getLaunchIntent_hasIntent() {
         assertThat(tester.getLaunchIntent()).isNotNull()
     }
@@ -58,8 +122,35 @@ class SpatialAudioApiScreenTest {
 
 @Implements(Spatializer::class)
 class ShadowSpatializer {
+    companion object {
+        var level = Spatializer.SPATIALIZER_IMMERSIVE_LEVEL_MULTICHANNEL
+        val compatibleDevices = mutableListOf<AudioDeviceAttributes>()
+        var availableForDevice = true
+
+        fun reset() {
+            level = Spatializer.SPATIALIZER_IMMERSIVE_LEVEL_MULTICHANNEL
+            compatibleDevices.clear()
+            availableForDevice = true
+        }
+    }
+
     @Implementation
-    fun getImmersiveAudioLevel(): Int {
-        return Spatializer.SPATIALIZER_IMMERSIVE_LEVEL_MULTICHANNEL
+    fun getImmersiveAudioLevel(): Int = level
+
+    @Implementation
+    fun isAvailableForDevice(attributes: AudioDeviceAttributes): Boolean = availableForDevice
+
+    @Implementation
+    fun getCompatibleAudioDevices(): List<AudioDeviceAttributes> = compatibleDevices
+
+    @Implementation
+    fun addCompatibleAudioDevice(attributes: AudioDeviceAttributes) {
+        compatibleDevices.add(attributes)
+    }
+
+    @Implementation
+    fun removeCompatibleAudioDevice(attributes: AudioDeviceAttributes) {
+        compatibleDevices.remove(attributes)
     }
 }
+
