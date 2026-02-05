230b65e47f2347107759bd588836c01f9fa0637a
Nergi Rahardi <nergi@google.com>
2026-02-05 15:35:26 +0900

    Refactor out surface rendering logic to separate class
    Similar to a11y and arrow movement refactoring, it'll be cleaner for
    DisplayBlock to delegate operations from upper PrefController to both
    surface rendering and a11y movements as needed. This cleans up many
    moving parts (changing variables) of DisplayBlock to another
    single-purpose class
    
    Bug: 481853550
    Test: atest DisplayBlockTest DisplayTopologyPreferenceTest
    Flag: EXEMPT BUGFIX
    Change-Id: Ice2395b4e6abf987070ddd781ab46edf1f582e34

--- src/com/android/settings/connecteddevice/display/DisplayBlock.kt
+++ src/com/android/settings/connecteddevice/display/DisplayBlock.kt
@@ -19,11 +19,7 @@ package com.android.settings.connecteddevice.display
 import android.graphics.Outline
 import android.graphics.PointF
 import android.graphics.Rect
-import android.os.Trace
-import android.util.Log
 import android.util.Size
-import android.view.SurfaceControl
-import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.view.TouchDelegate
 import android.view.View
@@ -31,8 +27,6 @@ import android.view.ViewOutlineProvider
 import android.widget.FrameLayout
 import androidx.annotation.VisibleForTesting
 import com.android.settings.R
-import com.android.settingslib.utils.ThreadUtils
-import kotlin.time.Duration.Companion.milliseconds
 
 /** Represents a draggable block in the topology pane. */
 class DisplayBlock(val injector: ConnectedDisplayInjector) : FrameLayout(injector.context!!) {
@@ -52,47 +46,8 @@ class DisplayBlock(val injector: ConnectedDisplayInjector) : FrameLayout(injecto
         context.resources.getDimensionPixelSize(R.dimen.display_block_arrow_tappable_area_size)
 
     // Id of the logical display this DisplayBlock represents
-    var logicalDisplayId: Int = -1
-        private set
-
-    // This doesn't necessarily refer to the actual display this block represents. In case of
-    // mirroring, it will be the id of the mirrored display
-    private var displayIdToShowWallpaper: Int? = null
-
-    /** Scale of the mirrored wallpaper to the actual wallpaper size. */
-    @VisibleForTesting
-    internal var surfaceScale: Float? = null
-        private set
-
-    // Size of the surface of the display that would be projected on this block
-    @VisibleForTesting
-    internal var surfaceSize: Size? = null
-        private set
-
-    // These are surfaces which must be removed from the display block hierarchy and released once
-    // the new surface is put in place. This list can have more than one item because we may get
-    // two reset calls before we get a single surfaceChange callback.
-    private val oldSurfaces = mutableListOf<SurfaceControl>()
-    private var letterboxBackgroundSurface: SurfaceControl? = null
-    private var wallpaperSurface: SurfaceControl? = null
-
-    private val updateSurfaceView = Runnable { updateSurfaceView() }
-
-    private val holderCallback =
-        object : SurfaceHolder.Callback {
-            override fun surfaceCreated(h: SurfaceHolder) {}
-
-            override fun surfaceChanged(
-                h: SurfaceHolder,
-                format: Int,
-                newWidth: Int,
-                newHeight: Int,
-            ) {
-                updateSurfaceView()
-            }
-
-            override fun surfaceDestroyed(h: SurfaceHolder) {}
-        }
+    val logicalDisplayId: Int
+        get() = surfaceRenderer.logicalDisplayId
 
     private val layoutChangeListener = OnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
         touchDelegate =
@@ -147,6 +102,9 @@ class DisplayBlock(val injector: ConnectedDisplayInjector) : FrameLayout(injecto
     internal val arrowButtons: Map<Direction, View>
         get() = a11yController.arrowButtons
 
+    @VisibleForTesting
+    internal val surfaceRenderer =
+        DisplayBlockSurfaceRenderer(injector, wallpaperView, cornerRadiusPx)
     private val a11yController =
         DisplayBlockA11yController(context) { direction -> onA11yMoveListener?.invoke(direction) }
 
@@ -169,7 +127,6 @@ class DisplayBlock(val injector: ConnectedDisplayInjector) : FrameLayout(injecto
         accessibilityDelegate = a11yController.accessibilityDelegate
 
         addOnLayoutChangeListener(layoutChangeListener)
-        wallpaperView.holder.addCallback(holderCallback)
     }
 
     override fun onDetachedFromWindow() {
@@ -177,108 +134,6 @@ class DisplayBlock(val injector: ConnectedDisplayInjector) : FrameLayout(injecto
         setTouchListener(null)
         removeOnLayoutChangeListener(layoutChangeListener)
         onA11yMoveListener = null
-    }
-
-    @VisibleForTesting
-    fun updateSurfaceView() {
-        val displayId = displayIdToShowWallpaper ?: return
-
-        if (parent == null) {
-            Log.i(TAG, "View for display $displayId has no parent - cancelling update")
-            return
-        }
-
-        val currentSurface = wallpaperSurface
-        if (currentSurface != null) {
-            renderSurfaceView(currentSurface)
-            return
-        }
-        ThreadUtils.postOnBackgroundThread {
-            Trace.beginSection("Settings Wallpaper fetchSurfaceView $logicalDisplayId")
-            val fetchedSurface = injector.wallpaper(displayId)
-            Trace.endSection()
-            if (fetchedSurface == null) {
-                // Fetch failed, schedule a retry
-                injector.handler.postDelayed(
-                    ::updateSurfaceView,
-                    REFETCH_WALLPAPER_DELAY.inWholeMilliseconds,
-                )
-            } else {
-                injector.handler.post { handleFetchedWallpaperSurface(displayId, fetchedSurface) }
-            }
-        }
-    }
-
-    private fun handleFetchedWallpaperSurface(
-        fetchedForDisplayId: Int,
-        fetchedSurface: SurfaceControl,
-    ) {
-        // If the view is no longer attached or the target display has changed, ignore the result
-        if (parent == null || fetchedForDisplayId != displayIdToShowWallpaper) {
-            Log.i(TAG, "Wallpaper display changed or view detached, ignoring stale surface.")
-            return
-        }
-        wallpaperSurface = fetchedSurface
-        renderSurfaceView(fetchedSurface)
-    }
-
-    /**
-     * Reparents surface to the SurfaceControl of wallpaperView, so that view will render `surface`.
-     * Any surfaces which may be parented to wallpaperView already should be passed in oldSurfaces
-     * and they will be removed from the wallpaperView's hierarchy and released.
-     */
-    private fun renderSurfaceView(surface: SurfaceControl) {
-        val surfaceScale = surfaceScale ?: return
-        val surfaceSize = surfaceSize ?: return
-        val isMirroringOtherDisplay = logicalDisplayId != displayIdToShowWallpaper
-
-        Trace.beginSection("Settings Wallpaper renderSurfaceView display#$displayIdToShowWallpaper")
-
-        val t = injector.createSurfaceTransaction()
-        t.reparent(surface, wallpaperView.surfaceControl)
-
-        // Calculate the final (scaled) dimensions of the wallpaper content
-        val scaledSurfaceWidth = surfaceSize.width * surfaceScale
-        val scaledSurfaceHeight = surfaceSize.height * surfaceScale
-        // Calculate the top-left position needed to center the surface within the wallpaperView
-        val positionX = (wallpaperView.width - scaledSurfaceWidth) / 2f
-        val positionY = (wallpaperView.height - scaledSurfaceHeight) / 2f
-        val destinationCrop = Rect(0, 0, surfaceSize.width, surfaceSize.height)
-
-        t.setScale(surface, surfaceScale, surfaceScale)
-        t.setPosition(surface, positionX, positionY)
-        t.setCrop(surface, destinationCrop)
-        if (isMirroringOtherDisplay) {
-            // In mirroring mode, area outside the letterbox will be translucent, causing display
-            // behind to be spilling over. The workaround here is to add another black background
-            // surface behind the original wallpaper surface
-            val backgroundSurface =
-                injector
-                    .getSurfaceControlBuilder()
-                    .setName("Letterbox background display#$logicalDisplayId")
-                    .setColorLayer()
-                    .build()
-            letterboxBackgroundSurface = backgroundSurface
-
-            t.reparent(backgroundSurface, wallpaperView.surfaceControl)
-            // Set main wallpaper surface to be on top of the background
-            t.setLayer(surface, 1)
-
-            t.setAlpha(backgroundSurface, 0.5f)
-            t.show(backgroundSurface)
-        } else {
-            // Corner radius should only be applied when display is not letterboxed, and has to be
-            // set on the original surface size, so apply a inverse scale here.
-            t.setCornerRadius(surface, cornerRadiusPx.toFloat() / surfaceScale)
-            letterboxBackgroundSurface = null
-        }
-
-        oldSurfaces.forEach { t.remove(it) }
-        t.apply()
-        oldSurfaces.forEach { it.release() }
-        oldSurfaces.clear()
-
-        Trace.endSection()
     }
 
     fun setHighlighted(value: Boolean) {
@@ -326,17 +181,8 @@ class DisplayBlock(val injector: ConnectedDisplayInjector) : FrameLayout(injecto
         surfaceSize: Size,
         arrowMovement: ArrowMovement,
     ) {
-        wallpaperSurface?.let { oldSurfaces.add(it) }
-        letterboxBackgroundSurface?.let { oldSurfaces.add(it) }
-        injector.handler.removeCallbacks(updateSurfaceView)
-        wallpaperSurface = null
-        letterboxBackgroundSurface = null
+        surfaceRenderer.reset(logicalDisplayId, displayIdToShowWallpaper, surfaceScale, surfaceSize)
         positionInPane = topLeft
-
-        this.logicalDisplayId = logicalDisplayId
-        this.displayIdToShowWallpaper = displayIdToShowWallpaper
-        this.surfaceScale = surfaceScale
-        this.surfaceSize = surfaceSize
         a11yController.arrowMovement = arrowMovement
 
         val displayDevice = injector.getDisplay(logicalDisplayId)
@@ -383,7 +229,7 @@ class DisplayBlock(val injector: ConnectedDisplayInjector) : FrameLayout(injecto
         if (width == paddedWidth && height == paddedHeight) {
             // Will not receive a surfaceChanged callback, so in case the wallpaper is different,
             // apply it.
-            updateSurfaceView()
+            surfaceRenderer.updateSurfaceView()
             return
         }
 
@@ -430,10 +276,5 @@ class DisplayBlock(val injector: ConnectedDisplayInjector) : FrameLayout(injecto
         wallpaperView.clipToOutline = true
 
         requestLayout()
-    }
-
-    private companion object {
-        private val REFETCH_WALLPAPER_DELAY = 500.milliseconds
-        private const val TAG = "DisplayBlock"
     }
 }
