c88e29e7675f337f5fec98670039959d4262921f
Ting-Hao Lin <tinghaolin@google.com>
2026-02-11 00:05:27 +0000

    Add Allow WEP networks Screen API
    Implement the "Allow WEP networks" toggle using the Screen API.
    Location: Network & internet > Internet > Network preferences > allow wep networks
    
    NO_IFTTT=Implementing Allow WEP networks in Screen API
    
    Bug: 483071127
    Test: Manually verified Screen API state sync and launching via api-tool
    Test: atest SettingsRoboTests:com.android.settings.wifi.ConfigureWifiApiScreenTest
    Flag: com.android.settings.flags.catalyst_migration_26q2
    Change-Id: I09778709770d76916ff836954990243c6d8c36c2

--- src/com/android/settings/wifi/ConfigureWifiApiScreen.kt
+++ src/com/android/settings/wifi/ConfigureWifiApiScreen.kt
@@ -18,6 +18,8 @@ package com.android.settings.wifi
 
 import android.Manifest.permission.NETWORK_SETTINGS
 import android.Manifest.permission.NETWORK_SETUP_WIZARD
+import android.net.ConnectivityManager
+import android.net.wifi.WifiInfo
 import android.net.wifi.WifiManager
 import com.android.settings.R
 import com.android.settings.flags.Flags
@@ -25,8 +27,12 @@ import com.android.settingslib.datastore.Permissions.Companion.anyOf
 import com.android.settingslib.metadata.ProvidePreferenceScreen
 import com.android.settingslib.metadata.preferencesapi.PreferencesApiScreen
 import com.android.settingslib.metadata.preferencesapi.category.Category
+import com.android.settingslib.metadata.preferencesapi.preconditions.Allowed
+import com.android.settingslib.metadata.preferencesapi.preconditions.HardwareUnsupported
+import com.android.settingslib.metadata.preferencesapi.preconditions.InvalidPreference
 import com.android.settingslib.metadata.preferencesapi.types.AnyBoolean
 import com.android.settingslib.utils.ThreadUtils
+import java.util.concurrent.CountDownLatch
 
 // LINT.IfChange
 @ProvidePreferenceScreen(ConfigureWifiApiScreen.KEY)
@@ -76,6 +82,68 @@ class ConfigureWifiApiScreen :
 
                     OpenNetworkNotifierHelper.getInstance(context.applicationContext)
                         .setEnabled(wifiManager, bgExecutor, enabled)
+                }
+            }
+        }
+
+        preference(
+            key = "wifi_allow_wep_networks",
+            purpose = R.string.wifi_allow_wep_networks_purpose,
+            type = AnyBoolean,
+        ) {
+            preconditions(R.string.wifi_allow_wep_networks_summary_carrier_not_allow) {
+                val wifiManager = context.getSystemService(WifiManager::class.java)!!
+                if (wifiManager.isWepSupported) {
+                    Allowed
+                } else {
+                    HardwareUnsupported(R.string.wifi_allow_wep_networks_summary_carrier_not_allow)
+                }
+            }
+
+            get {
+                permissions(anyOf(NETWORK_SETTINGS, NETWORK_SETUP_WIZARD))
+                execute {
+                    val wifiManager = context.getSystemService(WifiManager::class.java)!!
+                    var isWepAllowed = false
+                    val latch = CountDownLatch(1)
+                    val bgExecutor = ThreadUtils.getBackgroundExecutor()
+
+                    wifiManager.queryWepAllowed(bgExecutor) { allowed ->
+                        isWepAllowed = allowed
+                        latch.countDown()
+                    }
+                    latch.await()
+                    isWepAllowed
+                }
+            }
+
+            set {
+                permissions(anyOf(NETWORK_SETTINGS, NETWORK_SETUP_WIZARD))
+                valuePreconditions(R.string.wifi_settings_wep_networks_disconnect_title) { newValue
+                    ->
+                    val connectivityManager =
+                        context.getSystemService(ConnectivityManager::class.java)
+                    val wifiInfo =
+                        connectivityManager
+                            ?.getNetworkCapabilities(connectivityManager.activeNetwork)
+                            ?.transportInfo as? WifiInfo
+
+                    // If user tries to set to FALSE and they are currently on a WEP network.
+                    if (!newValue && wifiInfo?.currentSecurityType == WifiInfo.SECURITY_TYPE_WEP) {
+                        // Return a failure result with the warning summary
+                        InvalidPreference(
+                            "internet_settings",
+                            "main_toggle_wifi",
+                            R.string.wifi_settings_wep_networks_disconnect_summary,
+                        )
+                    } else {
+                        // Otherwise, the value is allowed to be set
+                        Allowed
+                    }
+                }
+
+                execute { newValue ->
+                    context.getSystemService(WifiManager::class.java)!!.setWepAllowed(newValue)
                 }
             }
         }
