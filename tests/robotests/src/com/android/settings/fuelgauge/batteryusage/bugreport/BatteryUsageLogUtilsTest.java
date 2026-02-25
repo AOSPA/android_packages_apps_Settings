0fe413acd668f7bb30d17b3bfcb107aa43821c4a
mxyyiyi <mxyyiyi@google.com>
2026-02-25 19:38:55 +0800

    [Log] Move the Battery usage log to the non-UI thread.
    Bug: 485704761
    Fix: 485704761
    Test: atest BatteryUsageLogUtilsTest
    Flag: EXEMPT BUGFIX
    Change-Id: I5ff933278f5d5614d4e923734ff7c4d53fc3c5c6

--- tests/robotests/src/com/android/settings/fuelgauge/batteryusage/bugreport/BatteryUsageLogUtilsTest.java
+++ tests/robotests/src/com/android/settings/fuelgauge/batteryusage/bugreport/BatteryUsageLogUtilsTest.java
@@ -30,9 +30,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.android.util.concurrent.PausedExecutorService;
+import org.robolectric.shadows.ShadowLooper;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(RobolectricTestRunner.class)
@@ -41,9 +45,12 @@ public final class BatteryUsageLogUtilsTest {
     private StringWriter mTestStringWriter;
     private PrintWriter mTestPrintWriter;
     private Context mContext;
+    private PausedExecutorService mExecutorService;
 
     @Before
     public void setUp() {
+        mExecutorService = new PausedExecutorService();
+        BatteryUsageLogUtils.setExecutor(mExecutorService);
         mContext = ApplicationProvider.getApplicationContext();
         mTestStringWriter = new StringWriter();
         mTestPrintWriter = new PrintWriter(mTestStringWriter);
@@ -63,34 +70,65 @@ public final class BatteryUsageLogUtilsTest {
     @Test
     public void writeLog_multipleLogs_withCorrectCounts() {
         final int expectedCount = 10;
+        final List<String> expectedTokens = new ArrayList<>();
         for (int i = 0; i < expectedCount; i++) {
-            BatteryUsageLogUtils.writeLog(mContext, Action.SCHEDULE_JOB, "");
+            writeLogWrapper(mContext, Action.SCHEDULE_JOB, " " + i, expectedTokens);
         }
-        BatteryUsageLogUtils.writeLog(mContext, Action.EXECUTE_JOB, "");
+        writeLogWrapper(mContext, Action.EXECUTE_JOB, "execute", expectedTokens);
 
+        assertActionCount("EXECUTE_JOB", 0);
+        mExecutorService.runAll();
+        ShadowLooper.idleMainLooper();
         BatteryUsageLogUtils.printHistoricalLog(mContext, mTestPrintWriter);
 
+        assertInOrder(expectedTokens);
         assertActionCount("SCHEDULE_JOB", expectedCount);
         assertActionCount("EXECUTE_JOB", 1);
     }
 
     @Test
     public void writeLog_overMaxEntriesLogs_withCorrectCounts() {
-        BatteryUsageLogUtils.writeLog(mContext, Action.SCHEDULE_JOB, "");
-        BatteryUsageLogUtils.writeLog(mContext, Action.SCHEDULE_JOB, "");
+        final List<String> expectedTokens = new ArrayList<>();
+        writeLogWrapper(mContext, Action.SCHEDULE_JOB, "0", expectedTokens);
+        writeLogWrapper(mContext, Action.SCHEDULE_JOB, "1", expectedTokens);
         for (int i = 0; i < BatteryUsageLogUtils.MAX_ENTRIES * 2; i++) {
-            BatteryUsageLogUtils.writeLog(mContext, Action.EXECUTE_JOB, "");
+            writeLogWrapper(mContext, Action.EXECUTE_JOB, "" + i, expectedTokens);
         }
 
+        assertActionCount("EXECUTE_JOB", 0);
+        mExecutorService.runAll();
+        ShadowLooper.idleMainLooper();
         BatteryUsageLogUtils.printHistoricalLog(mContext, mTestPrintWriter);
 
         final String dumpResults = mTestStringWriter.toString();
         assertThat(dumpResults.contains("SCHEDULE_JOB")).isFalse();
+        assertInOrder(expectedTokens);
         assertActionCount("EXECUTE_JOB", BatteryUsageLogUtils.MAX_ENTRIES);
     }
 
     private void assertActionCount(String token, int count) {
         final String dumpResults = mTestStringWriter.toString();
         assertThat(dumpResults.split(token).length).isEqualTo(count + 1);
+    }
+
+    private void assertInOrder(List<String> tokens) {
+        final String dumpResults = mTestStringWriter.toString();
+        int lastIndex = 0;
+        for (String token : tokens) {
+            int foundIndex = dumpResults.indexOf(token, lastIndex);
+            assertThat(foundIndex).isNotEqualTo(-1);
+            lastIndex = foundIndex + token.length();
+        }
+    }
+
+    private void writeLogWrapper(Context context, Action action, String actionDescription,
+                                 final List<String> expectedTokens) {
+        BatteryUsageLogUtils.writeLog(context, action, actionDescription);
+        if (expectedTokens != null) {
+            if (expectedTokens.size() >= BatteryUsageLogUtils.MAX_ENTRIES) {
+                expectedTokens.removeFirst();
+            }
+            expectedTokens.add(action.name() + " " + actionDescription);
+        }
     }
 }
