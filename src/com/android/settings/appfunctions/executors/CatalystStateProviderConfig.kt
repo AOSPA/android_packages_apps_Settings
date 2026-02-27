4b0bf1feae1823a73c2fcda9e3e75b2a99b368f5
mxyyiyi <mxyyiyi@google.com>
2026-02-27 23:35:50 +0800

    [API First] Deep link for App battery usage list.
    Bug: 464955539
    Test: atest AppBatteryUsageListApiScreenTest
    Flag: com.android.settings.flags.catalyst_migration_26q2
    Change-Id: I0f81d0dca4f9ddddc91373b5bd7c4e492d7ecd22

--- src/com/android/settings/appfunctions/executors/CatalystStateProviderConfig.kt
+++ src/com/android/settings/appfunctions/executors/CatalystStateProviderConfig.kt
@@ -104,8 +104,8 @@ import com.android.settings.deviceinfo.storage.StoragePreferenceScreen
 import com.android.settings.display.AmbientDisplayAlwaysOnPreferenceScreen
 import com.android.settings.display.AutoBrightnessScreen
 import com.android.settings.display.ColorContrastApiScreen
-import com.android.settings.display.ColorModeScreen
 import com.android.settings.display.ColorModeApiScreen
+import com.android.settings.display.ColorModeScreen
 import com.android.settings.display.DisplayScreen
 import com.android.settings.display.HdrBrightnessApiScreen
 import com.android.settings.display.NightDisplayApiScreen
@@ -114,7 +114,6 @@ import com.android.settings.display.ScreenTimeoutScreen
 import com.android.settings.display.darkmode.DarkModeScreen
 import com.android.settings.dream.DreamSettingsApiScreen
 import com.android.settings.dream.ScreensaverScreen
-import com.android.settings.flags.Flags
 import com.android.settings.emergency.EmergencyDashboardScreen
 import com.android.settings.fuelgauge.batterysaver.BatterySaverScreen
 import com.android.settings.fuelgauge.batteryusage.PowerUsageAdvancedScreen
@@ -138,8 +137,8 @@ import com.android.settings.location.BluetoothScanningApiScreen
 import com.android.settings.location.LocationScreen
 import com.android.settings.location.LocationServicesScreen
 import com.android.settings.location.RecentLocationAccessScreen
-import com.android.settings.network.AdaptiveConnectivityScreen
 import com.android.settings.network.AdaptiveConnectivityApiScreen
+import com.android.settings.network.AdaptiveConnectivityScreen
 import com.android.settings.network.AirplaneModeSettingsScreen
 import com.android.settings.network.MobileNetworkListScreen
 import com.android.settings.network.NetworkDashboardScreen
@@ -162,7 +161,6 @@ import com.android.settings.safetycenter.ui.DeviceUnlockApiScreen
 import com.android.settings.safetycenter.ui.MoreSecurityPrivacyScreenApi
 import com.android.settings.safetycenter.ui.PrivacyControlsScreenApi
 import com.android.settings.safetycenter.ui.SystemAndUpdatesScreenApi
-import com.android.settings.security.ContentProtectionScreenApi
 import com.android.settings.security.CredentialManagementAppScreenApi
 import com.android.settings.security.EncryptionAndCredentialScreenApi
 import com.android.settings.security.InstallCertificateFromStorageScreenApi
@@ -170,6 +168,7 @@ import com.android.settings.security.LockScreenPreferenceScreen
 import com.android.settings.security.trustagent.TrustAgentApiScreen
 import com.android.settings.sound.MediaControlsScreen
 import com.android.settings.spa.app.appcompat.UserAspectRatioAppsApiScreen
+import com.android.settings.spa.app.battery.AppBatteryUsageListApiScreen
 import com.android.settings.spa.app.catalyst.AllAppsScreen
 import com.android.settings.spa.app.catalyst.AppInfoScreen
 import com.android.settings.spa.app.catalyst.AppInfoStorageScreen
@@ -330,6 +329,11 @@ private fun getCatalystScreenConfigs() =
             screenKey = PowerUsageAdvancedScreen.KEY,
             appFunctionTypes = setOf(DeviceStateAppFunctionType.GET_BATTERY),
         ),
+        PerScreenCatalystConfig(
+            enabled = true,
+            screenKey = AppBatteryUsageListApiScreen.KEY,
+            appFunctionTypes = setOf(DeviceStateAppFunctionType.GET_BATTERY),
+        ),
         PerScreenCatalystConfig(enabled = true, screenKey = ScreenTimeoutScreen.KEY),
         PerScreenCatalystConfig(
             enabled = true,
@@ -377,10 +381,7 @@ private fun getCatalystScreenConfigs() =
             screenKey = ScreensaverScreen.KEY,
             appFunctionTypes = setOf(DeviceStateAppFunctionType.GET_UNCATEGORIZED),
         ),
-        PerScreenCatalystConfig(
-            enabled = true,
-            screenKey = DreamSettingsApiScreen.KEY,
-        ),
+        PerScreenCatalystConfig(enabled = true, screenKey = DreamSettingsApiScreen.KEY),
         PerScreenCatalystConfig(
             enabled = true,
             screenKey = SystemNavigationGestureScreen.KEY,
---
+++ tests/robotests/src/com/android/settings/spa/app/battery/AppBatteryUsageListApiScreenTest.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.spa.app.battery
+
+import android.platform.test.annotations.DisableFlags
+import android.platform.test.annotations.EnableFlags
+import android.platform.test.flag.junit.SetFlagsRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.settings.flags.Flags
+import com.android.settings.testutils2.ApiTester
+import com.google.common.truth.Truth.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AppBatteryUsageListApiScreenTest {
+    @get:Rule val setFlagsRule = SetFlagsRule()
+
+    private val tester = ApiTester(AppBatteryUsageListApiScreen())
+
+    @Test
+    @EnableFlags(Flags.FLAG_CATALYST_MIGRATION_26Q2)
+    fun getScreen_flagEnabled_isNotNull() {
+        assertThat(tester.getScreen()).isNotNull()
+    }
+
+    @Test
+    @DisableFlags(Flags.FLAG_CATALYST_MIGRATION_26Q2)
+    fun getScreen_flagDisabled_isNull() {
+        assertThat(tester.getScreen()).isNull()
+    }
+}
