55378eae521abecb90880c63c5e6bbcbde91afb5
smaftei <smaftei@google.com>
2026-03-03 17:43:37 +0200

    [Catalyst][API First] Add set warning to screens
    NO_IFTTT=Adding warnings to catalyst screens only
    
    Bug: 489340694
    Test: checked hint text manually
    Flag: com.android.settings.flags.catalyst_migration_26q2
    
    Change-Id: Ie12329dd00df706d82573ed86a3e2aeb6a6a636c

--- src/com/android/settings/accessibility/hearingdevices/ui/HearingAidCompatibilitySwitchPreference.kt
+++ src/com/android/settings/accessibility/hearingdevices/ui/HearingAidCompatibilitySwitchPreference.kt
@@ -31,9 +31,11 @@ import com.android.settingslib.metadata.PreferenceAvailabilityProvider
 import com.android.settingslib.metadata.PreferenceLifecycleContext
 import com.android.settingslib.metadata.PreferenceLifecycleProvider
 import com.android.settingslib.metadata.PreferenceMetadata
+import com.android.settingslib.metadata.PreferenceSetWarningProvider
 import com.android.settingslib.metadata.ReadWritePermit
 import com.android.settingslib.metadata.SensitivityLevel
 import com.android.settingslib.metadata.SwitchPreference
+import com.android.settingslib.metadata.WarningInfo
 import com.android.settingslib.preference.SwitchPreferenceBinding
 
 class HearingAidCompatibilitySwitchPreference(private val context: Context) :
@@ -46,6 +48,7 @@ class HearingAidCompatibilitySwitchPreference(private val context: Context) :
     SwitchPreferenceBinding,
     PreferenceAvailabilityProvider,
     Preference.OnPreferenceChangeListener,
+    PreferenceSetWarningProvider,
     PreferenceLifecycleProvider {
     private var telephonyManager: TelephonyManager =
         context.getSystemService(TelephonyManager::class.java)!!
@@ -72,6 +75,20 @@ class HearingAidCompatibilitySwitchPreference(private val context: Context) :
         callingPid: Int,
         callingUid: Int,
     ) = ReadWritePermit.ALLOW
+
+    private fun getWarningMessage(): String? {
+        val hearingAidCompatibilityDisclaimerMessage =
+            context.getText(R.string.hac_disclaimer_message)
+
+        if (!TextUtils.isEmpty(hearingAidCompatibilityDisclaimerMessage)) {
+            return hearingAidCompatibilityDisclaimerMessage.toString()
+        }
+
+        return null
+    }
+
+    override val setWarning =
+        getWarningMessage()?.let { warningMessage -> WarningInfo(warningMessage = warningMessage) }
 
     override val sensitivityLevel
         get() = SensitivityLevel.DEEP_LINK_ONLY
