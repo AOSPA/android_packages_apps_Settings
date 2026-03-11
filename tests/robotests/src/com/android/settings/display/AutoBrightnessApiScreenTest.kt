3c62aeedd4f03a35e318d0245d519a5ec00ffa27
Fiona Campbell <flc@google.com>
2026-03-11 05:17:22 -0700

    Autobrightness Preference - settings API
    Bug: 488295007
    Flag: com.android.settings.flags.catalyst_migration_26q2
    Test: atest AutoBrightnessApiScreenTest
    NO_IFTTT=migration in progress
    Change-Id: I8c719e66f86202827b12a4039a5cb2bd0ba052c6

--- tests/robotests/src/com/android/settings/display/AutoBrightnessApiScreenTest.kt
+++ tests/robotests/src/com/android/settings/display/AutoBrightnessApiScreenTest.kt
@@ -15,29 +15,53 @@
  */
 package com.android.settings.display
 
+import android.Manifest.permission.WRITE_SETTINGS
+import android.app.Application
+import android.content.Context
+import android.os.UserManager
 import android.platform.test.annotations.DisableFlags
 import android.platform.test.annotations.EnableFlags
 import android.platform.test.flag.junit.SetFlagsRule
+import android.provider.Settings
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.internal.R
 import com.android.server.display.feature.flags.Flags.FLAG_DISPLAY_SETTINGS_API_SCREEN_SUPPORT
 import com.android.settings.flags.Flags.FLAG_CATALYST_MIGRATION_26Q2
 import com.android.settings.testutils.shadow.SettingsShadowResources
+import com.android.settings.testutils.shadow.ShadowUserManager
 import com.android.settings.testutils2.ApiTester
 import com.android.settings.testutils2.FailedPreconditionException
+import com.android.settings.testutils2.MissingPermissionException
 import com.google.common.truth.Truth.assertThat
 import org.junit.Assert.assertThrows
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.Shadows.shadowOf
 import org.robolectric.annotation.Config
+import org.robolectric.shadow.api.Shadow
+import org.robolectric.shadows.ShadowApplication
 
 // LINT.IfChange
 @RunWith(AndroidJUnit4::class)
-@Config(shadows = [SettingsShadowResources::class])
+@Config(
+    shadows = [SettingsShadowResources::class, ShadowUserManager::class]
+)
 class AutoBrightnessApiScreenTest {
     private val tester = ApiTester(AutoBrightnessApiScreen())
     @get:Rule val setFlagsRule = SetFlagsRule()
+    private val context: Context = ApplicationProvider.getApplicationContext()
+    private val shadowApplication: ShadowApplication = shadowOf(context as Application)
+    private lateinit var shadowUserManager: ShadowUserManager
+
+    @Before
+    fun setUp() {
+        SettingsShadowResources.overrideResource(R.bool.config_automatic_brightness_available, true)
+        shadowApplication.grantPermissions(WRITE_SETTINGS)
+        shadowUserManager = Shadow.extract(context.getSystemService(Context.USER_SERVICE))
+    }
 
     @Test
     @EnableFlags(FLAG_CATALYST_MIGRATION_26Q2, FLAG_DISPLAY_SETTINGS_API_SCREEN_SUPPORT)
@@ -63,9 +87,114 @@ class AutoBrightnessApiScreenTest {
 
     @Test
     fun getLaunchIntent_preconditionMet_isNotNull() {
-        SettingsShadowResources.overrideResource(R.bool.config_automatic_brightness_available, true)
 
         assertThat(tester.getLaunchIntent()).isNotNull()
+    }
+
+    @Test
+    @EnableFlags(FLAG_CATALYST_MIGRATION_26Q2)
+    fun getSetting_manualMode() {
+        Settings.System.putInt(
+            context.contentResolver,
+            Settings.System.SCREEN_BRIGHTNESS_MODE,
+            Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL,
+        )
+
+        val isAutomatic = false
+
+        assertThat(tester.get<Boolean>(AutoBrightnessApiScreen.PREFERENCE_KEY))
+            .isEqualTo(isAutomatic)
+    }
+
+    @Test
+    @EnableFlags(FLAG_CATALYST_MIGRATION_26Q2)
+    fun getSetting_automaticMode() {
+
+        Settings.System.putInt(
+            context.contentResolver,
+            Settings.System.SCREEN_BRIGHTNESS_MODE,
+            Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC,
+        )
+        // verify whether setting is set to automatic mode
+        assertThat(tester.get<Boolean>(AutoBrightnessApiScreen.PREFERENCE_KEY)).isTrue()
+    }
+
+    @Test
+    @EnableFlags(FLAG_CATALYST_MIGRATION_26Q2)
+    fun setSetting_manualMode() {
+        val isAutomatic = false
+
+        tester.set<Boolean>(AutoBrightnessApiScreen.PREFERENCE_KEY, isAutomatic)
+
+        // verify whether setting is set to manual mode
+        assertThat(
+                Settings.System.getInt(
+                    context.contentResolver,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL,
+                )
+            )
+            .isEqualTo(Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL)
+    }
+
+    @Test
+    @EnableFlags(FLAG_CATALYST_MIGRATION_26Q2)
+    fun setSetting_automaticMode() {
+        val isAutomatic = true
+
+        tester.set<Boolean>(AutoBrightnessApiScreen.PREFERENCE_KEY, isAutomatic)
+
+        // verify whether setting is set to automatic mode
+        assertThat(
+                Settings.System.getInt(
+                    context.contentResolver,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL,
+                )
+            )
+            .isEqualTo(Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC)
+    }
+
+    @Test
+    @EnableFlags(FLAG_CATALYST_MIGRATION_26Q2)
+    fun autoBrightnessConfigDisabled_throwsException() {
+        SettingsShadowResources.overrideResource(
+            R.bool.config_automatic_brightness_available,
+            false,
+        )
+
+        assertThrows(FailedPreconditionException::class.java) {
+            tester.get<Int>(AutoBrightnessApiScreen.PREFERENCE_KEY)
+        }
+
+        assertThrows(FailedPreconditionException::class.java) { tester.getLaunchIntent() }
+
+        assertThrows(FailedPreconditionException::class.java) {
+            val isAutomatic = true
+            tester.set<Boolean>(AutoBrightnessApiScreen.PREFERENCE_KEY, isAutomatic)
+        }
+    }
+
+    @Test
+    @EnableFlags(FLAG_CATALYST_MIGRATION_26Q2)
+    fun userRestrictionDisallowConfigBrightness_set_throwsException() {
+        shadowUserManager.addBaseUserRestriction(UserManager.DISALLOW_CONFIG_BRIGHTNESS)
+
+        assertThrows(FailedPreconditionException::class.java) {
+            val isAutomatic = true
+            tester.set<Boolean>(AutoBrightnessApiScreen.PREFERENCE_KEY, isAutomatic)
+        }
+    }
+
+    @Test
+    @EnableFlags(FLAG_CATALYST_MIGRATION_26Q2)
+    fun setSetting_failPermission() {
+        shadowApplication.denyPermissions(WRITE_SETTINGS)
+        val isAutomatic = true
+
+        assertThrows(MissingPermissionException::class.java) {
+            tester.set<Boolean>(AutoBrightnessApiScreen.PREFERENCE_KEY, isAutomatic)
+        }
     }
 }
 // LINT.ThenChange(AutoBrightnessPreferenceControllerTest.java, AutoBrightnessScreenTest.kt)
