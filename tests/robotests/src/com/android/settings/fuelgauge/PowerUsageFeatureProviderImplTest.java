e17392ac9475cc8a3142810fbdec6b79b2a36dd6
Wesley Wang <wesleycwwang@google.com>
2026-03-20 02:12:01 -0700

    Introduce BatteryAdvanceInfoDialog
    Bug: 493089114
    Flag: com.google.android.settings.flags.battery_advanced_info_enabled
    Test: atest SettingsTest
    Change-Id: Ic39c421d919bf0c82fc3c4e1e1fd1ed2760b817e

--- tests/robotests/src/com/android/settings/fuelgauge/PowerUsageFeatureProviderImplTest.java
+++ tests/robotests/src/com/android/settings/fuelgauge/PowerUsageFeatureProviderImplTest.java
@@ -27,6 +27,7 @@ import android.content.pm.PackageManager;
 import android.os.Process;
 
 import com.android.settings.fuelgauge.batteryusage.BatteryAdvanceInfoController;
+import com.android.settings.fuelgauge.batteryusage.BatteryAdvanceInfoDialog;
 import com.android.settings.fuelgauge.batteryusage.DataProcessor;
 
 import org.junit.Before;
@@ -157,6 +158,13 @@ public class PowerUsageFeatureProviderImplTest {
     @Test
     public void isTypeSystem_uidOther_returnFalse() {
         assertThat(mPowerFeatureProvider.isTypeSystem(UID_OTHER, null)).isFalse();
+    }
+
+    @Test
+    public void getBatteryAdvanceInfoDialog_returnNull() {
+        BatteryAdvanceInfoDialog dialog = mPowerFeatureProvider.getBatteryAdvanceInfoDialog();
+
+        assertThat(dialog).isNull();
     }
 
     @Test
