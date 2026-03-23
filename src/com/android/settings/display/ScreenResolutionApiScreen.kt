47671ccd30a8915bd5cb36d6d1a0d133eee115d6
Fiona Campbell <flc@google.com>
2026-03-23 09:08:49 -0700

    Screen resolution set / get
    Bug: 490405099
    Flag: com.android.settings.flags.catalyst_migration_26q2
    Test: atest ScreenResolutionApiScreenTest
    NO_IFTTT=migration in progress
    
    
    
    Change-Id: I230af106b1455d757fd43d5e3b991428f4c4d062

--- src/com/android/settings/display/ScreenResolutionApiScreen.kt
+++ src/com/android/settings/display/ScreenResolutionApiScreen.kt
@@ -16,6 +16,7 @@
 
 package com.android.settings.display
 
+import android.Manifest.permission.MODIFY_USER_PREFERRED_DISPLAY_MODE
 import com.android.settings.R
 import com.android.settings.flags.Flags
 import com.android.settingslib.metadata.ProvidePreferenceScreen
@@ -23,6 +24,16 @@ import com.android.settingslib.metadata.preferencesapi.PreferencesApiScreen
 import com.android.settingslib.metadata.preferencesapi.category.Category
 import com.android.settingslib.metadata.preferencesapi.preconditions.Allowed
 import com.android.settingslib.metadata.preferencesapi.preconditions.HardwareUnsupported
+import com.android.settingslib.metadata.preferencesapi.types.CustomEnum
+import com.android.settingslib.metadata.preferencesapi.types.EnumApiWithRes
+
+enum class ScreenResolutionOption(
+    override val asApiValue: Int,
+    override val purpose: Int,
+) : EnumApiWithRes<Int> {
+    HIGH(0, R.string.screen_resolution_option_high),
+    FULL(1, R.string.screen_resolution_option_full),
+}
 
 // LINT.IfChange
 @ProvidePreferenceScreen(ScreenResolutionApiScreen.KEY)
@@ -45,10 +56,43 @@ class ScreenResolutionApiScreen :
                 HardwareUnsupported(R.string.screen_resolution_unsupported)
             }
         }
+
+        preference(
+            key = PREFERENCE_KEY,
+            purpose = R.string.screen_resolution_purpose,
+            type = CustomEnum(
+                ScreenResolutionOption::class,
+                R.string.screen_resolution_purpose
+            )
+        ) {
+            get {
+                execute {
+                    val currentWidth = context.getDisplayWidth()
+                    if (currentWidth == context.getHighResolutionDisplayWidth()) {
+                        ScreenResolutionOption.HIGH
+                    } else {
+                        ScreenResolutionOption.FULL
+                    }
+                }
+            }
+            set {
+                permissions(MODIFY_USER_PREFERRED_DISPLAY_MODE)
+                execute { value ->
+                    val width =
+                        if (value == ScreenResolutionOption.HIGH) {
+                            context.getHighResolutionDisplayWidth()
+                        } else {
+                            context.getFullResolutionDisplayWidth()
+                        }
+                    context.setDisplayModeByWidth(width)
+                }
+            }
+        }
     }
 
     companion object {
         const val KEY = "screen_resolution"
+        const val PREFERENCE_KEY = "screen_resolution_preference"
     }
 }
 // LINT.ThenChange(ScreenResolutionFragment.java, ScreenResolutionController.java)
