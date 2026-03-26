f0ab542927f471050558ee7bba857f2466156c47
Jonathan Scott <scottjonathan@google.com>
2026-03-26 11:44:59 -0700

    Small fixes.
    NO_IFTTT=Catalyst only
    Bug: 494410190
    Flag: com.android.settings.flags.catalyst_migration_26q2
    Change-Id: I5d6b2031e687705b2f353e338dba5d3a46ae69e6

--- src/com/android/settings/accessibility/screenmagnification/ui/CursorFollowingPreference.kt
+++ src/com/android/settings/accessibility/screenmagnification/ui/CursorFollowingPreference.kt
@@ -148,7 +148,7 @@ class CursorFollowingPreference :
         return !context.isInSetupWizard() && isMagnificationCursorFollowingModeDialogSupported()
     }
 
-    override fun getEnabledDescription(): String = "Screen magnification must be set to full screen or both."
+    override fun getEnabledDescription(): String = "Screen magnification must be set to 'full screen' or 'switch between full and partial screen'"
 
     override fun getEnabledStability() = PreconditionStability.UNSTABLE
 
